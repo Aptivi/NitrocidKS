0a9bd6eaf6613ab51a3a9037a5cdf20b6b5ca347
EoflaOE <eoflaoevicecity@gmail.com>
2020-02-09 17:58:48 +0200

    [Linux] Use VLC (currently unusable)

--- Kernel Simulator/Misc/Speech/VoiceManagement.vb
+++ Kernel Simulator/Misc/Speech/VoiceManagement.vb
@@ -17,6 +17,7 @@
 '    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Imports NAudio.Wave
+Imports LibVLCSharp.Shared
 
 Public Module VoiceManagement
 
@@ -28,24 +29,44 @@ Public Module VoiceManagement
         Wdbg("I", "Headers required: {0}", SpeakReq.Headers.Count)
         SpeakReq.DownloadFile("http://translate.google.com/translate_tts?tl=en&q=" + Text + "&client=gtx", paths("Temp") + "/tts.mpeg")
 
-        'Read the file
-        Dim AudioRead As New AudioFileReader(paths("Temp") + "/tts.mpeg")
-        Wdbg("I", "AudioRead: {0}", AudioRead.TotalTime)
+        'Platform checks, because NAudio is Windows only (mfplat.dll, Windows APIs, etc.). See https://github.com/naudio/NAudio/issues/184
+        'TODO: Remove NAudio in favor of VLC.
+        If EnvironmentOSType.Contains("Windows") Then
+            'Read the file
+            Dim AudioRead As New AudioFileReader(paths("Temp") + "/tts.mpeg")
+            Wdbg("I", "AudioRead: {0}", AudioRead.TotalTime)
 
-        'Initialize Wave Out
-        Dim WaveEvent As New WaveOutEvent()
-        WaveEvent.Init(AudioRead)
-        Wdbg("I", "Initialized Wave Out using {0}", WaveEvent.DeviceNumber)
+            'Initialize Wave Out
+            Dim WaveEvent As New WaveOutEvent()
+            WaveEvent.Init(AudioRead)
+            Wdbg("I", "Initialized Wave Out using {0}", WaveEvent.DeviceNumber)
 
-        'Play the speech
-        WaveEvent.Play()
-        While WaveEvent.PlaybackState = PlaybackState.Playing
-        End While
+            'Play the speech
+            WaveEvent.Play()
+            While WaveEvent.PlaybackState = PlaybackState.Playing
+            End While
 
-        'Dispose and close objects
-        Wdbg("I", "Stopped.")
-        WaveEvent.Dispose()
-        AudioRead.Close()
+            'Dispose and close objects
+            Wdbg("I", "Stopped.")
+            WaveEvent.Dispose()
+            AudioRead.Close()
+        Else
+            'Use VLC for playback. Requires installing packages as specified in https://github.com/videolan/libvlcsharp/blob/3.x/docs/linux-setup.md
+            Core.Initialize()
+            Dim MLib As New LibVLC
+            Dim MP As New MediaPlayer(MLib)
+            AddHandler MLib.Log, Sub(sender, e) W($"{e.Level}@{e.Module}: {e.Message}", True, ColTypes.Neutral)
+            Dim MFile As New Media(MLib, paths("Temp") + "/tts.mpeg")
+            MP.Media = MFile
+            MP.Play()
+            While MP.State = VLCState.Playing
+            End While
+
+            'Dispose and close objects
+            Wdbg("I", "Stopped.")
+            MFile.Dispose()
+            MP.Dispose()
+        End If
 
         'Remove the file
         IO.File.Delete(paths("Temp") + "/tts.mpeg")
