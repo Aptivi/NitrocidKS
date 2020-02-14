b6704646031ab1dcb6a123bd2b1522c85a1a3fa6
EoflaOE <eoflaoevicecity@gmail.com>
2020-02-14 19:10:33 +0200

    Added workaround for voice not working on Unix

--- Kernel Simulator/Misc/Speech/VoiceManagement.vb
+++ Kernel Simulator/Misc/Speech/VoiceManagement.vb
@@ -28,21 +28,33 @@ Public Module VoiceManagement
         Wdbg("I", "Headers required: {0}", SpeakReq.Headers.Count)
         SpeakReq.DownloadFile("http://translate.google.com/translate_tts?tl=en&q=" + Text + "&client=gtx", paths("Temp") + "/tts.mpeg")
 
-        'Use VLC for playback. Requires installing packages as specified in https://github.com/videolan/libvlcsharp/blob/3.x/docs/linux-setup.md
-        Core.Initialize()
-        Dim MLib As New LibVLC
-        Dim MP As New MediaPlayer(MLib)
-        AddHandler MLib.Log, Sub(sender, e) W($"{e.Level}@{e.Module}: {e.Message}", True, ColTypes.Neutral)
-        Dim MFile As New Media(MLib, paths("Temp") + "/tts.mpeg")
-        MP.Media = MFile
-        MP.Play()
-        While MP.State = VLCState.Playing
-        End While
+        'Use VLC for playback. Linux: Requires installing packages as specified in https://github.com/videolan/libvlcsharp/blob/3.x/docs/linux-setup.md
+        'Linux: Used process `cvlc` because of instabilities (not being able to play mpeg files) as a workaround. You still have to follow installation steps in the link above
+        If EnvironmentOSType.Contains("Unix") Then
+            Dim VLCProc As New Process()
+            Dim VLCProcS As New ProcessStartInfo("/usr/bin/cvlc", "--play-and-exit -vvvv " + paths("Temp") + "/tts.mpeg")
+            AddHandler VLCProc.OutputDataReceived, Sub(sender, e) W(e.Data, True, ColTypes.Neutral)
+            VLCProcS.UseShellExecute = False
+            VLCProcS.RedirectStandardOutput = True
+            VLCProc.StartInfo = VLCProcS
+            VLCProc.Start()
+            VLCProc.WaitForExit()
+        Else
+            Core.Initialize()
+            Dim MLib As New LibVLC
+            Dim MP As New MediaPlayer(MLib)
+            AddHandler MLib.Log, Sub(sender, e) W($"{e.Level}@{e.Module}: {e.Message}", True, ColTypes.Neutral)
+            Dim MFile As New Media(MLib, paths("Temp") + "/tts.mpeg")
+            MP.Media = MFile
+            MP.Play()
+            While MP.State = VLCState.Playing
+            End While
+            MFile.Dispose()
+            MP.Dispose()
+        End If
 
         'Dispose and close objects
         Wdbg("I", "Stopped.")
-        MFile.Dispose()
-        MP.Dispose()
 
         'Remove the file
         IO.File.Delete(paths("Temp") + "/tts.mpeg")
