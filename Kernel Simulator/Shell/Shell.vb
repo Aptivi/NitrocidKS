f2492ad851937ed3a396a85bbb052c63002c364e
EoflaOE <eoflaoevicecity@gmail.com>
2020-03-05 19:42:39 +0200

    Removed remaining `sses` command

--- Kernel Simulator/Shell/Shell.vb
+++ Kernel Simulator/Shell/Shell.vb
@@ -26,7 +26,7 @@ Public Module Shell
     Public availableCommands() As String = {"help", "logout", "list", "chdir", "cdir", "read", "shutdown", "reboot", "adduser", "chmotd",
                                             "chhostname", "showtd", "chpwd", "sysinfo", "arginj", "setcolors", "rmuser", "cls", "perm", "chusrname",
                                             "setthemes", "netinfo", "md", "rd", "debuglog", "reloadconfig", "showtdzone", "alias", "chmal",
-                                            "savescreen", "lockscreen", "setsaver", "reloadsaver", "ftp", "usermanual", "cdbglog", "sses", "chlang",
+                                            "savescreen", "lockscreen", "setsaver", "reloadsaver", "ftp", "usermanual", "cdbglog", "chlang",
                                             "reloadmods", "get", "lsdbgdev", "disconndbgdev", "lset", "move", "copy", "search", "listdrives",
                                             "listparts", "sumfile", "rdebug", "speak", "spellbee", "mathbee", "loteresp", "sshell", "bsynth", "shownotifs",
                                             "dismissnotif", "rexec", "calc", "update"}
