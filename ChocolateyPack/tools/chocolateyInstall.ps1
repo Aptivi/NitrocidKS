8d65a3998b32b13b76b535a3552d1a3f51a618ac
EoflaOE <eoflaoevicecity@gmail.com>
2020-03-22 17:28:13 +0200

    We know we're too fast, but, starting 0.0.9 development

--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
-$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.8.5-alpha/0.0.8.5-bin.rar"
+$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.9.0-alpha/0.0.9.0-bin.rar"
 $md5check   = "eea89fda4a2aa3dc7f935916d6729506"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
--- ChocolateyPack/tools/chocolateyuninstall.ps1
+++ ChocolateyPack/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.8.5-bin.rar"
+  zipFileName   = "0.0.9.0-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
