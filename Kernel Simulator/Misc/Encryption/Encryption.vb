cc83b9fc743c77d5db6582d8a3cdd75752dda9fe
EoflaOE <eoflaoevicecity@gmail.com>
2020-05-12 16:58:59 +0300

    Fixed mods not being able to access class: Encryption

--- Kernel Simulator/Misc/Encryption/Encryption.vb
+++ Kernel Simulator/Misc/Encryption/Encryption.vb
@@ -16,7 +16,7 @@
 '    You should have received a copy of the GNU General Public License
 '    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-Module Encryption
+Public Module Encryption
     Public Function GetArrayEnc(ByVal encrypted As Byte()) As String
         Dim hash As String = ""
         For i As Integer = 0 To encrypted.Length - 1
