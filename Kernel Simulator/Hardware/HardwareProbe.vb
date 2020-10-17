73e073818b04e66b9cd1c1fa4fd491a9806038de
EoflaOE <eoflaoevicecity@gmail.com>
2020-10-17 10:41:01 +0300

    Improved Linux partition probing
    Now, it doesn't show one partition for the boot disk; it shows all the mounted partitions in their own hard drive lines.

--- Kernel Simulator/Hardware/HardwareProbe.vb
+++ Kernel Simulator/Hardware/HardwareProbe.vb
@@ -253,7 +253,7 @@ Public Module HardwareProbe
             If File.Exists("/usr/bin/inxi") Then
                 Wdbg("I", "Inxi installed. Continuing...")
                 Dim inxi As New Process
-                Dim inxinfo As New ProcessStartInfo With {.FileName = "/usr/bin/inxi", .Arguments = "-P -D --output json --output-file print",
+                Dim inxinfo As New ProcessStartInfo With {.FileName = "/usr/bin/inxi", .Arguments = "-p -D --output json --output-file print",
                                                           .WindowStyle = ProcessWindowStyle.Hidden,
                                                           .CreateNoWindow = True,
                                                           .UseShellExecute = False,
@@ -271,6 +271,7 @@ Public Module HardwareProbe
                 'Get information about drives
                 Dim inxitoken As JToken = JToken.Parse(inxiout)
                 Dim inxiReady As Boolean = False
+                Dim DrvId As Integer = 0
                 For Each inxidrvs In inxitoken.SelectToken("000#Drives")
                     If inxiReady Then
                         Dim DriveSize As String = inxidrvs("004#size")
@@ -286,37 +287,44 @@ Public Module HardwareProbe
                             Wdbg("I", "Final drive model: {0}", DriveModel)
                         End If
                         HDDList.Add(New HDD_Linux With {.Size_LNX = DriveSize, .Model_LNX = DriveModel, .Vendor_LNX = DriveVendor})
+
+                        'Partition info
+                        Dim PartitionToken As JToken = inxitoken.SelectToken("001#Partition")
+                        If Not IsNothing(PartitionToken) Then
+                            For Each inxiparts In PartitionToken
+                                If IsNothing(inxiparts("006#dev")) Then
+                                    Dim DrvDevPath As String = inxiparts("005#dev").ToString
+                                    Dim TarDevPath As String = inxidrvs("001#ID").ToString
+                                    Dim DrvDevChar As Char
+                                    Dim CurrDrvChar As Char
+                                    Wdbg("I", "Drive path: {0}, Partition path: {1}", TarDevPath, DrvDevChar)
+
+                                    If DrvDevPath.ContainsAny({"hd", "sd", "vd"}) Then '/dev/hdX, /dev/sdX, /dev/vdX
+                                        Wdbg("I", "Drive seems to be one of /dev/(h|s|v)dXY format. {0}", DrvDevPath)
+                                        CurrDrvChar = DrvDevPath.Replace("/dev/sd", "").Replace("/dev/hd", "").Replace("/dev/vd", "").Chars(0)
+                                        DrvDevChar = TarDevPath.Replace("/dev/sd", "").Replace("/dev/hd", "").Replace("/dev/vd", "").Chars(0)
+                                    ElseIf DrvDevPath.Contains("mmcblk") Then '/dev/mmcblkXpY
+                                        Wdbg("I", "Drive seems to be one of /dev/mmcblkXpY format. {0}", DrvDevPath)
+                                        CurrDrvChar = DrvDevPath.Replace("/dev/mmcblk", "").Chars(0)
+                                        DrvDevChar = TarDevPath.Replace("/dev/mmcblk", "").Chars(0)
+                                    ElseIf DrvDevPath.Contains("nvme") Then '/dev/nvmeXnY
+                                        Wdbg("I", "Drive seems to be one of /dev/nvmeXnY format. {0}", DrvDevPath)
+                                        CurrDrvChar = DrvDevPath.Replace("/dev/nvme", "").Chars(0)
+                                        DrvDevChar = TarDevPath.Replace("/dev/nvme", "").Chars(0)
+                                    End If
+                                    Wdbg("I", "Drive char: {0}, Partition char: {1}", DrvDevChar, CurrDrvChar)
+
+                                    If CurrDrvChar = DrvDevChar Then
+                                        Wdbg("I", "Adding drive with arguments [.Part = {0}, .FileSystem = {1}, .SizeMEAS = {2}, .Used = {3}] to partition list of drive {4}...", DrvDevPath, inxiparts("004#fs"), inxiparts("002#size"), inxiparts("003#used"), DrvId)
+                                        HDDList(DrvId).Parts.Add(New Part_Linux With {.Part = DrvDevPath, .FileSystem = inxiparts("004#fs"), .SizeMEAS = inxiparts("002#size"), .Used = inxiparts("003#used")})
+                                    End If
+                                End If
+                            Next
+                        End If
+                        DrvId += 1
                     End If
                     inxiReady = True
                 Next
-
-                'Get information about partitions
-                Dim OldDrvChar As Char = "a"
-                Dim DrvId As Integer = 0
-                Dim PartitionToken As JToken = inxitoken.SelectToken("001#Partition")
-                If Not IsNothing(PartitionToken) Then
-                    For Each inxiparts In PartitionToken
-                        Dim DrvDevPath As String = inxiparts("005#dev").ToString
-                        Dim CurrDrvChar As Char
-
-                        Wdbg("I", "Prototype old / current dev path: {0} / {1}", OldDrvChar, CurrDrvChar)
-                        If DrvDevPath.ContainsAny({"hd", "sd", "vd"}) Then '/dev/hdX, /dev/sdX, /dev/vdX
-                            Wdbg("I", "Drive seems to be one of /dev/(h|s|v)dXY format. {0}", DrvDevPath)
-                            CurrDrvChar = DrvDevPath.Replace("/dev/sd", "").Replace("/dev/hd", "").Replace("/dev/vd", "").Chars(0)
-                        ElseIf DrvDevPath.Contains("mmcblk") Then '/dev/mmcblkXpY
-                            Wdbg("I", "Drive seems to be one of /dev/mmcblkXpY format. {0}", DrvDevPath)
-                            If Not OldDrvChar.ToString.IsNumeric Then OldDrvChar = "0"
-                            CurrDrvChar = DrvDevPath.Replace("/dev/mmcblk", "").Chars(0)
-                        End If
-                        If CurrDrvChar <> OldDrvChar Then
-                            Wdbg("I", "Transporting to another drive.")
-                            DrvId += 1
-                        End If
-
-                        Wdbg("I", "Adding drive with arguments [.Part = {0}, .FileSystem = {1}, .SizeMEAS = {2}, .Used = {3}] to partition list of drive {4}...", DrvDevPath, inxiparts("004#fs"), inxiparts("002#size"), inxiparts("003#used"), DrvId)
-                        HDDList(DrvId).Parts.Add(New Part_Linux With {.Part = DrvDevPath, .FileSystem = inxiparts("004#fs"), .SizeMEAS = inxiparts("002#size"), .Used = inxiparts("003#used")})
-                    Next
-                End If
             Else
                 HDDDone = False
                 Wdbg("E", "Inxi not installed. Hard drive probing failed.")
