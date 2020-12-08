ab748beef120930337fe682349fc0b8c7d3b63a0
EoflaOE <eoflaoevicecity@gmail.com>
2020-12-08 07:11:25 +0200

    Ready for final release of 0.0.12.3

--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
 $url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.12.3-alpha/0.0.12.3-bin.rar"
-$md5check   = "3c7e5ad444c3dec7d3eb9fe150c759c7"
+$md5check   = "cfad1423de72b2e3b00fc08d4afdfed0"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
