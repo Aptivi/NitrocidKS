Aptivi/NitrocidKS

commit 04d60b60424205f3b4cd4b59431b923dc5704f03

Rewritten shells to reflect latest CommandInfo
We'll test it later, and anything that gets whacky will be fixed.

Kernel Simulator/Misc/TextEdit/TextEditGetCommand.vb
@@ -28,8 +28,8 @@ Public Module TextEditGetCommand
 
     Sub TextEdit_ParseCommand(ByVal CommandText As String)
         Try
-            'Indicator if command finished
-            Dim CommandDone As Boolean
+            'Indicator if required arguments are provided
+            Dim RequiredArgumentsProvided As Boolean = True
 
             'Separate between command and arguments specified
             Dim Command As String = CommandText.Split(" ")(0)
@@ -44,6 +44,7 @@ Public Module TextEditGetCommand
                 For i As Integer = 0 To Arguments.Length - 1
                     Arguments(i).Replace("""", "")
                 Next
+                RequiredArgumentsProvided = Arguments?.Length >= TextEdit_Commands(Command).MinimumArguments
             End If
 
             'Remove first entry from array
@@ -60,16 +61,12 @@ Public Module TextEditGetCommand
                     Wdbg("I", "Requested help for all commands")
                     TextEdit_GetHelp()
                 End If
-                CommandDone = True
             ElseIf Command = "exit" Then
-                CommandDone = True
                 TextEdit_SaveTextFile(True)
                 TextEdit_Exiting = True
             ElseIf Command = "exitnosave" Then
-                CommandDone = True
                 TextEdit_Exiting = True
             ElseIf Command = "save" Then
-                CommandDone = True
                 TextEdit_SaveTextFile(False)
             ElseIf Command = "print" Then
                 Dim LineNumber As Integer = 1
@@ -99,9 +96,8 @@ Public Module TextEditGetCommand
                         LineNumber += 1
                     Next
                 End If
-                CommandDone = True
             ElseIf Command = "addline" Then
-                If Arguments?.Length > 0 Then
+                If RequiredArgumentsProvided Then
                     Dim NewLineContent As String = Arguments.Join(" ")
                     Wdbg("I", "Prototype new line content: {0}", NewLineContent)
                     If NewLineContent.StartsWith("""") And NewLineContent.EndsWith("""") Then
@@ -112,11 +108,9 @@ Public Module TextEditGetCommand
                     End If
                     Wdbg("I", "Final new line content: {0}", NewLineContent)
                     TextEdit_FileLines.Add(NewLineContent)
-                    CommandDone = True
                 End If
             ElseIf Command = "delline" Then
-                If Arguments?.Length > 0 Then
-                    CommandDone = True
+                If RequiredArgumentsProvided Then
                     Wdbg("I", "Is argument numeric: {0}", IsNumeric(Arguments(0)))
                     If IsNumeric(Arguments(0)) Then
                         Dim LineIndex As Integer = Arguments(0) - 1
@@ -135,8 +129,7 @@ Public Module TextEditGetCommand
                     End If
                 End If
             ElseIf Command = "replace" Then
-                If Arguments?.Length > 1 Then
-                    CommandDone = True
+                If RequiredArgumentsProvided Then
                     Wdbg("I", "Source: {0}, Target: {1}", Arguments(0), Arguments(1))
                     For LineIndex As Integer = 0 To TextEdit_FileLines.Count - 1
                         Wdbg("I", "Replacing ""{0}"" with ""{1}"" in line {2}", Arguments(0), Arguments(1), LineIndex + 1)
@@ -144,8 +137,7 @@ Public Module TextEditGetCommand
                     Next
                 End If
             ElseIf Command = "replaceinline" Then
-                If Arguments?.Length > 2 Then
-                    CommandDone = True
+                If RequiredArgumentsProvided Then
                     Wdbg("I", "Source: {0}, Target: {1}, Line Number: {2}", Arguments(0), Arguments(1), Arguments(2))
                     Wdbg("I", "File lines: {0}", TextEdit_FileLines.Count)
                     If Arguments(2).IsNumeric Then
@@ -162,8 +154,7 @@ Public Module TextEditGetCommand
                     End If
                 End If
             ElseIf Command = "delword" Then
-                If Arguments?.Length > 1 Then
-                    CommandDone = True
+                If RequiredArgumentsProvided Then
                     Wdbg("I", "Word/Phrase: {0}, Line: {1} ({2})", Arguments(0), Arguments(1), IsNumeric(Arguments(1)))
                     If IsNumeric(Arguments(1)) Then
                         Dim LineIndex As Integer = Arguments(1) - 1
@@ -181,8 +172,7 @@ Public Module TextEditGetCommand
                     End If
                 End If
             ElseIf Command = "delcharnum" Then
-                If Arguments?.Length > 1 Then
-                    CommandDone = True
+                If RequiredArgumentsProvided Then
                     Wdbg("I", "Char number: {0} ({1}), Line: {2} ({3})", Arguments(0), IsNumeric(Arguments(0)), Arguments(1), IsNumeric(Arguments(1)))
                     If IsNumeric(Arguments(1)) Then
                         Dim LineIndex As Integer = Arguments(1) - 1
@@ -202,8 +192,7 @@ Public Module TextEditGetCommand
                     End If
                 End If
             ElseIf Command = "querychar" Then
-                If Arguments?.Length > 1 Then
-                    CommandDone = True
+                If RequiredArgumentsProvided Then
                     Wdbg("I", "Char: {0}, Line: {1} ({2})", Arguments(0), Arguments(1), IsNumeric(Arguments(1)))
                     If IsNumeric(Arguments(1)) Then
                         Dim LineIndex As Integer = Arguments(1) - 1
@@ -236,7 +225,7 @@ Public Module TextEditGetCommand
             End If
 
             'See if the command is done (passed all required arguments)
-            If Not CommandDone Then
+            If TextEdit_Commands(Command).ArgumentsRequired And Not RequiredArgumentsProvided Then
                 W(DoTranslation("Required arguments are not passed to command {0}"), True, ColTypes.Err, Command)
                 Wdbg("E", "Passed arguments were not enough to run command {0}. Arguments passed: {1}", Command, Arguments.Length)
                 TextEdit_GetHelp(Command)

Kernel Simulator/Misc/ZipFile/ZipGetCommand.vb
@@ -29,8 +29,8 @@ Public Module ZipGetCommand
 
     Sub ZipShell_ParseCommand(ByVal CommandText As String)
         Try
-            'Indicator if command finished
-            Dim CommandDone As Boolean
+            'Indicator if required arguments are provided
+            Dim RequiredArgumentsProvided As Boolean = True
 
             'Separate between command and arguments specified
             Dim Command As String = CommandText.Split(" ")(0)
@@ -45,6 +45,7 @@ Public Module ZipGetCommand
                 For i As Integer = 0 To Arguments.Length - 1
                     Arguments(i).Replace("""", "")
                 Next
+                RequiredArgumentsProvided = Arguments?.Length >= ZipShell_Commands(Command).MinimumArguments
             End If
 
             'Remove first entry from array
@@ -61,12 +62,9 @@ Public Module ZipGetCommand
                     Wdbg("I", "Requested help for all commands")
                     ZipShell_GetHelp()
                 End If
-                CommandDone = True
             ElseIf Command = "exit" Then
-                CommandDone = True
                 ZipShell_Exiting = True
             ElseIf Command = "list" Then
-                CommandDone = True
                 Dim Entries As List(Of ZipArchiveEntry)
                 If Arguments?.Length > 0 Then
                     Wdbg("I", "Listing entries with {0} as target directory", Arguments(0))
@@ -79,8 +77,7 @@ Public Module ZipGetCommand
                     W("- {0}: ", False, ColTypes.ListEntry, Entry.FullName) : W("{0} ({0})", True, ColTypes.ListValue, Entry.CompressedLength.FileSizeToString, Entry.Length.FileSizeToString)
                 Next
             ElseIf Command = "get" Then
-                If Arguments?.Length > 0 Then
-                    CommandDone = True
+                If RequiredArgumentsProvided Then
                     Dim Where As String = ""
                     Dim Absolute As Boolean
                     If Arguments?.Length > 1 Then
@@ -92,15 +89,13 @@ Public Module ZipGetCommand
                     ExtractZipFileEntry(Arguments(0), Where, Absolute)
                 End If
             ElseIf Command = "chdir" Then
-                If Arguments?.Length > 0 Then
-                    CommandDone = True
+                If RequiredArgumentsProvided Then
                     If Not ChangeWorkingZipLocalDirectory(Arguments(0)) Then
                         W(DoTranslation("Directory {0} doesn't exist"), True, ColTypes.Err, Arguments(0))
                     End If
                 End If
             ElseIf Command = "chadir" Then
-                If Arguments?.Length > 0 Then
-                    CommandDone = True
+                If RequiredArgumentsProvided Then
                     If Not ChangeWorkingArchiveDirectory(Arguments(0)) Then
                         W(DoTranslation("Archive directory {0} doesn't exist"), True, ColTypes.Err, Arguments(0))
                     End If
@@ -108,7 +103,7 @@ Public Module ZipGetCommand
             End If
 
             'See if the command is done (passed all required arguments)
-            If Not CommandDone Then
+            If ZipShell_Commands(Command).ArgumentsRequired And Not RequiredArgumentsProvided Then
                 W(DoTranslation("Required arguments are not passed to command {0}"), True, ColTypes.Err, Command)
                 Wdbg("E", "Passed arguments were not enough to run command {0}. Arguments passed: {1}", Command, Arguments.Length)
                 ZipShell_GetHelp(Command)

Kernel Simulator/Network/FTP/FTPGetCommand.vb
@@ -31,12 +31,17 @@ Public Module FTPGetCommand
     ''' </summary>
     ''' <param name="cmd">A command. It may come with arguments</param>
     Public Sub ExecuteCommand(ByVal cmd As String)
+        'Variables
+        Dim RequiredArgumentsProvided As Boolean = True
 
+        'Get command and arguments
         Dim index As Integer = cmd.IndexOf(" ")
         If index = -1 Then index = cmd.Length
         Dim words = cmd.Split({" "c})
         Dim strArgs As String = cmd.Substring(index)
         If Not index = cmd.Length Then strArgs = strArgs.Substring(1)
+
+        'Parse arguments
         Dim ArgsQ() As String
         Dim TStream As New MemoryStream(Encoding.Default.GetBytes(strArgs))
         Dim Parser As New TextFieldParser(TStream) With {
@@ -48,12 +53,13 @@ Public Module FTPGetCommand
             For i As Integer = 0 To ArgsQ.Length - 1
                 ArgsQ(i).Replace("""", "")
             Next
+            RequiredArgumentsProvided = ArgsQ?.Length >= FTPCommands(Command).MinimumArguments
         End If
 
         'Command code
         Try
             If words(0) = "connect" Then
-                If ArgsQ?.Count <> 0 Then
+                If RequiredArgumentsProvided Then
                     If ArgsQ(0).StartsWith("ftp://") Or ArgsQ(0).StartsWith("ftps://") Or ArgsQ(0).StartsWith("ftpes://") Then
                         TryToConnect(ArgsQ(0))
                     Else
@@ -67,7 +73,7 @@ Public Module FTPGetCommand
             ElseIf words(0) = "changeremotedir" Or words(0) = "cdr" Then
                 FTPChangeRemoteDir(strArgs)
             ElseIf words(0) = "copy" Or words(0) = "cp" Then
-                If cmd <> "copy" Or cmd <> "cp" Then
+                If RequiredArgumentsProvided Then
                     If connected Then
                         W(DoTranslation("Copying {0} to {1}..."), True, ColTypes.Neutral, ArgsQ(0), ArgsQ(1))
                         If FTPCopyItem(ArgsQ(0), ArgsQ(1)) Then
@@ -90,7 +96,7 @@ Public Module FTPGetCommand
                     W(DoTranslation("You must connect to server before getting current remote directory."), True, ColTypes.Err)
                 End If
             ElseIf words(0) = "delete" Or words(0) = "del" Then
-                If cmd <> "delete" Or cmd <> "del" Then
+                If RequiredArgumentsProvided Then
                     If connected = True Then
                         'Print a message
                         W(DoTranslation("Deleting {0}..."), True, ColTypes.Neutral, strArgs)
@@ -127,7 +133,7 @@ Public Module FTPGetCommand
                     W(DoTranslation("You haven't connected to any server yet"), True, ColTypes.Err)
                 End If
             ElseIf words(0) = "download" Or words(0) = "get" Then
-                If cmd <> "download" Or cmd <> "get" Then
+                If RequiredArgumentsProvided Then
                     W(DoTranslation("Downloading file {0}..."), False, ColTypes.Neutral, strArgs)
                     If FTPGetFile(strArgs) Then
                         Console.WriteLine()
@@ -156,7 +162,7 @@ Public Module FTPGetCommand
                     W(Entry, True, ColTypes.Neutral)
                 Next
             ElseIf words(0) = "move" Or words(0) = "mv" Then
-                If cmd <> "move" Or cmd <> "mv" Then
+                If RequiredArgumentsProvided Then
                     If connected Then
                         W(DoTranslation("Moving {0} to {1}..."), True, ColTypes.Neutral, ArgsQ(0), ArgsQ(1))
                         If FTPMoveItem(ArgsQ(0), ArgsQ(1)) Then
@@ -177,7 +183,7 @@ Public Module FTPGetCommand
                     W(DoTranslation("You should disconnect from server before connecting to another server"), True, ColTypes.Err)
                 End If
             ElseIf words(0) = "upload" Or words(0) = "put" Then
-                If cmd <> "upload" Or cmd <> "put" Then
+                If RequiredArgumentsProvided Then
                     W(DoTranslation("Uploading file {0}..."), True, ColTypes.Neutral, strArgs)
 
                     'Begin the uploading process

commit d6441d8b72b10e7c284b63ec5fe30e307697f31a

Fixed some issues that commit 04d60b6 caused
There are some more issues that need to be fixed before the second dramatic change.

Kernel Simulator/Misc/TextEdit/TextEditGetCommand.vb
@@ -31,10 +31,21 @@ Public Module TextEditGetCommand
             'Indicator if required arguments are provided
             Dim RequiredArgumentsProvided As Boolean = True
 
+            'Get the index of the first space
+            Dim index As Integer = CommandText.IndexOf(" ")
+            If index = -1 Then index = CommandText.Length
+            Wdbg("I", "Index: {0}", index)
+
+            'Get the String Of arguments
+            Dim strArgs As String = CommandText.Substring(index)
+            Wdbg("I", "Prototype strArgs: {0}", strArgs)
+            If Not index = CommandText.Length Then strArgs = strArgs.Substring(1)
+            Wdbg("I", "Finished strArgs: {0}", strArgs)
+
             'Separate between command and arguments specified
             Dim Command As String = CommandText.Split(" ")(0)
             Dim Arguments() As String
-            Dim TStream As New MemoryStream(Encoding.Default.GetBytes(CommandText))
+            Dim TStream As New MemoryStream(Encoding.Default.GetBytes(strArgs))
             Dim Parser As New TextFieldParser(TStream) With {
                 .Delimiters = {" "},
                 .HasFieldsEnclosedInQuotes = True
@@ -45,12 +56,9 @@ Public Module TextEditGetCommand
                     Arguments(i).Replace("""", "")
                 Next
                 RequiredArgumentsProvided = Arguments?.Length >= TextEdit_Commands(Command).MinimumArguments
+            ElseIf TextEdit_Commands(Command).ArgumentsRequired And Arguments Is Nothing Then
+                RequiredArgumentsProvided = False
             End If
-
-            'Remove first entry from array
-            Dim ArgumentsList As List(Of String) = Arguments.ToList
-            ArgumentsList.Remove(Command)
-            Arguments = ArgumentsList.ToArray
 
             'Try to parse command
             If Command = "help" Then
@@ -227,7 +235,7 @@ Public Module TextEditGetCommand
             'See if the command is done (passed all required arguments)
             If TextEdit_Commands(Command).ArgumentsRequired And Not RequiredArgumentsProvided Then
                 W(DoTranslation("Required arguments are not passed to command {0}"), True, ColTypes.Err, Command)
-                Wdbg("E", "Passed arguments were not enough to run command {0}. Arguments passed: {1}", Command, Arguments.Length)
+                Wdbg("E", "Passed arguments were not enough to run command {0}. Arguments passed: {1}", Command, Arguments?.Length)
                 TextEdit_GetHelp(Command)
             End If
         Catch ex As Exception

Kernel Simulator/Misc/ZipFile/ZipGetCommand.vb
@@ -32,10 +32,21 @@ Public Module ZipGetCommand
             'Indicator if required arguments are provided
             Dim RequiredArgumentsProvided As Boolean = True
 
+            'Get the index of the first space
+            Dim index As Integer = CommandText.IndexOf(" ")
+            If index = -1 Then index = CommandText.Length
+            Wdbg("I", "Index: {0}", index)
+
+            'Get the String Of arguments
+            Dim strArgs As String = CommandText.Substring(index)
+            Wdbg("I", "Prototype strArgs: {0}", strArgs)
+            If Not index = CommandText.Length Then strArgs = strArgs.Substring(1)
+            Wdbg("I", "Finished strArgs: {0}", strArgs)
+
             'Separate between command and arguments specified
             Dim Command As String = CommandText.Split(" ")(0)
             Dim Arguments() As String
-            Dim TStream As New MemoryStream(Encoding.Default.GetBytes(CommandText))
+            Dim TStream As New MemoryStream(Encoding.Default.GetBytes(strArgs))
             Dim Parser As New TextFieldParser(TStream) With {
                 .Delimiters = {" "},
                 .HasFieldsEnclosedInQuotes = True
@@ -46,12 +57,9 @@ Public Module ZipGetCommand
                     Arguments(i).Replace("""", "")
                 Next
                 RequiredArgumentsProvided = Arguments?.Length >= ZipShell_Commands(Command).MinimumArguments
+            ElseIf ZipShell_Commands(Command).ArgumentsRequired And Arguments Is Nothing Then
+                RequiredArgumentsProvided = False
             End If
-
-            'Remove first entry from array
-            Dim ArgumentsList As List(Of String) = Arguments.ToList
-            ArgumentsList.Remove(Command)
-            Arguments = ArgumentsList.ToArray
 
             'Try to parse command
             If Command = "help" Then
@@ -105,7 +113,7 @@ Public Module ZipGetCommand
             'See if the command is done (passed all required arguments)
             If ZipShell_Commands(Command).ArgumentsRequired And Not RequiredArgumentsProvided Then
                 W(DoTranslation("Required arguments are not passed to command {0}"), True, ColTypes.Err, Command)
-                Wdbg("E", "Passed arguments were not enough to run command {0}. Arguments passed: {1}", Command, Arguments.Length)
+                Wdbg("E", "Passed arguments were not enough to run command {0}. Arguments passed: {1}", Command, Arguments?.Length)
                 ZipShell_GetHelp(Command)
             End If
         Catch ex As Exception

Kernel Simulator/Misc/ZipFile/ZipTools.vb
@@ -147,7 +147,7 @@ Public Module ZipTools
         If String.IsNullOrWhiteSpace(Target) Then Target = ZipShell_CurrentDirectory
         If Directory.Exists(NeutralizePath(Target, ZipShell_CurrentDirectory)) Then
             Wdbg("I", "{0} found. Changing...", Target)
-            ZipShell_CurrentArchiveDirectory = Target
+            ZipShell_CurrentDirectory = Target
             Return True
         Else
             Wdbg("E", "{0} not found.", Target)

Kernel Simulator/Network/FTP/FTPGetCommand.vb
@@ -53,7 +53,9 @@ Public Module FTPGetCommand
             For i As Integer = 0 To ArgsQ.Length - 1
                 ArgsQ(i).Replace("""", "")
             Next
-            RequiredArgumentsProvided = ArgsQ?.Length >= FTPCommands(Command).MinimumArguments
+            RequiredArgumentsProvided = ArgsQ?.Length >= FTPCommands(words(0)).MinimumArguments
+        ElseIf FTPCommands(words(0)).ArgumentsRequired And ArgsQ Is Nothing Then
+            RequiredArgumentsProvided = False
         End If
 
         'Command code

Kernel Simulator/Network/Mail/MailGetCommand.vb
@@ -45,7 +45,9 @@ Module MailGetCommand
             For i As Integer = 0 To FullArgsLQ.Length - 1
                 FullArgsLQ(i).Replace("""", "")
             Next
-            RequiredArgsProvided = FullArgsLQ?.Length >= Shell.Commands(cmd).MinimumArguments
+            RequiredArgsProvided = FullArgsLQ?.Length >= MailCommands(cmd).MinimumArguments
+        ElseIf MailCommands(cmd).ArgumentsRequired And FullArgsLQ Is Nothing Then
+            RequiredArgsProvided = False
         End If
         Wdbg("I", "Arguments with enclosed quotes count: {0}", FullArgsLQ?.Count)
 

Kernel Simulator/Network/SFTP/SFTPGetCommand.vb
@@ -31,13 +31,17 @@ Public Module SFTPGetCommand
     ''' </summary>
     ''' <param name="cmd">A command. It may come with arguments</param>
     Public Sub ExecuteCommand(ByVal cmd As String)
-
+        'Variables
         Dim RequiredArgumentsProvided As Boolean = True
+
+        'Get command and arguments
         Dim index As Integer = cmd.IndexOf(" ")
         If index = -1 Then index = cmd.Length
         Dim words = cmd.Split({" "c})
         Dim strArgs As String = cmd.Substring(index)
         If Not index = cmd.Length Then strArgs = strArgs.Substring(1)
+
+        'Parse arguments
         Dim ArgsQ() As String
         Dim TStream As New MemoryStream(Encoding.Default.GetBytes(strArgs))
         Dim Parser As New TextFieldParser(TStream) With {
@@ -49,7 +53,9 @@ Public Module SFTPGetCommand
             For i As Integer = 0 To ArgsQ.Length - 1
                 ArgsQ(i).Replace("""", "")
             Next
-            RequiredArgumentsProvided = ArgsQ?.Length >= SFTPCommands(words(0)).MinimumArguments
+            RequiredArgumentsProvided = ArgsQ?.Length >= FTPCommands(words(0)).MinimumArguments
+        ElseIf FTPCommands(words(0)).ArgumentsRequired And ArgsQ Is Nothing Then
+            RequiredArgumentsProvided = False
         End If
 
         'Command code

Kernel Simulator/Network/SFTP/SFTPShell.vb
@@ -29,7 +29,7 @@ Public Module SFTPShell
                                                                         {"cdl", New CommandInfo("cdl", ShellCommandType.SFTPShell, True, 1, False, False, False, False)},
                                                                         {"cdr", New CommandInfo("cdr", ShellCommandType.SFTPShell, True, 1, False, False, False, False)},
                                                                         {"delete", New CommandInfo("delete", ShellCommandType.SFTPShell, True, 1, False, False, False, False)},
-                                                                        {"del", New CommandInfo("del", ShellCommandType.SFTPShell, True, 2, False, False, False, False)},
+                                                                        {"del", New CommandInfo("del", ShellCommandType.SFTPShell, True, 1, False, False, False, False)},
                                                                         {"disconnect", New CommandInfo("disconnect", ShellCommandType.SFTPShell, False, 0, False, False, False, False)},
                                                                         {"download", New CommandInfo("download", ShellCommandType.SFTPShell, True, 1, False, False, False, False)},
                                                                         {"exit", New CommandInfo("exit", ShellCommandType.SFTPShell, False, 0, False, False, False, False)},

Kernel Simulator/Shell/GetCommand.vb
@@ -67,6 +67,8 @@ Public Module GetCommand
                 eqargs(i).Replace("""", "")
             Next
             RequiredArgumentsProvided = eqargs?.Length >= Shell.Commands(Command).MinimumArguments
+        ElseIf Shell.Commands(Command).ArgumentsRequired And eqargs Is Nothing Then
+            RequiredArgumentsProvided = False
         End If
 
         '4a. Debug: get all arguments from eqargs()
@@ -95,11 +97,11 @@ Public Module GetCommand
 
                 Case "adduser"
 
-                    If requestedCommand <> "adduser" Then
-                        If RequiredArgumentsProvided Then
+                    If RequiredArgumentsProvided Then
+                        If eqargs?.Length = 1 Then
                             W(DoTranslation("usrmgr: Creating username {0}..."), True, ColTypes.Neutral, eqargs(0))
                             AddUser(eqargs(0))
-                        ElseIf RequiredArgumentsProvided Then
+                        ElseIf eqargs?.Length > 2 Then
                             If eqargs(1) = eqargs(2) Then
                                 W(DoTranslation("usrmgr: Creating username {0}..."), True, ColTypes.Neutral, eqargs(0))
                                 AddUser(eqargs(0), eqargs(1))
@@ -111,14 +113,14 @@ Public Module GetCommand
 
                 Case "alias"
 
-                    If requestedCommand <> "alias" Then
-                        If RequiredArgumentsProvided Then
+                    If RequiredArgumentsProvided Then
+                        If eqargs?.Length > 3 Then
                             If eqargs(0) = "add" And (eqargs(1) = AliasType.Shell Or eqargs(1) = AliasType.RDebug Or eqargs(1) = AliasType.FTPShell Or eqargs(1) = AliasType.SFTPShell Or eqargs(1) = AliasType.MailShell) Then
                                 ManageAlias(eqargs(0), eqargs(1), eqargs(2), eqargs(3))
                             Else
                                 W(DoTranslation("Invalid type {0}."), True, ColTypes.Err, eqargs(1))
                             End If
-                        ElseIf RequiredArgumentsProvided Then
+                        ElseIf eqargs?.Length = 3 Then
                             If eqargs(0) = "rem" And (eqargs(1) = AliasType.Shell Or eqargs(1) = AliasType.RDebug Or eqargs(1) = AliasType.FTPShell Or eqargs(1) = AliasType.SFTPShell Or eqargs(1) = AliasType.MailShell) Then
                                 ManageAlias(eqargs(0), eqargs(1), eqargs(2))
                             Else
@@ -288,7 +290,7 @@ Public Module GetCommand
 
                 Case "chmotd"
 
-                    If RequiredArgumentsProvided Then
+                    If eqargs?.Length > 0 Then
                         If strArgs = "" Then
                             W(DoTranslation("Blank message of the day."), True, ColTypes.Err)
                         Else
@@ -299,12 +301,11 @@ Public Module GetCommand
                         InitializeTextShell(paths("Home") + "/MOTD.txt")
                         W(DoTranslation("Changing MOTD..."), True, ColTypes.Neutral)
                         ReadMOTDFromFile(MessageType.MOTD)
-
                     End If
 
                 Case "chmal"
 
-                    If RequiredArgumentsProvided Then
+                    If eqargs?.Length > 0 Then
                         If strArgs = "" Then
                             W(DoTranslation("Blank MAL After Login."), True, ColTypes.Err)
                         Else
@@ -463,7 +464,7 @@ Public Module GetCommand
                 Case "ftp"
 
                     Try
-                        If eqargs?.Length = 0 Then
+                        If eqargs?.Length = 0 Or IsNothing(eqargs) Then
                             InitiateShell()
                         Else
                             InitiateShell(True, eqargs(0))
@@ -887,7 +888,7 @@ Public Module GetCommand
                 Case "sftp"
 
                     Try
-                        If eqargs?.Length = 0 Then
+                        If eqargs?.Length = 0 Or IsNothing(eqargs) Then
                             SFTPInitiateShell()
                         Else
                             SFTPInitiateShell(True, eqargs(0))
@@ -1127,7 +1128,7 @@ Public Module GetCommand
 
                 Case "unzip"
 
-                    If RequiredArgumentsProvided Then
+                    If RequiredArgumentsProvided And eqargs?.Length = 1 Then
                         Dim ZipArchiveName As String = NeutralizePath(eqargs(0))
                         ZipFile.ExtractToDirectory(ZipArchiveName, CurrDir)
                     ElseIf RequiredArgumentsProvided And eqargs?.Length > 1 Then