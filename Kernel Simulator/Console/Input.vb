62b1dbe65c26d7a05a6560574425b01cfa66c925
EoflaOE <eoflaoevicecity@gmail.com>
2021-05-13 18:57:03 +0300

    Added the ReadLineUntil API

--- Kernel Simulator/Console/Input.vb
+++ Kernel Simulator/Console/Input.vb
@@ -63,4 +63,36 @@ Public Module Input
         Return Console.ReadKey(Intercept)
     End Function
 
+    ''' <summary>
+    ''' Reads the next line of characters until the condition is met or the user pressed ENTER
+    ''' </summary>
+    ''' <param name="Condition">The condition to be met</param>
+    Public Function ReadLineUntil(ByRef Condition As Boolean) As String
+        Dim Final As String = ""
+        Dim Finished As Boolean
+        While Not Finished
+            Dim KeyInfo As ConsoleKeyInfo
+            Dim KeyCharacter As Char
+            While Not Console.KeyAvailable
+                If Condition Then Finished = True
+                Threading.Thread.Sleep(1)
+            End While
+            KeyInfo = Console.ReadKey(True)
+            KeyCharacter = KeyInfo.KeyChar
+            If KeyCharacter = vbCr Or KeyCharacter = vbLf Then
+                Finished = True
+            ElseIf KeyInfo.Key = ConsoleKey.Backspace Then
+                If Not Final.Length = 0 Then
+                    Final = Final.Remove(Final.Length - 1)
+                    Console.Write(GetEsc() + "D") 'Cursor backwards by one character
+                    Console.Write(GetEsc() + "[1X") 'Remove a character
+                End If
+            Else
+                Final += KeyCharacter
+                Console.Write(KeyCharacter)
+            End If
+        End While
+        Return Final
+    End Function
+
 End Module
