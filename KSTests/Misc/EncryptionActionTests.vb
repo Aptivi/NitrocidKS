c01fa8f427eae58edf02ce4cf5d008d9534357c9
EoflaOE <eoflaoevicecity@gmail.com>
2021-05-23 13:53:31 +0300

    Added new APIs for hash verifier

--- KSTests/Misc/EncryptionActionTests.vb
+++ KSTests/Misc/EncryptionActionTests.vb
@@ -126,6 +126,56 @@ Imports KS
         End Try
     End Sub
 
+    ''' <summary>
+    ''' Tests hash verification for an uncalculated file
+    ''' </summary>
+    <TestMethod()> <TestCategory("Action")> Public Sub TestVerifyUncalculatedHashFromHash()
+        InitPaths()
+        Dim FileStreamHash As FileStream = File.Create(paths("Home") + "/TestSum.txt")
+        FileStreamHash.Write(Text.Encoding.Default.GetBytes("Test hashing with path."), 0, 23)
+        FileStreamHash.Flush()
+        FileStreamHash.Close()
+        Dim FileHashMD5 As String = GetEncryptedFile(paths("Home") + "/TestSum.txt", Algorithms.MD5)
+        Dim FileHashSHA1 As String = GetEncryptedFile(paths("Home") + "/TestSum.txt", Algorithms.SHA1)
+        Dim FileHashSHA256 As String = GetEncryptedFile(paths("Home") + "/TestSum.txt", Algorithms.SHA256)
+        Dim FileHashSHA512 As String = GetEncryptedFile(paths("Home") + "/TestSum.txt", Algorithms.SHA512)
+        Try
+            Dim ResultMD5 As Boolean = VerifyUncalculatedHashFromHash(paths("Home") + "/TestSum.txt", Algorithms.MD5, "CD5578C85A4CF32E48D157746A90C7F6")
+            Dim ResultSHA1 As Boolean = VerifyUncalculatedHashFromHash(paths("Home") + "/TestSum.txt", Algorithms.SHA1, "36EBF31AF7234D6C99CA65DC4EDA524161600657")
+            Dim ResultSHA256 As Boolean = VerifyUncalculatedHashFromHash(paths("Home") + "/TestSum.txt", Algorithms.SHA256, "7E6857729A34755DE8C2C9E535A8765BDE241F593BE3588B8FA6D29D949EFADA")
+            Dim ResultSHA512 As Boolean = VerifyUncalculatedHashFromHash(paths("Home") + "/TestSum.txt", Algorithms.SHA512, "6DF635C184D4B131B0243D4F2BD66925A61B82A5093F573920F42D7B8474D6332FD2886920F3CA36D9206C73DD59C8F1EEA18501E6FEF15FDDA664B1ABB0E361")
+            File.Delete(paths("Home") + "/TestSum.txt")
+            ResultMD5.ShouldBeTrue
+            ResultSHA1.ShouldBeTrue
+            ResultSHA256.ShouldBeTrue
+            ResultSHA512.ShouldBeTrue
+        Catch ex As Exception
+            Assert.Fail(ex.Message)
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' Tests hash verification from hashes file for an uncalculated file
+    ''' </summary>
+    <TestMethod()> <TestCategory("Action")> Public Sub TestVerifyUncalculatedHashFromFileStdFormat()
+        Dim FileHashMD5 As String = GetEncryptedFile(Environment.CurrentDirectory + "/TestText.txt", Algorithms.MD5)
+        Dim FileHashSHA1 As String = GetEncryptedFile(Environment.CurrentDirectory + "/TestText.txt", Algorithms.SHA1)
+        Dim FileHashSHA256 As String = GetEncryptedFile(Environment.CurrentDirectory + "/TestText.txt", Algorithms.SHA256)
+        Dim FileHashSHA512 As String = GetEncryptedFile(Environment.CurrentDirectory + "/TestText.txt", Algorithms.SHA512)
+        Try
+            Dim ResultMD5 As Boolean = VerifyUncalculatedHashFromHashesFile(Environment.CurrentDirectory + "/TestText.txt", Algorithms.MD5, Environment.CurrentDirectory + "/TestVerifyMD5.txt")
+            Dim ResultSHA1 As Boolean = VerifyUncalculatedHashFromHashesFile(Environment.CurrentDirectory + "/TestText.txt", Algorithms.SHA1, Environment.CurrentDirectory + "/TestVerifySHA1.txt")
+            Dim ResultSHA256 As Boolean = VerifyUncalculatedHashFromHashesFile(Environment.CurrentDirectory + "/TestText.txt", Algorithms.SHA256, Environment.CurrentDirectory + "/TestVerifySHA256.txt")
+            Dim ResultSHA512 As Boolean = VerifyUncalculatedHashFromHashesFile(Environment.CurrentDirectory + "/TestText.txt", Algorithms.SHA512, Environment.CurrentDirectory + "/TestVerifySHA512.txt")
+            ResultMD5.ShouldBeTrue
+            ResultSHA1.ShouldBeTrue
+            ResultSHA256.ShouldBeTrue
+            ResultSHA512.ShouldBeTrue
+        Catch ex As Exception
+            Assert.Fail(ex.Message)
+        End Try
+    End Sub
+
     <TestMethod> <TestCategory("Action")> Public Sub TestGetEmptyHash()
         GetEmptyHash(Algorithms.MD5).ShouldNotBeNullOrEmpty
         GetEmptyHash(Algorithms.SHA1).ShouldNotBeNullOrEmpty
