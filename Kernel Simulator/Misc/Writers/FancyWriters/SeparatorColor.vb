ef7968cbc6e771aa2187c85f54972585b5641f81
EoflaOE <eoflaoevicecity@gmail.com>
2021-06-11 17:31:29 +0300

    Fixed wrong number of closing minus sign on wrap command

--- Kernel Simulator/Misc/Writers/FancyWriters/SeparatorColor.vb
+++ Kernel Simulator/Misc/Writers/FancyWriters/SeparatorColor.vb
@@ -26,11 +26,24 @@ Public Module SeparatorColor
     ''' <param name="ColTypes">A type of colors that will be changed.</param>
     ''' <param name="Vars">Endless amounts of any variables that are separated by commas.</param>
     Public Sub WriteSeparator(ByVal Text As String, ByVal PrintSuffix As Boolean, ByVal ColTypes As ColTypes, ParamArray Vars() As Object)
+        'Print the suffix and the text
         If Not String.IsNullOrWhiteSpace(Text) Then
             If PrintSuffix Then Text = "- " + Text
             W(Text.Truncate(Console.WindowWidth - 6) + " ", False, ColTypes, Vars)
         End If
-        W("-".Repeat(Console.WindowWidth - Console.CursorLeft), True, ColTypes)
+
+        'See how many times to repeat the closing minus sign. We could be running this in the wrap command.
+        Dim RepeatTimes As Integer
+        If Not Console.CursorLeft = 0 Then
+            RepeatTimes = Console.WindowWidth - Console.CursorLeft
+        Else
+            RepeatTimes = Console.WindowWidth - (Text.Truncate(Console.WindowWidth - 6) + " ").Length
+        End If
+
+        'Write the closing minus sign.
+        W("-".Repeat(RepeatTimes), True, ColTypes)
+
+        'Fix CursorTop value on Unix systems. Mono...
         If IsOnUnix() Then
             If Not Console.CursorTop = Console.BufferHeight - 1 Then Console.CursorTop -= 1
         End If
@@ -44,11 +57,24 @@ Public Module SeparatorColor
     ''' <param name="Color">A color that will be changed to.</param>
     ''' <param name="Vars">Endless amounts of any variables that are separated by commas.</param>
     Public Sub WriteSeparatorC16(ByVal Text As String, ByVal PrintSuffix As Boolean, ByVal Color As ConsoleColor, ParamArray Vars() As Object)
+        'Print the suffix and the text
         If Not String.IsNullOrWhiteSpace(Text) Then
             If PrintSuffix Then Text = "- " + Text
             WriteC16(Text.Truncate(Console.WindowWidth - 6) + " ", False, Color, Vars)
         End If
-        WriteC16("-".Repeat(Console.WindowWidth - Console.CursorLeft), True, Color)
+
+        'See how many times to repeat the closing minus sign. We could be running this in the wrap command.
+        Dim RepeatTimes As Integer
+        If Not Console.CursorLeft = 0 Then
+            RepeatTimes = Console.WindowWidth - Console.CursorLeft
+        Else
+            RepeatTimes = Console.WindowWidth - (Text.Truncate(Console.WindowWidth - 6) + " ").Length
+        End If
+
+        'Write the closing minus sign.
+        WriteC16("-".Repeat(RepeatTimes), True, Color)
+
+        'Fix CursorTop value on Unix systems. Mono...
         If IsOnUnix() Then
             If Not Console.CursorTop = Console.BufferHeight - 1 Then Console.CursorTop -= 1
         End If
@@ -63,11 +89,24 @@ Public Module SeparatorColor
     ''' <param name="BackgroundColor">A background color that will be changed to.</param>
     ''' <param name="Vars">Endless amounts of any variables that are separated by commas.</param>
     Public Sub WriteSeparatorC16(ByVal Text As String, ByVal PrintSuffix As Boolean, ByVal ForegroundColor As ConsoleColor, ByVal BackgroundColor As ConsoleColor, ParamArray Vars() As Object)
+        'Print the suffix and the text
         If Not String.IsNullOrWhiteSpace(Text) Then
             If PrintSuffix Then Text = "- " + Text
             WriteC16(Text.Truncate(Console.WindowWidth - 6) + " ", False, ForegroundColor, BackgroundColor, Vars)
         End If
-        WriteC16("-".Repeat(Console.WindowWidth - Console.CursorLeft), True, ForegroundColor, BackgroundColor)
+
+        'See how many times to repeat the closing minus sign. We could be running this in the wrap command.
+        Dim RepeatTimes As Integer
+        If Not Console.CursorLeft = 0 Then
+            RepeatTimes = Console.WindowWidth - Console.CursorLeft
+        Else
+            RepeatTimes = Console.WindowWidth - (Text.Truncate(Console.WindowWidth - 6) + " ").Length
+        End If
+
+        'Write the closing minus sign.
+        WriteC16("-".Repeat(RepeatTimes), True, ForegroundColor, BackgroundColor)
+
+        'Fix CursorTop value on Unix systems. Mono...
         If IsOnUnix() Then
             If Not Console.CursorTop = Console.BufferHeight - 1 Then Console.CursorTop -= 1
         End If
@@ -81,11 +120,24 @@ Public Module SeparatorColor
     ''' <param name="Color">A color that will be changed to.</param>
     ''' <param name="Vars">Endless amounts of any variables that are separated by commas.</param>
     Public Sub WriteSeparatorC(ByVal Text As String, ByVal PrintSuffix As Boolean, ByVal Color As Color, ParamArray Vars() As Object)
+        'Print the suffix and the text
         If Not String.IsNullOrWhiteSpace(Text) Then
             If PrintSuffix Then Text = "- " + Text
             WriteC(Text.Truncate(Console.WindowWidth - 6) + " ", False, Color, Vars)
         End If
-        WriteC("-".Repeat(Console.WindowWidth - Console.CursorLeft), True, Color)
+
+        'See how many times to repeat the closing minus sign. We could be running this in the wrap command.
+        Dim RepeatTimes As Integer
+        If Not Console.CursorLeft = 0 Then
+            RepeatTimes = Console.WindowWidth - Console.CursorLeft
+        Else
+            RepeatTimes = Console.WindowWidth - (Text.Truncate(Console.WindowWidth - 6) + " ").Length
+        End If
+
+        'Write the closing minus sign.
+        WriteC("-".Repeat(RepeatTimes), True, Color)
+
+        'Fix CursorTop value on Unix systems. Mono...
         If IsOnUnix() Then
             If Not Console.CursorTop = Console.BufferHeight - 1 Then Console.CursorTop -= 1
         End If
@@ -100,11 +152,24 @@ Public Module SeparatorColor
     ''' <param name="BackgroundColor">A background color that will be changed to.</param>
     ''' <param name="Vars">Endless amounts of any variables that are separated by commas.</param>
     Public Sub WriteSeparatorC(ByVal Text As String, ByVal PrintSuffix As Boolean, ByVal ForegroundColor As Color, ByVal BackgroundColor As Color, ParamArray Vars() As Object)
+        'Print the suffix and the text
         If Not String.IsNullOrWhiteSpace(Text) Then
             If PrintSuffix Then Text = "- " + Text
             WriteC(Text.Truncate(Console.WindowWidth - 6) + " ", False, ForegroundColor, BackgroundColor, Vars)
         End If
-        WriteC("-".Repeat(Console.WindowWidth - Console.CursorLeft), True, ForegroundColor, BackgroundColor)
+
+        'See how many times to repeat the closing minus sign. We could be running this in the wrap command.
+        Dim RepeatTimes As Integer
+        If Not Console.CursorLeft = 0 Then
+            RepeatTimes = Console.WindowWidth - Console.CursorLeft
+        Else
+            RepeatTimes = Console.WindowWidth - (Text.Truncate(Console.WindowWidth - 6) + " ").Length
+        End If
+
+        'Write the closing minus sign.
+        WriteC("-".Repeat(RepeatTimes), True, ForegroundColor, BackgroundColor)
+
+        'Fix CursorTop value on Unix systems. Mono...
         If IsOnUnix() Then
             If Not Console.CursorTop = Console.BufferHeight - 1 Then Console.CursorTop -= 1
         End If
