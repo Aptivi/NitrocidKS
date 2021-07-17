b6cac0134719aefd6baf7d2669a6a1bfb2238d4c
EoflaOE <eoflaoevicecity@gmail.com>
2021-07-17 20:17:01 +0300

    Started development of 0.0.18.0

--- ChocolateyPack/tools/chocolateyuninstall.ps1
+++ ChocolateyPack/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.17.1-bin.rar"
+  zipFileName   = "0.0.18.0-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
