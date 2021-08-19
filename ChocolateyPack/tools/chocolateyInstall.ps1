43bc538b39bbff3d8c273b6e345bb8e7c8ab342e
EoflaOE <eoflaoevicecity@gmail.com>
2021-08-19 17:42:01 +0300

    Started development of 0.0.16.7

--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
-$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.16.6-alpha/0.0.16.6-bin.rar"
+$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.16.7-alpha/0.0.16.7-bin.rar"
 $md5check   = "4b655e70fb8fcd3442d6150ea96db6e8"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
