f6532899934cc621b8801a0af24d4808fe246a5d
EoflaOE <eoflaoevicecity@gmail.com>
2021-08-29 15:24:08 +0300

    Backported fix 3c0e166 from 0.0.19.0

--- Kernel Simulator/Files/Filesystem.vb
+++ Kernel Simulator/Files/Filesystem.vb
@@ -161,7 +161,11 @@ Public Module Filesystem
 
         'Append current directory to path
         If (IsOnWindows() And Not Path.Contains(":/")) Or (IsOnUnix() And Not Path.StartsWith("/")) Then
-            Path = $"{CurrDir}/{Path}"
+            If Not CurrDir.EndsWith("/") Then
+                Path = $"{CurrDir}/{Path}"
+            Else
+                Path = $"{CurrDir}{Path}"
+            End If
         End If
         Wdbg("I", "Prototype path: {0}", Path)
 
@@ -199,7 +203,11 @@ Public Module Filesystem
 
         'Append current directory to path
         If (IsOnWindows() And Not Path.Contains(":/")) Or (IsOnUnix() And Not Path.StartsWith("/")) Then
-            Path = $"{Source}/{Path}"
+            If Not CurrDir.EndsWith("/") Then
+                Path = $"{CurrDir}/{Path}"
+            Else
+                Path = $"{CurrDir}{Path}"
+            End If
         End If
         Wdbg("I", "Prototype path: {0}", Path)
 
