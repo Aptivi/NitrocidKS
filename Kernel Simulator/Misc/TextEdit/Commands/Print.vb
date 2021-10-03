c99c7a3518e2dc0063bd3037e031ccfb040fa1d7
EoflaOE <eoflaoevicecity@gmail.com>
2021-10-03 19:53:53 +0300

    Two number ranges are supported in text editor print

--- Kernel Simulator/Misc/TextEdit/Commands/Print.vb
+++ Kernel Simulator/Misc/TextEdit/Commands/Print.vb
@@ -16,6 +16,8 @@
 '    You should have received a copy of the GNU General Public License
 '    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+Imports Extensification.IntegerExts
+
 Class TextEdit_PrintCommand
     Inherits CommandExecutor
     Implements ICommand
@@ -23,22 +25,48 @@ Class TextEdit_PrintCommand
     Public Overrides Sub Execute(StringArgs As String, ListArgs() As String) Implements ICommand.Execute
         Dim LineNumber As Integer = 1
         If ListArgs?.Length > 0 Then
-            Wdbg(DebugLevel.I, "Line number provided: {0}", ListArgs(0))
-            Wdbg(DebugLevel.I, "Is it numeric? {0}", ListArgs(0).IsNumeric)
-            If ListArgs(0).IsNumeric Then
-                LineNumber = ListArgs(0)
-                Wdbg(DebugLevel.I, "File lines: {0}", TextEdit_FileLines.Count)
-                If CInt(ListArgs(0)) <= TextEdit_FileLines.Count Then
-                    Dim Line As String = TextEdit_FileLines(LineNumber - 1)
-                    Wdbg(DebugLevel.I, "Line number: {0} ({1})", LineNumber, Line)
-                    W("- {0}: ", False, ColTypes.ListEntry, LineNumber)
-                    W(Line, True, ColTypes.ListValue)
+            If ListArgs?.Length = 1 Then
+                'We've only provided one line number
+                Wdbg(DebugLevel.I, "Line number provided: {0}", ListArgs(0))
+                Wdbg(DebugLevel.I, "Is it numeric? {0}", ListArgs(0).IsNumeric)
+                If ListArgs(0).IsNumeric Then
+                    LineNumber = ListArgs(0)
+                    Wdbg(DebugLevel.I, "File lines: {0}", TextEdit_FileLines.Count)
+                    If CInt(ListArgs(0)) <= TextEdit_FileLines.Count Then
+                        Dim Line As String = TextEdit_FileLines(LineNumber - 1)
+                        Wdbg(DebugLevel.I, "Line number: {0} ({1})", LineNumber, Line)
+                        W("- {0}: ", False, ColTypes.ListEntry, LineNumber)
+                        W(Line, True, ColTypes.ListValue)
+                    Else
+                        W(DoTranslation("The specified line number may not be larger than the last file line number."), True, ColTypes.Error)
+                    End If
                 Else
-                    W(DoTranslation("The specified line number may not be larger than the last file line number."), True, ColTypes.Error)
+                    W(DoTranslation("Specified line number {0} is not a valid number."), True, ColTypes.Error, ListArgs(0))
+                    Wdbg(DebugLevel.E, "{0} is not a numeric value.", ListArgs(0))
                 End If
             Else
-                W(DoTranslation("Specified line number {0} is not a valid number."), True, ColTypes.Error, ListArgs(0))
-                Wdbg(DebugLevel.E, "{0} is not a numeric value.", ListArgs(0))
+                'We've provided two line numbers in the range
+                Wdbg(DebugLevel.I, "Line numbers provided: {0}, {1}", ListArgs(0), ListArgs(1))
+                Wdbg(DebugLevel.I, "Is it numeric? {0}", ListArgs(0).IsNumeric, ListArgs(1).IsNumeric)
+                If ListArgs(0).IsNumeric And ListArgs(1).IsNumeric Then
+                    Dim LineNumberStart As Integer = ListArgs(0)
+                    Dim LineNumberEnd As Integer = ListArgs(1)
+                    LineNumberStart.SwapIfSourceLarger(LineNumberEnd)
+                    Wdbg(DebugLevel.I, "File lines: {0}", TextEdit_FileLines.Count)
+                    If LineNumberStart <= TextEdit_FileLines.Count And LineNumberEnd <= TextEdit_FileLines.Count Then
+                        For LineNumber = LineNumberStart To LineNumberEnd
+                            Dim Line As String = TextEdit_FileLines(LineNumber - 1)
+                            Wdbg(DebugLevel.I, "Line number: {0} ({1})", LineNumber, Line)
+                            W("- {0}: ", False, ColTypes.ListEntry, LineNumber)
+                            W(Line, True, ColTypes.ListValue)
+                        Next
+                    Else
+                        W(DoTranslation("The specified line number may not be larger than the last file line number."), True, ColTypes.Error)
+                    End If
+                Else
+                    W(DoTranslation("Specified line number {0} is not a valid number."), True, ColTypes.Error, ListArgs(0))
+                    Wdbg(DebugLevel.E, "{0} is not a numeric value.", ListArgs(0))
+                End If
             End If
         Else
             For Each Line As String In TextEdit_FileLines
