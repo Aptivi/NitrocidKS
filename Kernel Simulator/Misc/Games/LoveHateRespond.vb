b5606524bcc6fa3dd364ff77f4bbb9150ac39e4b
EoflaOE <eoflaoevicecity@gmail.com>
2021-10-17 17:45:48 +0300

    Improved the LoveHateRespond game
    1. Action for hate comments is now apologize instead of report. Because that assumes the player can't handle criticism.
    2. Each user is now assigned the type of comment: the love comment or the hate comment.
    3. Score system has been implemented. If you thank a lover or apologize to a hater, you gain a point. If you thank a hater or apologize to a lover, you lose a point.
    4. Made the prompt more modern so that it fits the design of the second-generation KS.
    5. Added a tip regarding how to play the game.
    6. Changed the shell command of the game from "loteresp" to "lovehate" for a variety of reasons:
      - "loteresp" sounds bland, dull, and generally bad.
      - It's difficult to understand what the name stood for unless you look at its help entry; it's oversimplified.
      - "lovehate" sounds better than "loteresp."

--- Kernel Simulator/Misc/Games/LoveHateRespond.vb
+++ Kernel Simulator/Misc/Games/LoveHateRespond.vb
@@ -43,53 +43,90 @@ Module LoveHateRespond
                                                        DoTranslation("Stop making this kind of video!!!"),
                                                        DoTranslation("Get back to your job, your videos are the worst!"),
                                                        DoTranslation("We prejudice on this video.")}
-    ReadOnly Comments As New Dictionary(Of String, List(Of String)) From {{"Love", LoveComments}, {"Hate", HateComments}}
-    ReadOnly Users As New List(Of String) From {"GS4L", "The Eagle", "Vercity", "Losting - Computers, fixes, and more", "WillStrike", "CyberBully #2095", "ProGamer453",
-                                                "ExtFS", "Elaine Stretch", "NFSMW2005", "WhatsUp", "BSearch", "Wellington Marks", "CD-OS", "LinuxUser348", "Speculate Ltd.",
-                                                "The Matrix"}
+    ReadOnly Comments As New Dictionary(Of String, List(Of String)) From {{CommentType.Love, LoveComments}, {CommentType.Hate, HateComments}}
+    ReadOnly Users As New Dictionary(Of String, CommentType) From {{"John Williams", Rnd()}, {"The Eagle", Rnd()},
+                                                                   {"Jim Walker", Rnd()}, {"Alex", Rnd()},
+                                                                   {"Will Philips", Rnd()}, {"Elaine McCann", Rnd()},
+                                                                   {"ProGamer453", Rnd()}, {"Rajput Singh", Rnd()},
+                                                                   {"Abihshek Chaudhary", Rnd()}, {"Johnny Alfonso", Rnd()},
+                                                                   {"Bruce Fitzgerald", Rnd()}, {"Mark Adams", Rnd()},
+                                                                   {"Wellington Marks", Rnd()}, {"CD-OS", Rnd()},
+                                                                   {"LinuxUser348", Rnd()}, {"Suspicion Ltd.", Rnd()}}
+
+    Enum CommentType
+        ''' <summary>
+        ''' A love comment
+        ''' </summary>
+        Love
+        ''' <summary>
+        ''' A hate comment
+        ''' </summary>
+        Hate
+    End Enum
 
     ''' <summary>
     ''' Initializes the game
     ''' </summary>
-    Sub InitializeLoteresp()
+    Sub InitializeLoveHate()
         Dim RandomDriver As New Random()
-        Dim RandomUser, RandomComment, Type, Response As String
-        While True
-            Type = Comments.ElementAt(RandomDriver.Next(Comments.Keys.Count)).Key
-            RandomUser = Users.ElementAt(RandomDriver.Next(Users.Count))
+        Dim RandomUser, RandomComment, Response As String
+        Dim Type As CommentType
+        Dim ExitRequested As Boolean
+        Dim Score, CommentNumber As Long
+        W(DoTranslation("Press A on hate comments to apologize. Press T on love comments to thank. Press Q to quit the game."), True, ColTypes.Tip)
+
+        'Game logic
+        While Not ExitRequested
+            'Set necessary variables
+            RandomUser = Users.Keys.ElementAt(RandomDriver.Next(Users.Keys.Count))
+            Type = Users(RandomUser)
             RandomComment = Comments(Type).ElementAt(RandomDriver.Next(Comments(Type).Count))
+            CommentNumber += 1
             Wdbg(DebugLevel.I, "Comment type: {0}", Type)
             Wdbg(DebugLevel.I, "Commenter: {0}", RandomUser)
             Wdbg(DebugLevel.I, "Comment: {0}", RandomComment)
+
+            'Ask the user the question
+            WriteSeparator("[{0}/{1}]", True, Score, CommentNumber)
             W(DoTranslation("If someone made this comment to your video:"), True, ColTypes.Neutral)
-            W("{0}: {1}", True, ColTypes.Neutral, RandomUser, RandomComment)
-            W(DoTranslation("How would you respond?") + " <R/T/Q> ", False, ColTypes.Input)
+            W("- {0}:", False, ColTypes.ListEntry, RandomUser)
+            W(" {0}", True, ColTypes.ListValue, RandomComment)
+            W(DoTranslation("How would you respond?") + " <A/T/Q> ", False, ColTypes.Input)
             Response = Console.ReadKey.KeyChar
             Console.WriteLine()
             Wdbg(DebugLevel.I, "Response: {0}", Response)
-            If Response = "R" Or Response = "r" Then
-                If Type = "Hate" Then
-                    Wdbg(DebugLevel.I, "Reported hate comment")
-                    W(DoTranslation("Great! {0}'s comment will be removed for that."), True, ColTypes.Neutral, RandomUser)
-                Else
-                    Wdbg(DebugLevel.I, "Reported love comment")
-                    W(DoTranslation("You just reported the love comment!"), True, ColTypes.Neutral)
-                End If
-            ElseIf Response = "T" Or Response = "t" Then
-                If Type = "Love" Then
-                    Wdbg(DebugLevel.I, "Thanked love comment")
-                    W(DoTranslation("Great! {0} will appreciate your thanks."), True, ColTypes.Neutral, RandomUser)
-                Else
-                    Wdbg(DebugLevel.I, "Thanked hate comment")
-                    W(DoTranslation("You just replied to the hate comment!"), True, ColTypes.Neutral)
-                End If
-            ElseIf Response = "Q" Or Response = "q" Then
-                Wdbg(DebugLevel.I, "Exit requested")
-                Exit Sub
-            Else
-                Wdbg(DebugLevel.I, "No such selection")
-                W(DoTranslation("Invalid selection. Going to the next comment..."), True, ColTypes.Error)
-            End If
+
+            'Parse response
+            Select Case Response
+                Case "A", "a" 'Apologize
+                    Select Case Type
+                        Case CommentType.Love
+                            Wdbg(DebugLevel.I, "Apologized to love comment")
+                            W(DoTranslation("Apologized to love comment. Not good enough."), True, ColTypes.Neutral)
+                            Score -= 1
+                        Case CommentType.Hate
+                            Wdbg(DebugLevel.I, "Apologized to hate comment")
+                            W(DoTranslation("You've apologized to a hate comment! Excellent!"), True, ColTypes.Neutral)
+                            Score += 1
+                    End Select
+                Case "T", "t" 'Thank
+                    Select Case Type
+                        Case CommentType.Love
+                            Wdbg(DebugLevel.I, "Thanked love comment")
+                            W(DoTranslation("Great! {0} will appreciate your thanks."), True, ColTypes.Neutral, RandomUser)
+                            Score += 1
+                        Case CommentType.Hate
+                            Wdbg(DebugLevel.I, "Thanked hate comment")
+                            W(DoTranslation("You just thanked the hater for the hate comment!"), True, ColTypes.Neutral)
+                            Score -= 1
+                    End Select
+                Case "Q", "q" 'Quit
+                    Wdbg(DebugLevel.I, "Exit requested")
+                    ExitRequested = True
+                Case Else
+                    Wdbg(DebugLevel.I, "No such selection")
+                    W(DoTranslation("Invalid selection. Going to the next comment..."), True, ColTypes.Error)
+            End Select
         End While
     End Sub
 
--- Kernel Simulator/Shell/Commands/LoveHate.vb
+++ Kernel Simulator/Shell/Commands/LoveHate.vb
@@ -16,12 +16,12 @@
 '    You should have received a copy of the GNU General Public License
 '    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-Class LoteRespCommand
+Class LoveHateCommand
     Inherits CommandExecutor
     Implements ICommand
 
     Public Overrides Sub Execute(StringArgs As String, ListArgs() As String, ListArgsOnly As String(), ListSwitchesOnly As String()) Implements ICommand.Execute
-        InitializeLoteresp()
+        InitializeLoveHate()
     End Sub
 
 End Class
