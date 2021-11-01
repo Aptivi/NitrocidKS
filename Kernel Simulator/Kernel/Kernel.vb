08c7fb0d719321e4950a7bb96e3138f471251e0b
EoflaOE <eoflaoevicecity@gmail.com>
2021-11-01 14:17:10 +0200

    -- Notify Milestone 3 --

--- Kernel Simulator/Kernel/Kernel.vb
+++ Kernel Simulator/Kernel/Kernel.vb
@@ -27,7 +27,7 @@ Public Module Kernel
     Public EventManager As New Events
     Public DefConsoleOut As TextWriter
     Public ReadOnly KernelVersion As String = GetExecutingAssembly().GetName().Version.ToString()
-    Public ReadOnly ConsoleTitle As String = $"Kernel Simulator v{KernelVersion} - Milestone 2"
+    Public ReadOnly ConsoleTitle As String = $"Kernel Simulator v{KernelVersion} - Milestone 3"
     Public ReadOnly vbNewLine As String = Environment.NewLine
     Friend StageTimer As New Stopwatch
 
