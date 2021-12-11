074f2afda471c8b2c6d3983634b1cd9fe226b9fb
EoflaOE <eoflaoevicecity@gmail.com>
2021-12-11 15:56:58 +0200

    Debugging the color wheel

--- Kernel Simulator/Console/ColorWheelOpen.vb
+++ Kernel Simulator/Console/ColorWheelOpen.vb
@@ -77,6 +77,8 @@ Public Module ColorWheelOpen
         Dim CurrentColorB As Integer = DefaultColorB
         Dim CurrentRange As Char = "R"
         Dim ColorWheelExiting As Boolean
+        Wdbg(DebugLevel.I, "Got color (R;G;B: {0};{1};{2})", CurrentColorR, CurrentColorG, CurrentColorB)
+        Wdbg(DebugLevel.I, "Got color ({0})", CurrentColor)
 
         Console.CursorVisible = False
         While Not ColorWheelExiting
@@ -85,10 +87,12 @@ Public Module ColorWheelOpen
                 Write(vbNewLine + DoTranslation("Select color using ""<-"" and ""->"" keys. Press ENTER to quit. Press ""i"" to insert color number manually."), True, ColTypes.Tip)
                 Write(DoTranslation("Press ""t"" to switch to 255 color mode."), True, ColTypes.Tip)
                 Write(DoTranslation("Press ""c"" to write full color code."), True, ColTypes.Tip)
+                Wdbg(DebugLevel.I, "Current Range: {0}", CurrentRange)
 
                 'The red color level
                 Dim RedForeground As Color = If(CurrentRange = "R", New Color(ConsoleColors.Black), New Color("255;0;0"))
                 Dim RedBackground As Color = If(CurrentRange = "R", New Color("255;0;0"), New Color(ConsoleColors.Black))
+                Wdbg(DebugLevel.I, "Red foreground: {0} | Red background: {1}", RedForeground.PlainSequence, RedBackground.PlainSequence)
                 Write(vbNewLine + "  ", False, ColTypes.Neutral)
                 Write(" < ", False, RedForeground, RedBackground)
                 WriteWhere("R: {0}", (Console.CursorLeft + 35 - $"R: {CurrentColorR}".Length) / 2, Console.CursorTop, True, New Color($"{CurrentColorR};0;0"), CurrentColorR)
@@ -97,6 +101,7 @@ Public Module ColorWheelOpen
                 'The green color level
                 Dim GreenForeground As Color = If(CurrentRange = "G", New Color(ConsoleColors.Black), New Color("0;255;0"))
                 Dim GreenBackground As Color = If(CurrentRange = "G", New Color("0;255;0"), New Color(ConsoleColors.Black))
+                Wdbg(DebugLevel.I, "Green foreground: {0} | Green background: {1}", GreenForeground.PlainSequence, GreenBackground.PlainSequence)
                 Write(vbNewLine + "  ", False, ColTypes.Neutral)
                 Write(" < ", False, GreenForeground, GreenBackground)
                 WriteWhere("G: {0}", (Console.CursorLeft + 35 - $"G: {CurrentColorG}".Length) / 2, Console.CursorTop, True, New Color($"0;{CurrentColorG};0"), CurrentColorG)
@@ -105,6 +110,7 @@ Public Module ColorWheelOpen
                 'The blue color level
                 Dim BlueForeground As Color = If(CurrentRange = "B", New Color(ConsoleColors.Black), New Color("0;0;255"))
                 Dim BlueBackground As Color = If(CurrentRange = "B", New Color("0;0;255"), New Color(ConsoleColors.Black))
+                Wdbg(DebugLevel.I, "Blue foreground: {0} | Blue background: {1}", BlueForeground.PlainSequence, BlueBackground.PlainSequence)
                 Write(vbNewLine + "  ", False, ColTypes.Neutral)
                 Write(" < ", False, BlueForeground, BlueBackground)
                 WriteWhere("B: {0}", (Console.CursorLeft + 35 - $"B: {CurrentColorB}".Length) / 2, Console.CursorTop, True, New Color($"0;0;{CurrentColorB}"), CurrentColorB)
@@ -125,49 +131,65 @@ Public Module ColorWheelOpen
 
                 'Read and get response
                 Dim ConsoleResponse As ConsoleKeyInfo = Console.ReadKey(True)
+                Wdbg(DebugLevel.I, "Keypress: {0}", ConsoleResponse.Key.ToString)
                 If ConsoleResponse.Key = ConsoleKey.LeftArrow Then
+                    Wdbg(DebugLevel.I, "Decrementing number...")
                     Select Case CurrentRange
                         Case "R"
                             If CurrentColorR = 0 Then
+                                Wdbg(DebugLevel.I, "Reached zero! Back to 255.")
                                 CurrentColorR = 255
                             Else
                                 CurrentColorR -= 1
+                                Wdbg(DebugLevel.I, "Decremented to {0}", CurrentColorR)
                             End If
                         Case "G"
                             If CurrentColorG = 0 Then
+                                Wdbg(DebugLevel.I, "Reached zero! Back to 255.")
                                 CurrentColorG = 255
                             Else
                                 CurrentColorG -= 1
+                                Wdbg(DebugLevel.I, "Decremented to {0}", CurrentColorG)
                             End If
                         Case "B"
                             If CurrentColorB = 0 Then
+                                Wdbg(DebugLevel.I, "Reached zero! Back to 255.")
                                 CurrentColorB = 255
                             Else
                                 CurrentColorB -= 1
+                                Wdbg(DebugLevel.I, "Decremented to {0}", CurrentColorB)
                             End If
                     End Select
                 ElseIf ConsoleResponse.Key = ConsoleKey.RightArrow Then
+                    Wdbg(DebugLevel.I, "Incrementing number...")
                     Select Case CurrentRange
                         Case "R"
                             If CurrentColorR = 255 Then
+                                Wdbg(DebugLevel.I, "Reached 255! Back to zero.")
                                 CurrentColorR = 0
                             Else
                                 CurrentColorR += 1
+                                Wdbg(DebugLevel.I, "Incremented to {0}", CurrentColorR)
                             End If
                         Case "G"
                             If CurrentColorG = 255 Then
+                                Wdbg(DebugLevel.I, "Reached 255! Back to zero.")
                                 CurrentColorG = 0
                             Else
                                 CurrentColorG += 1
+                                Wdbg(DebugLevel.I, "Incremented to {0}", CurrentColorG)
                             End If
                         Case "B"
                             If CurrentColorB = 255 Then
+                                Wdbg(DebugLevel.I, "Reached 255! Back to zero.")
                                 CurrentColorB = 0
                             Else
                                 CurrentColorB += 1
+                                Wdbg(DebugLevel.I, "Incremented to {0}", CurrentColorB)
                             End If
                     End Select
                 ElseIf ConsoleResponse.Key = ConsoleKey.UpArrow Then
+                    Wdbg(DebugLevel.I, "Changing range...")
                     Select Case CurrentRange
                         Case "R"
                             CurrentRange = "B"
@@ -177,6 +199,7 @@ Public Module ColorWheelOpen
                             CurrentRange = "G"
                     End Select
                 ElseIf ConsoleResponse.Key = ConsoleKey.DownArrow Then
+                    Wdbg(DebugLevel.I, "Changing range...")
                     Select Case CurrentRange
                         Case "R"
                             CurrentRange = "G"
@@ -186,6 +209,7 @@ Public Module ColorWheelOpen
                             CurrentRange = "R"
                     End Select
                 ElseIf ConsoleResponse.Key = ConsoleKey.I Then
+                    Wdbg(DebugLevel.I, "Prompting for color number...")
                     Dim _DefaultColor As Integer
                     Select Case CurrentRange
                         Case "R"
@@ -199,14 +223,20 @@ Public Module ColorWheelOpen
                     Console.CursorVisible = True
                     Dim ColorNum As String = Console.ReadLine
                     Console.CursorVisible = False
+                    Wdbg(DebugLevel.I, "Got response: {0}", ColorNum)
                     If IsNumeric(ColorNum) Then
+                        Wdbg(DebugLevel.I, "Numeric! Checking range...")
                         If ColorNum >= 0 And ColorNum <= 255 Then
+                            Wdbg(DebugLevel.I, "In range!")
                             Select Case CurrentRange
                                 Case "R"
+                                    Wdbg(DebugLevel.I, "Changing red color level to {0}...", ColorNum)
                                     CurrentColorR = ColorNum
                                 Case "G"
+                                    Wdbg(DebugLevel.I, "Changing green color level to {0}...", ColorNum)
                                     CurrentColorG = ColorNum
                                 Case "B"
+                                    Wdbg(DebugLevel.I, "Changing blue color level to {0}...", ColorNum)
                                     CurrentColorB = ColorNum
                             End Select
                         End If
@@ -217,6 +247,7 @@ Public Module ColorWheelOpen
                     Dim ColorSequence As String = Console.ReadLine
                     Console.CursorVisible = False
                     Try
+                        Wdbg(DebugLevel.I, "Parsing {0}...", ColorSequence)
                         Dim ParsedColor As New Color(ColorSequence)
                         CurrentColorR = ParsedColor.R
                         CurrentColorG = ParsedColor.G
@@ -226,8 +257,10 @@ Public Module ColorWheelOpen
                         Wdbg(DebugLevel.E, "Possible input error: {0} ({1})", ColorSequence, ex.Message)
                     End Try
                 ElseIf ConsoleResponse.Key = ConsoleKey.T Then
+                    Wdbg(DebugLevel.I, "Switching back to 255 color...")
                     TrueColor = False
                 ElseIf ConsoleResponse.Key = ConsoleKey.Enter Then
+                    Wdbg(DebugLevel.I, "Exiting...")
                     ColorWheelExiting = True
                 End If
             Else
@@ -244,31 +277,43 @@ Public Module ColorWheelOpen
 
                 'Read and get response
                 Dim ConsoleResponse As ConsoleKeyInfo = Console.ReadKey(True)
+                Wdbg(DebugLevel.I, "Keypress: {0}", ConsoleResponse.Key.ToString)
                 If ConsoleResponse.Key = ConsoleKey.LeftArrow Then
+                    Wdbg(DebugLevel.I, "Decrementing number...")
                     If CurrentColor = 0 Then
+                        Wdbg(DebugLevel.I, "Reached zero! Back to 255.")
                         CurrentColor = 255
                     Else
                         CurrentColor -= 1
+                        Wdbg(DebugLevel.I, "Decremented to {0}", CurrentColor)
                     End If
                 ElseIf ConsoleResponse.Key = ConsoleKey.RightArrow Then
                     If CurrentColor = 255 Then
+                        Wdbg(DebugLevel.I, "Reached 255! Back to zero.")
                         CurrentColor = 0
                     Else
                         CurrentColor += 1
+                        Wdbg(DebugLevel.I, "Incremented to {0}", CurrentColor)
                     End If
                 ElseIf ConsoleResponse.Key = ConsoleKey.I Then
+                    Wdbg(DebugLevel.I, "Prompting for color number...")
                     WriteWhere(DoTranslation("Enter color number from 0 to 255:") + " [{0}] ", 0, Console.WindowHeight - 1, False, ColTypes.Input, CInt(CurrentColor))
                     Console.CursorVisible = True
                     Dim ColorNum As String = Console.ReadLine
                     Console.CursorVisible = False
+                    Wdbg(DebugLevel.I, "Got response: {0}", ColorNum)
                     If IsNumeric(ColorNum) Then
+                        Wdbg(DebugLevel.I, "Numeric! Checking range...")
                         If ColorNum >= 0 And ColorNum <= 255 Then
+                            Wdbg(DebugLevel.I, "In range! Changing color level to {0}...", ColorNum)
                             CurrentColor = ColorNum
                         End If
                     End If
                 ElseIf ConsoleResponse.Key = ConsoleKey.T Then
+                    Wdbg(DebugLevel.I, "Switching back to 255 color...")
                     TrueColor = True
                 ElseIf ConsoleResponse.Key = ConsoleKey.Enter Then
+                    Wdbg(DebugLevel.I, "Exiting...")
                     ColorWheelExiting = True
                 End If
             End If
