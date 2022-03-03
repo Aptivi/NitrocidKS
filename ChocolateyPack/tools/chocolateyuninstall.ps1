a910985d9bc1b70588e308816921f91c17a86e88
EoflaOE <eoflaoevicecity@gmail.com>
2022-03-03 06:58:46 +0200

    Started development of 0.0.8.10

--- ChocolateyPack/tools/chocolateyuninstall.ps1
+++ ChocolateyPack/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.8.9-bin.rar"
+  zipFileName   = "0.0.8.10-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
