be8964906fb600cafd1c68e6f57cc6e30b2464db
EoflaOE <eoflaoevicecity@gmail.com>
2022-04-05 17:41:22 +0300

    Updated version to update debug symbol link

--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
-$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.8.10-alpha/0.0.8.10-bin.rar"
-$md5check   = "96dd5faa0d19d1794f6ac2bbb80ffba5"
+$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.8.11-alpha/0.0.8.11-bin.rar"
+$md5check   = "9c27710fd5107370160fb254cc1ca98a"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
--- ChocolateyPack/tools/chocolateyuninstall.ps1
+++ ChocolateyPack/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.8.10-bin.rar"
+  zipFileName   = "0.0.8.11-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
