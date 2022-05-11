2ed9108a0875c519ef29b4313932941e65f31ce8
EoflaOE <eoflaoevicecity@gmail.com>
2022-05-11 06:56:04 +0300

    Backported improvement 1ea0f1a

--- Kernel Simulator/Network/SSH/SSH.vb
+++ Kernel Simulator/Network/SSH/SSH.vb
@@ -36,7 +36,7 @@ Module SSH
 
         'Wait until disconnection
         While SSH.IsConnected
-            If Console.ReadKey(True).Key = ConsoleKey.Escape Then
+            If Console.ReadKey(True).Key = ConsoleKey.Escape Or SSHS.GetType.GetField("_input", Reflection.BindingFlags.NonPublic Or Reflection.BindingFlags.Instance).GetValue(SSHS) Is Nothing Then
                 SSHS.Stop()
                 SSH.Disconnect()
             End If
