bf102ae88f94fbae9e32315bce3c8da9e57307f9
EoflaOE <eoflaoevicecity@gmail.com>
2022-05-11 06:55:17 +0300

    Started development of 0.0.8.12

--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
-$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.8.11-alpha/0.0.8.11-bin.rar"
+$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.8.12-alpha/0.0.8.12-bin.rar"
 $md5check   = "9c27710fd5107370160fb254cc1ca98a"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
--- ChocolateyPack/tools/chocolateyuninstall.ps1
+++ ChocolateyPack/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.8.11-bin.rar"
+  zipFileName   = "0.0.8.12-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
