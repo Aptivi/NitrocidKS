f0492c03319c2d71b7a95b9f88b42f1311a60305
EoflaOE <eoflaoevicecity@gmail.com>
2022-05-29 18:42:39 +0300

    Don't print actual key char if said char is control char
    We don't want to corrupt the output.

--- Kernel Simulator/Shell/Commands/KeyInfo.vb
+++ Kernel Simulator/Shell/Commands/KeyInfo.vb
@@ -24,14 +24,26 @@ Namespace Shell.Commands
         Public Overrides Sub Execute(StringArgs As String, ListArgs() As String, ListArgsOnly As String(), ListSwitchesOnly As String()) Implements ICommand.Execute
             Write(DoTranslation("Enter a key or a combination of keys to display its information."), True, ColTypes.Neutral)
             Dim KeyPress As ConsoleKeyInfo = Console.ReadKey(True)
+
+            'Pressed key
             Write("- " + DoTranslation("Pressed key") + ": ", False, ColTypes.ListEntry)
             Write(KeyPress.Key.ToString, True, ColTypes.ListValue)
-            Write("- " + DoTranslation("Pressed key character") + ": ", False, ColTypes.ListEntry)
-            Write(KeyPress.KeyChar, True, ColTypes.ListValue)
+
+            'If the pressed key is a control key, don't write the actual key char so as not to corrupt the output
+            If Not Char.IsControl(KeyPress.KeyChar) Then
+                Write("- " + DoTranslation("Pressed key character") + ": ", False, ColTypes.ListEntry)
+                Write(KeyPress.KeyChar, True, ColTypes.ListValue)
+            End If
+
+            'Pressed key character code
             Write("- " + DoTranslation("Pressed key character code") + ": ", False, ColTypes.ListEntry)
             Write($"0x{Convert.ToInt32(KeyPress.KeyChar):X2} [{Convert.ToInt32(KeyPress.KeyChar)}]", True, ColTypes.ListValue)
+
+            'Pressed modifiers
             Write("- " + DoTranslation("Pressed modifiers") + ": ", False, ColTypes.ListEntry)
             Write(KeyPress.Modifiers.ToString, True, ColTypes.ListValue)
+
+            'Keyboard shortcut
             Write("- " + DoTranslation("Keyboard shortcut") + ": ", False, ColTypes.ListEntry)
             Write($"{String.Join(" +", KeyPress.Modifiers.ToString.Split(", "))} + {KeyPress.Key}", True, ColTypes.ListValue)
         End Sub
