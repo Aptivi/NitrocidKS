21286ff594217acca283b9b9aed2d0418ce79156
EoflaOE <eoflaoevicecity@gmail.com>
2022-06-10 15:06:52 +0300

    Started development of 0.0.18.6

--- Kernel Simulator/My Project/AssemblyInfo.vb
+++ Kernel Simulator/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.18.5")>
-<Assembly: AssemblyFileVersion("1.0.18.5")>
+<Assembly: AssemblyVersion("0.0.18.6")>
+<Assembly: AssemblyFileVersion("1.0.18.6")>
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")>
