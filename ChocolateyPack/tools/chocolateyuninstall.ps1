3ad56d06562c28d1d64622b858950e542fc2567d
EoflaOE <eoflaoevicecity@gmail.com>
2022-06-12 08:12:37 +0300

    Started development of 0.0.22.1

--- ChocolateyPack/tools/chocolateyuninstall.ps1
+++ ChocolateyPack/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.22.0-bin.rar"
+  zipFileName   = "0.0.22.1-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
