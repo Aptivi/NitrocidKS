1c4c94d10a285464041343ef2ce1ef5f9da3ac88
EoflaOE <eoflaoevicecity@gmail.com>
2022-06-28 07:08:57 +0300

    Fixed build failure on .NET 6.0 version of KS

--- Kernel Simulator/Console/Input.vb
+++ Kernel Simulator/Console/Input.vb
@@ -16,6 +16,10 @@
 '    You should have received a copy of the GNU General Public License
 '    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#If NETCOREAPP Then
+Imports System.Threading
+#End If
+
 Namespace ConsoleBase
     Public Module Input
 
