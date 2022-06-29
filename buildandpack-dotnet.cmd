45135c008924126edf313b54ea626911c6ae99c1
EoflaOE <eoflaoevicecity@gmail.com>
2022-06-29 06:31:10 +0300

    Added build and pack scripts for .NET

---
+++ buildandpack-dotnet.cmd
@@ -0,0 +1,93 @@
+@echo off
+
+REM    Kernel Simulator  Copyright (C) 2018-2021  EoflaOE
+REM
+REM    This file is part of Kernel Simulator
+REM
+REM    Kernel Simulator is free software: you can redistribute it and/or modify
+REM    it under the terms of the GNU General Public License as published by
+REM    the Free Software Foundation, either version 3 of the License, or
+REM    (at your option) any later version.
+REM
+REM    Kernel Simulator is distributed in the hope that it will be useful,
+REM    but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM    GNU General Public License for more details.
+REM
+REM    You should have received a copy of the GNU General Public License
+REM    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+REM This script builds KS and packs the artifacts. Use when you have VS installed.
+set ksversion=0.0.23.0
+
+echo Make sure you have the following:
+echo   - %ProgramFiles%\dotnet\dotnet.exe (SDK)
+echo   - %ProgramFiles%\WinRAR\rar.exe
+echo   - %ProgramFiles%\7-Zip\7z.exe
+echo   - %ProgramFiles(x86)%\GnuWin32\bin\gzip.exe
+echo.
+echo Press any key to start.
+pause > nul
+
+:download
+echo Downloading packages...
+"%ProgramFiles%\dotnet\dotnet.exe" msbuild "KS.DotNetSdk.sln" -t:restore -p:Configuration=Release-dotnet > %temp%/buildandpack.log 2>&1
+if %errorlevel% == 0 goto :build
+echo There was an error trying to download packages (%errorlevel%).
+goto :finished
+
+:build
+echo Building Kernel Simulator...
+"%ProgramFiles%\dotnet\dotnet.exe" msbuild "KS.DotNetSdk.sln" -p:Configuration=Release-dotnet >> %temp%/buildandpack.log 2>&1
+if %errorlevel% == 0 goto :packbin
+echo There was an error trying to build (%errorlevel%).
+goto :finished
+
+:packbin
+echo Packing binary...
+"%ProgramFiles%\WinRAR\rar.exe" a -ep1 -r -m5 %temp%/%ksversion%-bin-dotnet.rar "Kernel Simulator\KSBuild\net6.0\" >> %temp%/buildandpack.log 2>&1
+if %errorlevel% == 0 goto :packsrc
+echo There was an error trying to pack binary (%errorlevel%).
+goto :finished
+
+:packsrc
+echo Binary artifact found in root directory.
+echo Packing source...
+rmdir /S /Q "Kernel Simulator\KSBuild\" >> %temp%/buildandpack.log 2>&1
+rmdir /S /Q "Kernel Simulator\obj\" >> %temp%/buildandpack.log 2>&1
+rmdir /S /Q "KSTests\KSTest" >> %temp%/buildandpack.log 2>&1
+rmdir /S /Q "KSTests\obj\" >> %temp%/buildandpack.log 2>&1
+rmdir /S /Q "KSJsonifyLocales\obj\" >> %temp%/buildandpack.log 2>&1
+rmdir /S /Q "KSConverter\obj\" >> %temp%/buildandpack.log 2>&1
+rmdir /S /Q "DocGen\api\" >> %temp%/buildandpack.log 2>&1
+rmdir /S /Q "DocGen\obj\" >> %temp%/buildandpack.log 2>&1
+rmdir /S /Q "docs\" >> %temp%/buildandpack.log 2>&1
+echo Packing source using rar...
+"%ProgramFiles%\WinRAR\rar.exe" a -ep1 -r -m5 -x.git -x.vs %temp%/%ksversion%-src-dotnet.rar >> %temp%/buildandpack.log 2>&1
+if %errorlevel% == 0 goto :packsrctar
+echo There was an error trying to pack source using rar (%errorlevel%).
+goto :finished
+
+:packsrctar
+echo Packing source using tar...
+"%ProgramFiles%\7-Zip\7z.exe" a -ttar %temp%/%ksversion%-src-dotnet.tar -xr!.git -xr!.vs >> %temp%/buildandpack.log 2>&1
+if %errorlevel% == 0 goto :compresstar
+echo There was an error trying to pack source using tar (%errorlevel%).
+goto :finished
+
+:compresstar
+echo Compressing tar using gzip...
+"%ProgramFiles(x86)%\GnuWin32\bin\gzip.exe" -9 %temp%/%ksversion%-src-dotnet.tar >> %temp%/buildandpack.log 2>&1
+if %errorlevel% == 0 goto :complete
+echo There was an error trying to compress tar (%errorlevel%).
+goto :finished
+
+:complete
+move %temp%\%ksversion%-bin-dotnet.rar >> %temp%/buildandpack.log 2>&1
+move %temp%\%ksversion%-src-dotnet.rar >> %temp%/buildandpack.log 2>&1
+move %temp%\%ksversion%-src-dotnet.tar.gz >> %temp%/buildandpack.log 2>&1
+echo Build and pack successful.
+goto :finished
+
+:finished
+pause
---
+++ buildandpack-dotnet.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+#    Kernel Simulator  Copyright (C) 2018-2021  EoflaOE
+#
+#    This file is part of Kernel Simulator
+#
+#    Kernel Simulator is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Kernel Simulator is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# This script builds KS and packs the artifacts. Use when you have MSBuild installed.
+ksversion=0.0.23.0
+
+mkdir ~/tmp
+echo Make sure you have the following:
+echo "  - dotnet (from Microsoft repos)"
+echo 
+echo Press any key to start.
+read -n 1
+
+# Check for dependencies
+dotnetpath=`which dotnet`
+if [ ! $? == 0 ]; then
+	echo dotnet is not found.
+	exit 1
+fi
+gzippath=`which gzip`
+if [ ! $? == 0 ]; then
+	echo gzip is not found.
+	exit 1
+fi
+tarpath=`which tar`
+if [ ! $? == 0 ]; then
+	echo tar is not found.
+	exit 1
+fi
+rarpath=`which rar`
+if [ ! $? == 0 ]; then
+	echo rar is not found.
+	exit 1
+fi
+
+# Download packages
+echo Downloading packages...
+"$dotnetpath" msbuild "KS.DotNetSdk.sln" -t:restore -p:Configuration=Release-dotnet > ~/tmp/buildandpack.log
+if [ ! $? == 0 ]; then
+	echo Download failed.
+	exit 1
+fi
+
+# Build KS
+echo Building KS...
+"$dotnetpath" msbuild "KS.DotNetSdk.sln" -p:Configuration=Release-dotnet >> ~/tmp/buildandpack.log
+if [ ! $? == 0 ]; then
+	echo Build failed.
+	exit 1
+fi
+
+# Pack binary
+echo Packing binary...
+"$rarpath" a -ep1 -r -m5 ~/tmp/$ksversion-bin-dotnet.rar "Kernel Simulator/KSBuild/net6.0/" >> ~/tmp/buildandpack.log
+if [ ! $? == 0 ]; then
+	echo Packing using rar failed.
+	exit 1
+fi
+
+# Pack source
+echo Packing source...
+rm -rf "Kernel Simulator/KSBuild" >> ~/tmp/buildandpack.log
+rm -rf "Kernel Simulator/obj" >> ~/tmp/buildandpack.log
+rm -rf "KSTests/KSTest" >> ~/tmp/buildandpack.log
+rm -rf "KSTests/obj" >> ~/tmp/buildandpack.log
+rm -rf "KSJsonifyLocales/obj" >> ~/tmp/buildandpack.log
+rm -rf "KSConverter/obj" >> ~/tmp/buildandpack.log
+rm -rf "DocGen/api" >> ~/tmp/buildandpack.log
+rm -rf "DocGen/obj" >> ~/tmp/buildandpack.log
+rm -rf "docs" >> ~/tmp/buildandpack.log
+"$rarpath" a -ep1 -r -m5 -x.git -x.vs ~/tmp/$ksversion-src-dotnet.rar >> ~/tmp/buildandpack.log
+if [ ! $? == 0 ]; then
+	echo Packing source using rar failed.
+	exit 1
+fi
+
+# Pack source using tar
+echo Packing source using tar...
+"$tarpath" --exclude-vcs -cf ~/tmp/$ksversion-src-dotnet.tar . >> ~/tmp/buildandpack.log
+if [ ! $? == 0 ]; then
+	echo Packing source using tar failed.
+	exit 1
+fi
+
+# Compress source
+echo Compressing source...
+"$gzippath" -9 ~/tmp/$ksversion-src-dotnet.tar >> ~/tmp/buildandpack.log
+if [ ! $? == 0 ]; then
+	echo Compressing source failed.
+	exit 1
+fi
+
+# Inform success
+mv ~/tmp/$ksversion-bin-dotnet.rar .
+mv ~/tmp/$ksversion-src-dotnet.rar .
+mv ~/tmp/$ksversion-src-dotnet.tar.gz .
+echo Build and pack successful.
+exit 0
