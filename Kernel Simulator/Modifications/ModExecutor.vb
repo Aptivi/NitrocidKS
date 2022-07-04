12e973a1ece0200943fe98280b9d3de3ca988aab
EoflaOE <eoflaoevicecity@gmail.com>
2022-07-04 08:53:00 +0300

    ModExecutor is no longer visible since it contains no public...
    ...methods.

--- Kernel Simulator/Modifications/ModExecutor.vb
+++ Kernel Simulator/Modifications/ModExecutor.vb
@@ -17,7 +17,7 @@
 '    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Namespace Modifications
-    Public Module ModExecutor
+    Module ModExecutor
 
         ''' <summary>
         ''' Executes the command provided by a mod
