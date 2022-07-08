f12527e2c0717d8511fe64f43dbafa6c6b3fd3cd
EoflaOE <eoflaoevicecity@gmail.com>
2022-07-08 11:47:18 +0300

    Finalized 0.0.22.5

--- ChocolateyPack-DotNet/tools/chocolateyInstall.ps1
+++ ChocolateyPack-DotNet/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
 $url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.22.5-beta/0.0.22.5-bin-dotnet.rar"
-$md5check   = "42add39d182bc4998a0d5eb81119e7a8"
+$md5check   = "313a8dd900bfbafcdc5f7f4e8410f358"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
