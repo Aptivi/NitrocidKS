6452dbb201839809c7846faea0173b6b78cd907f
EoflaOE <eoflaoevicecity@gmail.com>
2022-07-11 11:30:07 +0300

    Started development of 0.0.23.1

--- ChocolateyPack-DotNet/tools/chocolateyuninstall.ps1
+++ ChocolateyPack-DotNet/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.23.0-bin.rar"
+  zipFileName   = "0.0.23.1-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
-$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.23.0-beta/0.0.23.0-bin.rar"
+$url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.23.1-beta/0.0.23.1-bin.rar"
 $md5check   = "165d9f1381f99388f74c34b03dcf55ef"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
