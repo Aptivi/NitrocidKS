77bf4dcca6a11230239cd03ca8e1ed071117dbe2
EoflaOE <eoflaoevicecity@gmail.com>
2022-07-28 09:57:19 +0300

    Added the meteor shooter!

--- Kernel Simulator/Shell/Commands/Meteor.vb
+++ Kernel Simulator/Shell/Commands/Meteor.vb
@@ -16,13 +16,15 @@
 '    You should have received a copy of the GNU General Public License
 '    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+Imports KS.Misc.Games
+
 Namespace Shell.Commands
     Class MeteorCommand
         Inherits CommandExecutor
         Implements ICommand
 
         Public Overrides Sub Execute(StringArgs As String, ListArgs() As String, ListArgsOnly As String(), ListSwitchesOnly As String()) Implements ICommand.Execute
-            Write("Meteor TBD", True, ColTypes.Warning)
+            InitializeMeteor()
         End Sub
 
     End Class
