0164eab901e66600a777b413325c5e82a4717ed2
EoflaOE <eoflaoevicecity@gmail.com>
2022-08-15 09:28:54 +0300

    Synchronized namespaces in KSTests

---
+++ KSTests/ConsoleTests/ConsoleColorsInfoInitializationTests.cs
@@ -0,0 +1,69 @@
+﻿
+// Kernel Simulator  Copyright (C) 2018-2022  Aptivi
+// 
+// This file is part of Kernel Simulator
+// 
+// Kernel Simulator is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Kernel Simulator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using KS.ConsoleBase.Colors;
+using NUnit.Framework;
+using Shouldly;
+
+namespace KSTests.ConsoleTests
+{
+
+    [TestFixture]
+    public class ConsoleColorsInfoInitializationTests
+    {
+
+        /// <summary>
+        /// Tests initializing an instance of ConsoleColorsInfo from a bright color
+        /// </summary>
+        [Test]
+        [Description("Initialization")]
+        public void TestInitializeConsoleColorsInfoInstanceBright()
+        {
+            // Create instance
+            var ConsoleColorsInfoInstance = new ConsoleColorsInfo(ConsoleColors.Grey85);
+
+            // Check for property correctness
+            ConsoleColorsInfoInstance.ColorID.ShouldBe(253);
+            ConsoleColorsInfoInstance.R.ShouldBe(218);
+            ConsoleColorsInfoInstance.G.ShouldBe(218);
+            ConsoleColorsInfoInstance.B.ShouldBe(218);
+            ConsoleColorsInfoInstance.IsBright.ShouldBeTrue();
+            ConsoleColorsInfoInstance.IsDark.ShouldBeFalse();
+        }
+
+        /// <summary>
+        /// Tests initializing an instance of ConsoleColorsInfo from a dark color
+        /// </summary>
+        [Test]
+        [Description("Initialization")]
+        public void TestInitializeConsoleColorsInfoInstanceDark()
+        {
+            // Create instance
+            var ConsoleColorsInfoInstance = new ConsoleColorsInfo(ConsoleColors.Grey11);
+
+            // Check for property correctness
+            ConsoleColorsInfoInstance.ColorID.ShouldBe(234);
+            ConsoleColorsInfoInstance.R.ShouldBe(28);
+            ConsoleColorsInfoInstance.G.ShouldBe(28);
+            ConsoleColorsInfoInstance.B.ShouldBe(28);
+            ConsoleColorsInfoInstance.IsBright.ShouldBeFalse();
+            ConsoleColorsInfoInstance.IsDark.ShouldBeTrue();
+        }
+
+    }
+}
---
+++ KSTests/LanguagesTests/LocalizationInitializationTests.cs
@@ -0,0 +1,82 @@
+﻿
+// Kernel Simulator  Copyright (C) 2018-2022  Aptivi
+// 
+// This file is part of Kernel Simulator
+// 
+// Kernel Simulator is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Kernel Simulator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Linq;
+using KS.Languages;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace KSTests.LanguagesTests
+{
+
+    [TestFixture]
+    public class LocalizationInitializationTests
+    {
+
+        /// <summary>
+        /// Tests creating the new instance of the language information
+        /// </summary>
+        [Test]
+        [Description("Initialization")]
+        public void TestCreateNewLanguageInfoInstance()
+        {
+            var InfoInstance = new LanguageInfo("arb", "Arabic", true);
+
+            // Check for null
+            InfoInstance.ShouldNotBeNull();
+            InfoInstance.LanguageResource.ShouldNotBeNull();
+            InfoInstance.Cultures.ShouldNotBeNull();
+
+            // Check for property correctness
+            InfoInstance.Transliterable.ShouldBeTrue();
+            InfoInstance.Custom.ShouldBeFalse();
+            InfoInstance.FullLanguageName.ShouldBe("Arabic");
+            InfoInstance.ThreeLetterLanguageName.ShouldBe("arb");
+            InfoInstance.Cultures.ShouldNotBeEmpty();
+        }
+
+        /// <summary>
+        /// Tests translation dictionary preparation for a language
+        /// </summary>
+        [Test]
+        [Description("Initialization")]
+        public void TestPrepareDictForOneLanguage()
+        {
+            int ExpectedLength = JObject.Parse(KS.My.Resources.Resources.spa).SelectToken("Localizations").Count();
+            int ActualLength = Translate.PrepareDict("spa").Values.Count;
+            ActualLength.ShouldBe(ExpectedLength);
+        }
+
+        /// <summary>
+        /// Tests translation dictionary preparation for all languages
+        /// </summary>
+        [Test]
+        [Description("Initialization")]
+        public void TestPrepareDictForAllLanguages()
+        {
+            foreach (string Lang in LanguageManager.Languages.Keys)
+            {
+                int ExpectedLength = JObject.Parse(KS.My.Resources.Resources.ResourceManager.GetString(Lang.Replace("-", "_"))).SelectToken("Localizations").Count();
+                int ActualLength = Translate.PrepareDict(Lang).Values.Count;
+                ActualLength.ShouldBe(ExpectedLength, $"Lang: {Lang}");
+            }
+        }
+
+    }
+}
