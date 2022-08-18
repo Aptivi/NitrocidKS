c7ac19f3fe02449538585ab54ccc39354d2240d6
EoflaOE <eoflaoevicecity@gmail.com>
2022-08-18 20:36:46 +0300

    Fixed formatting of Reboot command

--- Kernel Simulator/Shell/Shells/UESH/Commands/Reboot.vb
+++ Kernel Simulator/Shell/Shells/UESH/Commands/Reboot.vb
@@ -26,7 +26,6 @@ Namespace Shell.Shells.UESH.Commands
     ''' This command restarts your simulated kernel and reloads all the config that are not loaded using reloadconfig. This is especially useful if you want to change colors of text, set arguments into the kernel, inject arguments, and so on.
     ''' <br></br>
     ''' > [!WARNING]
-    ''' <br></br>
     ''' > There is no file system syncing because the current kernel version doesn't have the real file system to sync, and the kernel is not final.
     ''' </remarks>
     Class RebootCommand
