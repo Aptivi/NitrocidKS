79a87b78dbec190ed34987eae0066348c2cacd15
EoflaOE <eoflaoevicecity@gmail.com>
2022-08-21 12:20:28 +0300

    -- Notify Milestone 2 --

--- Kernel Simulator/Kernel/Kernel.cs
+++ Kernel Simulator/Kernel/Kernel.cs
@@ -83,7 +83,7 @@ namespace KS.Kernel
         public readonly static string ConsoleTitle = $"Kernel Simulator v{KernelVersion} - {KernelSimulatorMoniker} - Release Candidate";
 #elif SPECIFIERDEV
 #if MILESTONESPECIFIERALPHA
-        public readonly static string ConsoleTitle = $"Kernel Simulator v{KernelVersion} - {KernelSimulatorMoniker} - Developer Preview - Milestone 1";
+        public readonly static string ConsoleTitle = $"Kernel Simulator v{KernelVersion} - {KernelSimulatorMoniker} - Developer Preview - Milestone 2";
 #elif MILESTONESPECIFIERBETA
         public readonly static string ConsoleTitle = $"Kernel Simulator v{KernelVersion} - {KernelSimulatorMoniker} - Developer Preview - Beta 1";
 #elif MILESTONESPECIFIERDELTA
