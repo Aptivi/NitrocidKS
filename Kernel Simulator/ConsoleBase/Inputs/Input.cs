1d09fb01acfc4411b8a836f77fe52dfa19f3be57
EoflaOE <eoflaoevicecity@gmail.com>
2022-08-26 08:00:43 +0300

    It'll make its return on a new generation of ReadLine.Reboot

--- Kernel Simulator/ConsoleBase/Inputs/Input.cs
+++ Kernel Simulator/ConsoleBase/Inputs/Input.cs
@@ -124,6 +124,7 @@ namespace KS.ConsoleBase.Inputs
             return ConsoleWrapper.ReadKey(Intercept);
         }
 
+        [Obsolete("It'll make its return on a new generation of ReadLine.Reboot")]
         /// <summary>
         /// Reads the next line of characters until the condition is met or the user pressed ENTER
         /// </summary>
