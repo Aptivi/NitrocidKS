4f1a0405bf974805c1c28ed92fba5275bd033561
EoflaOE <eoflaoevicecity@gmail.com>
2022-08-30 06:49:35 +0300

    [KSTests] Manually initialize dictionary before translating

--- KSTests/LanguagesTests/LocalizationActionTests.cs
+++ KSTests/LanguagesTests/LocalizationActionTests.cs
@@ -34,6 +34,7 @@ namespace KSTests.LanguagesTests
         [Description("Action")]
         public void TestTranslate()
         {
+            Translate.translatedString = Translate.PrepareDict("spa");
             string ExpectedTranslation = "Apagando ...";
             string ActualTranslation = Translate.DoTranslation("Shutting down...", "spa");
             ActualTranslation.ShouldBe(ExpectedTranslation);
