44794b7802fa052c587c655e0f6898dd3fac571d
EoflaOE <eoflaoevicecity@gmail.com>
2022-10-10 18:41:44 +0300

    Added alternative choices support in selection style

--- public/Kernel Simulator/ConsoleBase/Inputs/Styles/SelectionStyle.cs
+++ public/Kernel Simulator/ConsoleBase/Inputs/Styles/SelectionStyle.cs
@@ -17,6 +17,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Extensification.StringExts;
 using KS.ConsoleBase.Colors;
@@ -36,7 +37,8 @@ namespace KS.ConsoleBase.Inputs.Styles
         /// </summary>
         /// <param name="Question">A question</param>
         /// <param name="AnswersStr">Set of answers. They can be written like this: Y/N/C.</param>
-        public static int PromptSelection(string Question, string AnswersStr) => PromptSelection(Question, AnswersStr, Array.Empty<string>());
+        public static int PromptSelection(string Question, string AnswersStr) => 
+            PromptSelection(Question, AnswersStr, Array.Empty<string>(), "", Array.Empty<string>());
 
         /// <summary>
         /// Prompts user for Selection
@@ -44,26 +46,53 @@ namespace KS.ConsoleBase.Inputs.Styles
         /// <param name="Question">A question</param>
         /// <param name="AnswersStr">Set of answers. They can be written like this: Y/N/C.</param>
         /// <param name="AnswersTitles">Working titles for each answer. It must be the same amount as the answers.</param>
-        public static int PromptSelection(string Question, string AnswersStr, string[] AnswersTitles)
+        public static int PromptSelection(string Question, string AnswersStr, string[] AnswersTitles) =>
+            PromptSelection(Question, AnswersStr, AnswersTitles, "", Array.Empty<string>());
+
+        /// <summary>
+        /// Prompts user for Selection
+        /// </summary>
+        /// <param name="Question">A question</param>
+        /// <param name="AnswersStr">Set of answers. They can be written like this: Y/N/C.</param>
+        /// <param name="AnswersTitles">Working titles for each answer. It must be the same amount as the answers.</param>
+        /// <param name="AlternateAnswersStr">Set of alternate answers. They can be written like this: Y/N/C.</param>
+        /// <param name="AlternateAnswersTitles">Working titles for each alternate answer. It must be the same amount as the alternate answers.</param>
+        public static int PromptSelection(string Question, string AnswersStr, string[] AnswersTitles, string AlternateAnswersStr, string[] AlternateAnswersTitles)
         {
             int HighlightedAnswer = 1;
             while (true)
             {
                 // Variables
                 var answers = AnswersStr.Split('/');
+                var altAnswers = AlternateAnswersStr.Split('/');
+                var finalAnswers = new List<string>();
+                var finalAnswerTitles = new List<string>();
+                int altAnswersFirstIdx = answers.Length;
                 ConsoleKeyInfo Answer;
                 ConsoleWrapper.Clear();
 
                 // Check to see if the answer titles are the same
                 if (answers.Length != AnswersTitles.Length)
                     Array.Resize(ref AnswersTitles, answers.Length);
+                if (altAnswers.Length != AlternateAnswersTitles.Length)
+                    Array.Resize(ref AlternateAnswersTitles, altAnswers.Length);
 
                 // Ask a question
                 TextWriterColor.Write(Question + Kernel.Kernel.NewLine, true, ColorTools.ColTypes.Question);
 
+                // Populate answers to final list for below operation
+                foreach (var answer in answers)
+                    finalAnswers.Add(answer);
+                foreach (var answer in altAnswers)
+                    finalAnswers.Add(answer);
+                foreach (var answer in AnswersTitles)
+                    finalAnswerTitles.Add(answer);
+                foreach (var answer in AlternateAnswersTitles)
+                    finalAnswerTitles.Add(answer);
+
                 // Make pages based on console window height
-                int pages = answers.Length / (ConsoleWrapper.WindowHeight - ConsoleWrapper.CursorTop);
-                int answersPerPage = answers.Length / pages;
+                int pages = finalAnswers.Count / (ConsoleWrapper.WindowHeight - ConsoleWrapper.CursorTop);
+                int answersPerPage = finalAnswers.Count / pages;
                 int displayAnswersPerPage = answersPerPage - ConsoleWrapper.CursorTop;
 
                 // The reason for subtracting the highlighted answer by one is that because while the highlighted answer number is one-based, the indexes are zero-based,
@@ -73,21 +102,24 @@ namespace KS.ConsoleBase.Inputs.Styles
                 int endIndex = displayAnswersPerPage * (currentPage + 1);
 
                 // Populate the answers
-                for (int AnswerIndex = startIndex; AnswerIndex <= endIndex && AnswerIndex <= answers.Length - 1; AnswerIndex++)
+                for (int AnswerIndex = startIndex; AnswerIndex <= endIndex && AnswerIndex <= finalAnswers.Count - 1; AnswerIndex++)
                 {
-                    string AnswerInstance = answers[AnswerIndex];
-                    string AnswerTitle = AnswersTitles[AnswerIndex] ?? "";
+                    bool AltAnswer = AnswerIndex >= altAnswersFirstIdx;
+                    string AnswerInstance = finalAnswers[AnswerIndex];
+                    string AnswerTitle = finalAnswerTitles[AnswerIndex] ?? "";
 
                     // Just like we told you previously in above few lines, the last option highlight to go to the next page is intentional. So, change the last option
                     // string so it says "Highlight this entry to go to the next page."
                     string AnswerOption = $" {AnswerInstance}) {AnswerTitle}";
-                    int AnswerTitleLeft = answers.Max(x => $" {x}) ".Length);
+                    int AnswerTitleLeft = finalAnswers.Max(x => $" {x}) ".Length);
                     if (AnswerTitleLeft < ConsoleWrapper.WindowWidth)
                     {
                         int blankRepeats = AnswerTitleLeft - $" {AnswerInstance}) ".Length;
                         AnswerOption = $" {AnswerInstance}) " + " ".Repeat(blankRepeats) + $"{AnswerTitle}";
                     }
-                    var AnswerColor = AnswerIndex + 1 == HighlightedAnswer ? ColorTools.ColTypes.SelectedOption : ColorTools.ColTypes.Option;
+                    var AnswerColor = AnswerIndex + 1 == HighlightedAnswer ? 
+                                      ColorTools.ColTypes.SelectedOption : 
+                                      AltAnswer ? ColorTools.ColTypes.AlternativeOption : ColorTools.ColTypes.Option;
                     TextWriterColor.Write(AnswerIndex == endIndex ? " " + Translate.DoTranslation("Highlight this entry to go to the next page.") : AnswerOption, true, AnswerColor);
                 }
 
@@ -101,13 +133,13 @@ namespace KS.ConsoleBase.Inputs.Styles
                         {
                             HighlightedAnswer -= 1;
                             if (HighlightedAnswer == 0)
-                                HighlightedAnswer = answers.Length;
+                                HighlightedAnswer = finalAnswers.Count;
 
                             break;
                         }
                     case ConsoleKey.DownArrow:
                         {
-                            if (HighlightedAnswer == answers.Length)
+                            if (HighlightedAnswer == finalAnswers.Count)
                                 HighlightedAnswer = 0;
                             HighlightedAnswer += 1;
                             break;
@@ -119,7 +151,7 @@ namespace KS.ConsoleBase.Inputs.Styles
                         }
                     case ConsoleKey.PageDown:
                         {
-                            HighlightedAnswer = answers.Length;
+                            HighlightedAnswer = finalAnswers.Count;
                             break;
                         }
                     case ConsoleKey.Enter:
