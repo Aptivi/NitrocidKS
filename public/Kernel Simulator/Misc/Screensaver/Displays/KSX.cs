92e3c34eb8cf9f65cd8668d2b3cfacb96926e5a7
EoflaOE <eoflaoevicecity@gmail.com>
2022-11-20 06:32:31 +0200

    This is the initial stage of KSX development

--- public/Kernel Simulator/Misc/Screensaver/Displays/KSX.cs
+++ public/Kernel Simulator/Misc/Screensaver/Displays/KSX.cs
@@ -19,7 +19,11 @@
 using System;
 using System.Collections.Generic;
 using ColorSeq;
+using KS.ConsoleBase;
 using KS.Kernel.Debugging;
+using KS.Misc.Threading;
+using KS.Misc.Writers.FancyWriters;
+using KS.Misc.Writers.FancyWriters.Tools;
 
 namespace KS.Misc.Screensaver.Displays
 {
@@ -40,13 +44,91 @@ namespace KS.Misc.Screensaver.Displays
         {
             // Variable preparations
             ConsoleBase.Colors.ColorTools.LoadBack(new Color(ConsoleColors.Black), true);
-            DebugWriter.WriteDebug(DebugLevel.I, "Console geometry: {0}x{1}", ConsoleBase.ConsoleWrapper.WindowWidth, ConsoleBase.ConsoleWrapper.WindowHeight);
+            ConsoleWrapper.CursorVisible = false;
+            DebugWriter.WriteDebug(DebugLevel.I, "Console geometry: {0}x{1}", ConsoleWrapper.WindowWidth, ConsoleWrapper.WindowHeight);
         }
 
         /// <inheritdoc/>
         public override void ScreensaverLogic()
         {
+            int step;
+            int maxSteps = 19;
+            Color darkGreen = new(ConsoleColors.DarkGreen);
+            Color green = new(ConsoleColors.Green);
+            Color black = new(ConsoleColors.Black);
 
+            // Start stepping
+            for (step = 1; step <= maxSteps; step++)
+            {
+                if (ConsoleResizeListener.WasResized(false))
+                    break;
+
+                switch (step)
+                {
+                    // Step 1: fade the X character in, and use figlet
+                    case 1:
+                        int colorSteps = 30;
+
+                        // Get the color thresholds
+                        double thresholdR = darkGreen.R / (double)colorSteps;
+                        double thresholdG = darkGreen.G / (double)colorSteps;
+                        double thresholdB = darkGreen.B / (double)colorSteps;
+
+                        // Now, transition from black to the target color
+                        int currentR = 0;
+                        int currentG = 0;
+                        int currentB = 0;
+                        for (int currentStep = 1; currentStep <= maxSteps; currentStep++)
+                        {
+                            if (ConsoleResizeListener.WasResized(false))
+                                break;
+
+                            // Add the values according to the threshold
+                            currentR = (int)Math.Round(currentR + thresholdR);
+                            currentG = (int)Math.Round(currentG + thresholdG);
+                            currentB = (int)Math.Round(currentB + thresholdB);
+
+                            // Now, make a color and write the X character using figlet
+                            Color col = new(currentR, currentG, currentB);
+                            var figFont = FigletTools.GetFigletFont("Banner");
+                            int figWidth = FigletTools.GetFigletWidth("X", figFont) / 2;
+                            int figHeight = FigletTools.GetFigletHeight("X", figFont) / 2;
+                            int consoleX = (ConsoleWrapper.WindowWidth / 2) - (figWidth / 2);
+                            int consoleY = (ConsoleWrapper.WindowHeight / 2) - (figHeight / 2);
+                            FigletWhereColor.WriteFigletWhere("X", consoleX, consoleY, true, figFont, col);
+
+                            // Sleep
+                            ThreadManager.SleepNoBlock(100, ScreensaverDisplayer.ScreensaverDisplayerThread);
+                        }
+                        break;
+                    case 2:
+                        int pulses = 11;
+                        for (int currentPulse = 1; currentPulse <= pulses; currentPulse++)
+                        {
+                            if (ConsoleResizeListener.WasResized(false))
+                                break;
+
+                            // Get the final color by using the odd/even comparison
+                            var finalCol = currentPulse % 2 == 0 ? green : darkGreen;
+
+                            // Pulse the X character, alternating between darkGreen and Green colors
+                            var figFont = FigletTools.GetFigletFont("Banner");
+                            int figWidth = FigletTools.GetFigletWidth("X", figFont) / 2;
+                            int figHeight = FigletTools.GetFigletHeight("X", figFont) / 2;
+                            int consoleX = (ConsoleWrapper.WindowWidth / 2) - (figWidth / 2);
+                            int consoleY = (ConsoleWrapper.WindowHeight / 2) - (figHeight / 2);
+                            FigletWhereColor.WriteFigletWhere("X", consoleX, consoleY, true, figFont, finalCol);
+
+                            // Sleep
+                            ThreadManager.SleepNoBlock(100, ScreensaverDisplayer.ScreensaverDisplayerThread);
+                        }
+                        break;
+                }
+            }
+
+            // Reset
+            ConsoleResizeListener.WasResized();
+            ConsoleBase.Colors.ColorTools.LoadBack(black, true);
         }
 
     }
