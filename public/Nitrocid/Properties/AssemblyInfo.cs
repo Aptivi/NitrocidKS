e4fb103e047f5cfe38037c468ff20c43af046471
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-02-04 13:37:31 +0200

    Migrated assembly info to csproj, making cleanups

--- public/Nitrocid/Properties/AssemblyInfo.cs
+++ public/Nitrocid/Properties/AssemblyInfo.cs
@@ -1,40 +1,21 @@
-﻿using System;
-using System.Reflection;
-using System.Resources;
+﻿
+// Nitrocid KS  Copyright (C) 2018-2023  Aptivi
+// 
+// This file is part of Nitrocid KS
+// 
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-// Review the values of the assembly attributes
-
-[assembly: AssemblyTitle("Nitrocid")]
-[assembly: AssemblyDescription("Simulates our future kernel")]
-[assembly: AssemblyCompany("Aptivi")]
-[assembly: AssemblyProduct("Nitrocid")]
-[assembly: AssemblyCopyright("Copyright © Aptivi 2018 - 2023")]
-[assembly: AssemblyTrademark("")]
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d2feb7cd-1e12-493a-998a-6366898f6aae")]
-
-// Version information for an assembly consists of the following four values:
-// 
-// Major Version
-// Minor Version 
-// Build Number
-// Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// <Assembly: AssemblyVersion("1.0.*")> 
-
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("3.0.25.0")]
-
-[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: InternalsVisibleTo("KSTests")]
