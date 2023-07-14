2b99ee232c4fad6f8882595ca1409c7fa05b6603
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-07-14 15:37:26 +0300

    add - ptp - Implemented connection selector (proto)
    Implemented connection selector. Currently, it's internal.
    
    ---
    
    We've added a connection selector. Currently, we plan to keep this as a placeholder while we try to deal with making network shells behave like "attach"able and "detach"able shells that adapt to every connection, as well as making them be able to "bail" without disconnecting.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Part: 1/1

---
+++ public/Nitrocid/Network/Base/Connections/NetworkConnectionSelector.cs
@@ -0,0 +1,47 @@
+﻿
+// Nitrocid KS  Copyright (C) 2018-2023  Aptivi
+// 
+// This file is part of Nitrocid KS
+// 
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using KS.ConsoleBase.Inputs;
+using KS.ConsoleBase.Inputs.Styles;
+using KS.Languages;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KS.Network.Base.Connections
+{
+    internal static class NetworkConnectionSelector
+    {
+        internal static int ConnectionSelector(NetworkConnectionType connectionType)
+        {
+            var connections = NetworkConnectionTools.GetNetworkConnections(connectionType);
+            var connectionNames = connections.Select((connection) => connection.ConnectionUri.ToString()).ToArray();
+
+            // We need to prompt the user to select a connection or to establish a new connection so that the new shell can
+            // attach to the selected connection
+            var connectionsChoiceList = new List<InputChoiceInfo>();
+            for (int i = 0; i < connectionNames.Length; i++)
+            {
+                string connectionUrl = connectionNames[i];
+                connectionsChoiceList.Add(new InputChoiceInfo($"{i + 1}", connectionUrl));
+            }
+
+            return SelectionStyle.PromptSelection(Translate.DoTranslation("Select a connection. If you have no connections, you'll have to create a new connection."),
+                connectionsChoiceList, new List<InputChoiceInfo>() { new InputChoiceInfo($"{connectionNames.Length + 1}", Translate.DoTranslation("Create a new connection")) });
+        }
+    }
+}
