daf8761f78c29afde7524f7935d00673bee91bed
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-07-22 14:55:25 +0300

    add - doc - Condensed wrapped command executor
    We've added the ExecuteCommandWrapped() function
    
    ---
    
    We've graduated the enhanced command wrapper by adding the ExecuteCommandWrapped() function to CommandExecutor, which used to be an internal class that now got public.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Part: 1/1

--- public/Nitrocid/Shell/ShellBase/Commands/CommandExecutor.cs
+++ public/Nitrocid/Shell/ShellBase/Commands/CommandExecutor.cs
@@ -28,13 +28,17 @@ using KS.Misc.Writers.ConsoleWriters;
 using KS.Shell.ShellBase.Shells;
 using KS.Kernel.Events;
 using System.Linq;
+using KS.Drivers.Console.Consoles;
+using KS.Drivers.Console;
+using KS.Drivers;
+using KS.Misc.Threading;
 
 namespace KS.Shell.ShellBase.Commands
 {
     /// <summary>
-    /// Command parser module
+    /// Command executor module
     /// </summary>
-    internal static class CommandExecutor
+    public static class CommandExecutor
     {
 
         /// <summary>
@@ -197,6 +201,103 @@ namespace KS.Shell.ShellBase.Commands
                 DebugWriter.WriteDebugStackTrace(ex);
                 TextWriterColor.Write(Translate.DoTranslation("Error trying to execute command") + " {2}." + CharManager.NewLine + Translate.DoTranslation("Error {0}: {1}"), true, KernelColorType.Error, ex.GetType().FullName, ex.Message, RequestedCommand);
             }
+        }
+
+        /// <summary>
+        /// Executes a command in a wrapped mode (must be run from a separate command execution entry point, <see cref="BaseCommand.Execute(string, string[], string[])"/>.)
+        /// </summary>
+        /// <param name="Command">Requested command with its arguments and switches</param>
+        public static void ExecuteCommandWrapped(string Command)
+        {
+            var currentShell = ShellStart.ShellStack[^1];
+            var currentType = currentShell.ShellType;
+            var StartCommandThread = currentShell.ShellCommandThread;
+            var argumentInfo = new ProvidedCommandArgumentsInfo(Command, currentType);
+            string CommandToBeWrapped = argumentInfo.Command;
+
+            // Check to see if the command is found
+            if (!CommandManager.IsCommandFound(CommandToBeWrapped, currentType))
+            {
+                TextWriterColor.Write(Translate.DoTranslation("The wrappable command is not found."), true, KernelColorType.Error);
+                return;
+            }
+
+            // Check to see if we can start an alternative thread
+            if (!StartCommandThread.IsAlive)
+            {
+                TextWriterColor.Write(Translate.DoTranslation("You must not directly execute this command in a wrapped mode."), true, KernelColorType.Error);
+                return;
+            }
+
+            // Now, check to see if the command is wrappable
+            if (!CommandManager.GetCommand(CommandToBeWrapped, currentType).Flags.HasFlag(CommandFlags.Wrappable))
+            {
+                var WrappableCmds = GetWrappableCommands(currentType);
+                TextWriterColor.Write(Translate.DoTranslation("The command is not wrappable. These commands are wrappable:"), true, KernelColorType.Error);
+                ListWriterColor.WriteList(WrappableCmds);
+                return;
+            }
+
+            bool buffered = false;
+            try
+            {
+                // First, initialize the alternative command thread
+                var AltThreads = ShellStart.ShellStack[^1].AltCommandThreads;
+                if (AltThreads.Count == 0 || AltThreads[^1].IsAlive)
+                {
+                    var WrappedCommand = new KernelThread($"Wrapped Shell Command Thread", false, (cmdThreadParams) => ExecuteCommand((ExecuteCommandParameters)cmdThreadParams));
+                    ShellStart.ShellStack[^1].AltCommandThreads.Add(WrappedCommand);
+                }
+
+                // Then, initialize the buffered writer and execute the commands
+                DriverHandler.BeginLocalDriver<IConsoleDriver>("Buffered");
+                Shell.GetLine(Command, "", currentType, false);
+                buffered = true;
+
+                // Extract the buffer and then end the local driver
+                var wrapBuffer = ((Buffered)DriverHandler.CurrentConsoleDriverLocal).consoleBuffer;
+                var wrapOutput = wrapBuffer.ToString();
+                DriverHandler.EndLocalDriver<IConsoleDriver>();
+
+                // Now, print the output
+                TextWriterWrappedColor.WriteWrapped(wrapOutput, false, KernelColorType.NeutralText);
+                if (!wrapOutput.EndsWith(CharManager.NewLine))
+                    TextWriterColor.Write();
+            }
+            catch (Exception ex)
+            {
+                DebugWriter.WriteDebug(DebugLevel.E, "Failed to wrap command {0}: {1}", CommandToBeWrapped, ex.Message);
+                DebugWriter.WriteDebugStackTrace(ex);
+                TextWriterColor.Write(Translate.DoTranslation("An error occurred while trying to wrap a command output") + ": {0}", true, KernelColorType.Error, ex.Message);
+            }
+
+            // In case error happens
+            if (!buffered)
+                DriverHandler.EndLocalDriver<IConsoleDriver>();
+        }
+
+        /// <summary>
+        /// Gets the wrappable commands
+        /// </summary>
+        /// <param name="shellType">Shell type</param>
+        /// <returns>List of commands that one of their flags contains <see cref="CommandFlags.Wrappable"/></returns>
+        public static string[] GetWrappableCommands(ShellType shellType) =>
+            GetWrappableCommands(Shell.GetShellTypeName(shellType));
+
+        /// <summary>
+        /// Gets the wrappable commands
+        /// </summary>
+        /// <param name="shellType">Shell type</param>
+        /// <returns>List of commands that one of their flags contains <see cref="CommandFlags.Wrappable"/></returns>
+        public static string[] GetWrappableCommands(string shellType)
+        {
+            // Get wrappable commands
+            var WrappableCmds = Shell.GetShellInfo(shellType).Commands.Values
+                .Where(CommandInfo => CommandInfo.Flags.HasFlag(CommandFlags.Wrappable))
+                .Select(CommandInfo => CommandInfo.Command)
+                .ToArray();
+
+            return WrappableCmds;
         }
 
     }
--- public/Nitrocid/Shell/Shells/UESH/Commands/Wrap.cs
+++ public/Nitrocid/Shell/Shells/UESH/Commands/Wrap.cs
@@ -43,85 +43,17 @@ namespace KS.Shell.Shells.UESH.Commands
     class WrapCommand : BaseCommand, ICommand
     {
 
-        public override void Execute(string StringArgs, string[] ListArgsOnly, string[] ListSwitchesOnly)
-        {
-            // TODO: Graduate this to a public API function if stability is proven
-            var currentShell = ShellStart.ShellStack[^1];
-            var currentType = currentShell.ShellType;
-            var argumentInfo = new ProvidedCommandArgumentsInfo(StringArgs, currentType);
-            string CommandToBeWrapped = argumentInfo.Command;
-
-            // Check to see if the command is found
-            if (!CommandManager.IsCommandFound(CommandToBeWrapped, currentType))
-            {
-                TextWriterColor.Write(Translate.DoTranslation("The wrappable command is not found."), true, KernelColorType.Error);
-                return;
-            }
-
-            // Now, check to see if the command is wrappable
-            if (!CommandManager.GetCommand(CommandToBeWrapped, currentType).Flags.HasFlag(CommandFlags.Wrappable))
-            {
-                var WrappableCmds = new List<string>();
-                foreach (CommandInfo CommandInfo in Shell.GetShellInfo(ShellType.Shell).Commands.Values)
-                {
-                    if (CommandInfo.Flags.HasFlag(CommandFlags.Wrappable))
-                        WrappableCmds.Add(CommandInfo.Command);
-                }
-                TextWriterColor.Write(Translate.DoTranslation("The command is not wrappable. These commands are wrappable:"), true, KernelColorType.Error);
-                ListWriterColor.WriteList(WrappableCmds);
-                return;
-            }
-
-            bool buffered = false;
-            try
-            {
-                // First, initialize the alternative command thread
-                var AltThreads = ShellStart.ShellStack[^1].AltCommandThreads;
-                if (AltThreads.Count == 0 || AltThreads[^1].IsAlive)
-                {
-                    var WrappedCommand = new KernelThread($"Wrapped Shell Command Thread", false, (cmdThreadParams) => CommandExecutor.ExecuteCommand((CommandExecutor.ExecuteCommandParameters)cmdThreadParams));
-                    ShellStart.ShellStack[^1].AltCommandThreads.Add(WrappedCommand);
-                }
-
-                // Then, initialize the buffered writer and execute the commands
-                DriverHandler.BeginLocalDriver<IConsoleDriver>("Buffered");
-                Shell.GetLine(StringArgs, "", currentType, false);
-                buffered = true;
-
-                // Extract the buffer and then end the local driver
-                var wrapBuffer = ((Buffered)DriverHandler.CurrentConsoleDriverLocal).consoleBuffer;
-                var wrapOutput = wrapBuffer.ToString();
-                DriverHandler.EndLocalDriver<IConsoleDriver>();
-
-                // Now, print the output
-                TextWriterWrappedColor.WriteWrapped(wrapOutput, false, KernelColorType.NeutralText);
-                if (!wrapOutput.EndsWith(CharManager.NewLine))
-                    TextWriterColor.Write();
-            }
-            catch (Exception ex)
-            {
-                DebugWriter.WriteDebug(DebugLevel.E, "Failed to wrap command {0}: {1}", CommandToBeWrapped, ex.Message);
-                DebugWriter.WriteDebugStackTrace(ex);
-                TextWriterColor.Write(Translate.DoTranslation("An error occurred while trying to wrap a command output") + ": {0}", true, KernelColorType.Error, ex.Message);
-            }
-
-            // In case error happens
-            if (!buffered)
-                DriverHandler.EndLocalDriver<IConsoleDriver>();
-        }
+        public override void Execute(string StringArgs, string[] ListArgsOnly, string[] ListSwitchesOnly) =>
+            CommandExecutor.ExecuteCommandWrapped(StringArgs);
 
         public override void HelpHelper()
         {
-            // Get wrappable commands
-            var WrappableCmds = new ArrayList();
-            foreach (CommandInfo CommandInfo in Shell.GetShellInfo(ShellType.Shell).Commands.Values)
-            {
-                if (CommandInfo.Flags.HasFlag(CommandFlags.Wrappable))
-                    WrappableCmds.Add(CommandInfo.Command);
-            }
-
-            // Print them along with help description
-            TextWriterColor.Write(Translate.DoTranslation("Wrappable commands:") + " {0}", string.Join(", ", WrappableCmds.ToArray()));
+            // Print the wrappable commands along with help description
+            var currentShell = ShellStart.ShellStack[^1];
+            var currentType = currentShell.ShellType;
+            var WrappableCmds = CommandExecutor.GetWrappableCommands(currentType);
+            TextWriterColor.Write(Translate.DoTranslation("Wrappable commands:"));
+            ListWriterColor.WriteList(WrappableCmds);
         }
 
     }
