c94d3078608806e7aa8c6ce47ad7c9b13ba8cc25
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-07-26 07:26:47 +0300

    imp - Wrappable help command!
    We've made the help command wrappable
    
    ---
    
    You can now wrap the help command output by calling wrap on help: "wrap help"
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid/Shell/ShellManager.cs
+++ public/Nitrocid/Shell/ShellManager.cs
@@ -96,7 +96,7 @@ namespace KS.Shell
             { "exit", new CommandInfo("exit", ShellType.Shell, /* Localizable */ "Exits the shell if running on subshell",
                 new CommandArgumentInfo(), new ExitUnifiedCommand()) },
             { "help", new CommandInfo("help", ShellType.Shell, /* Localizable */ "Help page",
-                new CommandArgumentInfo(new[] { "[command]" }, Array.Empty<SwitchInfo>(), false, 0, (_, _, _) => HelpUnifiedCommand.ListCmds()), new HelpUnifiedCommand()) },
+                new CommandArgumentInfo(new[] { "[command]" }, Array.Empty<SwitchInfo>(), false, 0, (_, _, _) => HelpUnifiedCommand.ListCmds()), new HelpUnifiedCommand(), CommandFlags.Wrappable) },
             { "wrap", new CommandInfo("wrap", ShellType.Shell, /* Localizable */ "Wraps the console output",
                 new CommandArgumentInfo(new[] { "command" }, Array.Empty<SwitchInfo>(), true, 1), new WrapCommand()) }
         };
