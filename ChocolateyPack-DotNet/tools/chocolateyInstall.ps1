487ce58476ecb50b7289f5a270f6cc0de68a7237
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-07-31 11:28:41 +0300

    [BF01] Finalized Chocolatey

--- ChocolateyPack-DotNet/tools/chocolateyInstall.ps1
+++ ChocolateyPack-DotNet/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
 $url        = "https://github.com/Aptivi/Kernel-Simulator/releases/download/v0.0.24.5-beta/0.0.24.5-bin-dotnet.rar"
-$md5check   = "1d32f6c847358ceab8df0987885d3a4a"
+$md5check   = "20e1ea70f40986f6a81f42f3354b43f8"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
