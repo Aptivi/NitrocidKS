e30900be3b40613ba4cb152b9add6132418649b1
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-08-02 13:39:02 +0300

    add - Added unit tests for user
    We've added unit tests for user!
    
    ---
    
    We've added unit tests for user management functionality!
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Part: 1/1

---
+++ private/KSTests/Users/Permissions/PermissionManagerTests.cs
@@ -0,0 +1,94 @@
+﻿
+// Nitrocid KS  Copyright (C) 2018-2023  Aptivi
+// 
+// This file is part of Nitrocid KS
+// 
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using KS.Drivers.Encryption;
+using KS.Kernel.Exceptions;
+using KS.Users;
+using KS.Users.Permissions;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Linq;
+
+namespace KSTests.Users.Permissions
+{
+    [TestFixture]
+    public class PermissionManagerTests
+    {
+
+        private static readonly UserInfo rootUser = new("root", Encryption.GetEncryptedString("", "SHA256"), Array.Empty<string>(), "System Account", "", Array.Empty<string>(), true, false, false, new());
+
+        /// <summary>
+        /// Add necessary user for testing
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        [SetUp]
+        public void AddNecessaryUser()
+        {
+            UserManagement.AddUser("Account");
+            UserManagement.UserExists("Account").ShouldBeTrue();
+        }
+
+        /// <summary>
+        /// Tests querying permission grant for root
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        public void TestIsPermissionGrantedForRoot() =>
+            PermissionsTools.IsPermissionGranted("root", PermissionTypes.ManageUsers).ShouldBeTrue();
+
+        /// <summary>
+        /// Tests querying permission grant for normal user
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        public void TestIsPermissionGrantedForNormalUser() =>
+            PermissionsTools.IsPermissionGranted("Account", PermissionTypes.ManageUsers).ShouldBeFalse();
+
+        /// <summary>
+        /// Tests demand for root
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        public void TestDemandForRoot() =>
+            Should.NotThrow(() => PermissionsTools.Demand(PermissionTypes.ManageUsers));
+
+        /// <summary>
+        /// Tests demand for normal user
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        public void TestDemandForNormalUser()
+        {
+            UserManagement.CurrentUserInfo = UserManagement.Users.Single((ui) => ui.Username == "Account");
+            Should.Throw(() => PermissionsTools.Demand(PermissionTypes.ManageUsers), typeof(KernelException));
+            UserManagement.CurrentUserInfo = rootUser;
+        }
+
+        /// <summary>
+        /// Remove necessary user for testing
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        [TearDown]
+        public void RemoveNecessaryUser() =>
+            UserManagement.TryRemoveUser("Account").ShouldBeTrue();
+
+    }
+}
--- private/KSTests/Users/UserManagementTests.cs
+++ private/KSTests/Users/UserManagementTests.cs
@@ -16,9 +16,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+using KS.Drivers;
+using KS.Drivers.Encryption;
 using KS.Users;
 using NUnit.Framework;
 using Shouldly;
+using System.Collections.Generic;
 
 namespace KSTests.Users
 {
@@ -49,6 +52,21 @@ namespace KSTests.Users
             UserManagement.TryChangeUsername("Account2", "Account3").ShouldBeTrue();
 
         /// <summary>
+        /// Tests listing all users
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        public void TestListAllUsers()
+        {
+            var list = UserManagement.ListAllUsers();
+            list.ShouldNotBeNull();
+            list.ShouldNotBeEmpty();
+            list.ShouldContain("root");
+            list.ShouldContain("Account1");
+            list.ShouldContain("Account3");
+        }
+
+        /// <summary>
         /// Tests removing user
         /// </summary>
         [Test]
@@ -56,8 +74,95 @@ namespace KSTests.Users
         public void TestRemoveUser()
         {
             UserManagement.TryRemoveUser("Account1").ShouldBeTrue();
-            UserManagement.TryRemoveUser("Account3").ShouldBeTrue();
         }
+
+        /// <summary>
+        /// Tests selecting user
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        public void TestSelectUser()
+        {
+            string user = UserManagement.SelectUser(1);
+            user.ShouldBe("root");
+        }
+
+        /// <summary>
+        /// Tests getting user
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        public void TestGetUser()
+        {
+            var user = UserManagement.GetUser("root");
+            var hashRegex = DriverHandler.GetDriver<IEncryptionDriver>("SHA256").HashRegex;
+            user.ShouldNotBeNull();
+            user.Username.ShouldNotBeNullOrEmpty();
+            user.Username.ShouldBe("root");
+            user.FullName.ShouldNotBeNullOrEmpty();
+            user.FullName.ShouldBe("System Account");
+            user.Password.ShouldNotBeNullOrEmpty();
+            user.Password.ShouldBe(Encryption.GetEncryptedString("", "SHA256"));
+            hashRegex.IsMatch(user.Password).ShouldBeTrue();
+            user.Permissions.ShouldNotBeNull();
+            user.Permissions.ShouldBeEmpty();
+            user.Groups.ShouldNotBeNull();
+            user.Groups.ShouldBeEmpty();
+            user.PreferredLanguage.ShouldBeNullOrEmpty();
+            user.Admin.ShouldBeTrue();
+            user.Anonymous.ShouldBeFalse();
+            user.Disabled.ShouldBeFalse();
+            user.CustomSettings.ShouldNotBeNull();
+            user.CustomSettings.ShouldBeEmpty();
+            UserManagement.GetUserDollarSign("root").ShouldBe("#");
+            UserManagement.GetUserDollarSign("Account3").ShouldBe("$");
+        }
+
+        /// <summary>
+        /// Tests getting index of user
+        /// </summary>
+        [Test]
+        [Description("Management")]
+        public void TestGetUserIndex()
+        {
+            int userIdx = UserManagement.GetUserIndex("root");
+            var userName = UserManagement.SelectUser(userIdx + 1);
+            var user = UserManagement.GetUser(userName);
+            var hashRegex = DriverHandler.GetDriver<IEncryptionDriver>("SHA256").HashRegex;
+            user.ShouldNotBeNull();
+            user.Username.ShouldNotBeNullOrEmpty();
+            user.Username.ShouldBe("root");
+            user.FullName.ShouldNotBeNullOrEmpty();
+            user.FullName.ShouldBe("System Account");
+            user.Password.ShouldNotBeNullOrEmpty();
+            user.Password.ShouldBe(Encryption.GetEncryptedString("", "SHA256"));
+            hashRegex.IsMatch(user.Password).ShouldBeTrue();
+            user.Permissions.ShouldNotBeNull();
+            user.Permissions.ShouldBeEmpty();
+            user.Groups.ShouldNotBeNull();
+            user.Groups.ShouldBeEmpty();
+            user.PreferredLanguage.ShouldBeNullOrEmpty();
+            user.Admin.ShouldBeTrue();
+            user.Anonymous.ShouldBeFalse();
+            user.Disabled.ShouldBeFalse();
+            user.CustomSettings.ShouldNotBeNull();
+            user.CustomSettings.ShouldBeEmpty();
+            UserManagement.GetUserDollarSign("root").ShouldBe("#");
+            UserManagement.GetUserDollarSign("Account3").ShouldBe("$");
+        }
+
+        /// <summary>
+        /// Tests validating username
+        /// </summary>
+        [Test]
+        [TestCase("myvalidname", ExpectedResult = true)]
+        [TestCase("MyValidName", ExpectedResult = true)]
+        [TestCase("My!Valid?Name", ExpectedResult = false)]
+        [TestCase("My Valid Name", ExpectedResult = false)]
+        [TestCase("", ExpectedResult = false)]
+        [Description("Management")]
+        public bool TestValidateUsername(string name) =>
+           UserManagement.ValidateUsername(name, false);
 
     }
 }
