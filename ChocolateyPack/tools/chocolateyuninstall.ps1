fe50a38886f38532ae8b9f0a9e1b683973776681
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-08-07 23:30:57 +0300

    Started development of 0.0.16.18

--- ChocolateyPack/tools/chocolateyuninstall.ps1
+++ ChocolateyPack/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.16.17-bin.rar"
+  zipFileName   = "0.0.16.18-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
