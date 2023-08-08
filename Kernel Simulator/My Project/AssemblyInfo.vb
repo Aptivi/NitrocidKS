40777b2c932f654481954c5dbec5cbe426fb7de3
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-08-08 08:47:17 +0300

    [BF02] Finished incrementing

--- Kernel Simulator/My Project/AssemblyInfo.vb
+++ Kernel Simulator/My Project/AssemblyInfo.vb
@@ -34,6 +34,6 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("0.0.16.18")>
-<Assembly: AssemblyFileVersion("1.0.16.15")>
+<Assembly: AssemblyFileVersion("1.0.16.18")>
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
