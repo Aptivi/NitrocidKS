87944189863e273096f4fc23b7c5e87d1e869116
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-08-14 07:25:55 +0300

    [BF03] Finalized Chocolatey

--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
 $url        = "https://github.com/EoflaOE/Kernel-Simulator/releases/download/v0.0.16.19-alpha/0.0.16.19-bin.rar"
-$md5check   = "57b66942d1f289a985ecad12c25a354f"
+$md5check   = "7d7bd478b1c3bd62a005994d1abd9259"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
