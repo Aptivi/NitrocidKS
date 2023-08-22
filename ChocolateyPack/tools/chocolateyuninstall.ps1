adae130b64ab10e669c84d0d56af733859b7d31b
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-08-22 09:55:11 +0300

    Started development of 0.0.20.13

--- ChocolateyPack/tools/chocolateyuninstall.ps1
+++ ChocolateyPack/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.20.12-bin.rar"
+  zipFileName   = "0.0.20.13-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
