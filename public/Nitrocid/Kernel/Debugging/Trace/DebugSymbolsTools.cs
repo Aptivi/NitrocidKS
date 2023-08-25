f05727f627695fe31bbfbc87f44a0c4c7d5f02a7
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-08-25 22:25:14 +0300

    fix - Fixed FTBFS regarding checking debug symbols
    We've fixed a FTBFS instance.
    
    ---
    
    We've fixed a release build failure because of the recent changes regarding the notification enumeration movement.
    
    ---
    
    Type: fix
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid/Kernel/Debugging/Trace/DebugSymbolsTools.cs
+++ public/Nitrocid/Kernel/Debugging/Trace/DebugSymbolsTools.cs
@@ -40,16 +40,16 @@ namespace KS.Kernel.Debugging.Trace
 #if SPECIFIERREL
 			if (!NetworkTools.NetworkAvailable)
 			{
-				NotifySend(new Notification(Translate.DoTranslation("No network while downloading debug data"), Translate.DoTranslation("Check your internet connection and try again."), NotificationPriority.Medium, NotifType.Normal));
+				NotifySend(new Notification(Translate.DoTranslation("No network while downloading debug data"), Translate.DoTranslation("Check your internet connection and try again."), NotificationPriority.Medium, NotificationType.Normal));
 			}
 			if (NetworkTools.NetworkAvailable)
 			{
-				//Check to see if we're running from Ubuntu PPA
+				// Check to see if we're running from Ubuntu PPA
 				bool PPASpotted = Paths.ExecPath.StartsWith("/usr/lib/ks");
 				if (PPASpotted)
 					SplashReport.ReportProgressError(Translate.DoTranslation("Use apt to update Nitrocid KS."));
 
-				//Download debug symbols
+				// Download debug symbols
 				if (!Checking.FileExists(Assembly.GetExecutingAssembly().Location.Replace(".exe", ".pdb")) & !PPASpotted)
 				{
 					try
@@ -58,7 +58,7 @@ namespace KS.Kernel.Debugging.Trace
 					}
 					catch (Exception)
 					{
-						NotifySend(new Notification(Translate.DoTranslation("Error downloading debug data"), Translate.DoTranslation("There is an error while downloading debug data. Check your internet connection."), NotificationPriority.Medium, NotifType.Normal));
+						NotifySend(new Notification(Translate.DoTranslation("Error downloading debug data"), Translate.DoTranslation("There is an error while downloading debug data. Check your internet connection."), NotificationPriority.Medium, NotificationType.Normal));
 					}
 				}
 			}
