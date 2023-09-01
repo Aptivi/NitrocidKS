05f6fe9730588b011ed097eb7b2b1d0b739a2b6a
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-09-01 14:01:28 +0300

    add - Added initial version of POSIX signal handlers
    ---
    
    They're not initialized yet, so this is a placeholder code for now.

---
+++ public/Nitrocid/Kernel/Power/PowerSignals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KS.Kernel.Power
+{
+    internal enum PowerSignals
+    {
+        SIGTERM = -4,
+        SIGINT = -3,
+        SIGSEGV = 11,
+        SIGUSR1 = 10,
+        SIGUSR2 = 11,
+    }
+}
