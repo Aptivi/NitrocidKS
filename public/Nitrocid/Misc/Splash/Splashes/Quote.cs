e56145e0ac573488ffa302ce1952be956a6a2768
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-09-04 19:42:53 +0300

    rem - Removed leftover code from Quote
    We've removed leftover code from Quote.
    
    ---
    
    The Quote splash had leftover code, so we removed it.
    
    ---
    
    Type: rem
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid/Misc/Splash/Splashes/Quote.cs
+++ public/Nitrocid/Misc/Splash/Splashes/Quote.cs
@@ -36,36 +36,6 @@ namespace KS.Misc.Splash.Splashes
         // Standalone splash information
         public override string SplashName => "Quote";
 
-        public int ProgressWritePositionX => 3;
-
-        public int ProgressWritePositionY
-        {
-            get
-            {
-                return Config.SplashConfig.QuoteProgressTextLocation switch
-                {
-                    (int)TextLocation.Top    => 1,
-                    (int)TextLocation.Bottom => ConsoleWrapper.WindowHeight - 2,
-                    _                        => 1,
-                };
-            }
-        }
-
-        public int ProgressReportWritePositionX => 9;
-
-        public int ProgressReportWritePositionY
-        {
-            get
-            {
-                return Config.SplashConfig.QuoteProgressTextLocation switch
-                {
-                    (int)TextLocation.Top    => 1,
-                    (int)TextLocation.Bottom => ConsoleWrapper.WindowHeight - 2,
-                    _                        => 1,
-                };
-            }
-        }
-
         // Actual logic
         public override void Display()
         {
@@ -87,9 +57,6 @@ namespace KS.Misc.Splash.Splashes
                     TextWriterWhereColor.WriteWhere(str, quotePosX, currentY, quoteColor);
                 }
 
-                // Display the progress text
-                UpdateProgressReport(SplashReport.Progress, false, false, SplashReport.ProgressText, ProgressWritePositionX, ProgressWritePositionY, ProgressReportWritePositionX, ProgressReportWritePositionY);
-
                 // Loop until closing
                 while (!SplashClosing)
                     Thread.Sleep(1);
@@ -98,32 +65,6 @@ namespace KS.Misc.Splash.Splashes
             {
                 DebugWriter.WriteDebug(DebugLevel.I, "Splash done.");
             }
-        }
-
-        public override void Report(int Progress, string ProgressReport, params object[] Vars) =>
-            UpdateProgressReport(Progress, false, false, ProgressReport, Vars);
-
-        public override void ReportWarning(int Progress, string WarningReport, Exception ExceptionInfo, params object[] Vars) =>
-            UpdateProgressReport(Progress, false, true, WarningReport, Vars);
-
-        public override void ReportError(int Progress, string ErrorReport, Exception ExceptionInfo, params object[] Vars) =>
-            UpdateProgressReport(Progress, true, false, ErrorReport, Vars);
-
-        /// <summary>
-        /// Updates the splash progress
-        /// </summary>
-        /// <param name="Progress">Progress percentage from 0 to 100</param>
-        /// <param name="ProgressErrored">The progress error or not</param>
-        /// <param name="ProgressWarning">The progress warning or not</param>
-        /// <param name="ProgressReport">The progress text</param>
-        /// <param name="Vars">Variables to be formatted in the text</param>
-        public void UpdateProgressReport(int Progress, bool ProgressErrored, bool ProgressWarning, string ProgressReport, params object[] Vars)
-        {
-            string RenderedText = ProgressReport.Truncate(ConsoleWrapper.WindowWidth - ProgressReportWritePositionX - ProgressWritePositionX - 3);
-            TextWriterWhereColor.WriteWhere("{0:000}%", ProgressWritePositionX, ProgressWritePositionY, true, KernelColorType.Progress, Progress);
-            TextWriterWhereColor.WriteWhere($"{(ProgressErrored ? "[X] " : "")}{RenderedText}", ProgressReportWritePositionX, ProgressReportWritePositionY, false, KernelColorType.Error, Vars);
-            TextWriterWhereColor.WriteWhere($"{(ProgressWarning ? "[!] " : "")}{RenderedText}", ProgressReportWritePositionX, ProgressReportWritePositionY, false, KernelColorType.Warning, Vars);
-            ConsoleExtensions.ClearLineToRight();
         }
 
     }
