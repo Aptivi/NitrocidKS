Aptivi/NitrocidKS

commit f48868e28dc96d41a1f02817ed4b3a20b603a0dd

imp - Made KernelVersion and KernelApiVersion properties
These two variables are now properties.

---

They used to be two read-only fields. Now, they are properties.

---

Type: imp
Breaking: False
Doc Required: False
Part: 1/1

public/Nitrocid/Kernel/KernelTools.cs
@@ -30,16 +30,6 @@ namespace KS.Kernel
     public static class KernelTools
     {
 
-        /// <summary>
-        /// Kernel version
-        /// </summary>
-        public readonly static Version KernelVersion = Assembly.GetExecutingAssembly().GetName().Version;
-        /// <summary>
-        /// Kernel API version
-        /// </summary>
-        // Refer to NitrocidModAPIVersion in the project file.
-        public readonly static Version KernelApiVersion = new(FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion);
-
         internal static Stopwatch StageTimer = new();
         internal static string bannerFigletFont = "Banner";
 
@@ -67,6 +57,24 @@ namespace KS.Kernel
 #else
         internal readonly static string ConsoleTitle = $"Nitrocid v{KernelVersion} {ReleaseSpecifier} (API v{KernelApiVersion})";
 #endif
+
+        // Private variables
+        private static readonly Version kernelVersion =
+            Assembly.GetExecutingAssembly().GetName().Version;
+        private static readonly Version kernelApiVersion =
+            new(FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion);
+
+        /// <summary>
+        /// Kernel version
+        /// </summary>
+        public static Version KernelVersion =>
+            kernelVersion;
+        /// <summary>
+        /// Kernel API version
+        /// </summary>
+        // Refer to NitrocidModAPIVersion in the project file.
+        public static Version KernelApiVersion =>
+            kernelApiVersion;
 
         /// <summary>
         /// Check to see if KernelError has been called

commit 0ef08030cdb83e9187b78c4a5fcfbfbd70ebd0a3

fix - reg - Fixed version not showing on console title
We've fixed a regression caused by one of the latest commits.

---

One of the latest commits, f48868e28dc96d41a1f02817ed4b3a20b603a0dd, caused the console title to no longer hold information about the kernel and mod API version. This is now fixed.

---

Type: fix
Breaking: False
Doc Required: False
Part: 1/1

public/Nitrocid/Kernel/KernelTools.cs
@@ -32,6 +32,10 @@ namespace KS.Kernel
 
         internal static Stopwatch StageTimer = new();
         internal static string bannerFigletFont = "Banner";
+        private static readonly Version kernelVersion =
+            Assembly.GetExecutingAssembly().GetName().Version;
+        private static readonly Version kernelApiVersion =
+            new(FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion);
 
         // #ifdef'd variables...
         // Release specifiers (SPECIFIER: REL, RC, or DEV | MILESTONESPECIFIER: ALPHA, BETA, or NONE | None satisfied: Unsupported Release)
@@ -57,12 +61,6 @@ namespace KS.Kernel
 #else
         internal readonly static string ConsoleTitle = $"Nitrocid v{KernelVersion} {ReleaseSpecifier} (API v{KernelApiVersion})";
 #endif
-
-        // Private variables
-        private static readonly Version kernelVersion =
-            Assembly.GetExecutingAssembly().GetName().Version;
-        private static readonly Version kernelApiVersion =
-            new(FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion);
 
         /// <summary>
         /// Kernel version