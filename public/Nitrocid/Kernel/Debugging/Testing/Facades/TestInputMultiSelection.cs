4733247b1a55df20e457065c43894ef5ef5dc6dd
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-09-24 09:04:18 +0300

    und - Updated Ubuntu selections
    ---
    
    Type: und
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid/Kernel/Debugging/Testing/Facades/TestInputMultiSelection.cs
+++ public/Nitrocid/Kernel/Debugging/Testing/Facades/TestInputMultiSelection.cs
@@ -35,11 +35,12 @@ namespace KS.Kernel.Debugging.Testing.Facades
             {
                 new InputChoiceInfo("focal", "20.04 (Focal Fossa)", "Ubuntu 20.04 LTS, codenamed Focal Fossa, is a long-term support release and was released on 23 April 2020."),
                 new InputChoiceInfo("jammy", "22.04 (Jammy Jellyfish)", "Ubuntu 22.04, codenamed Jammy Jellyfish, was released on 21 April 2022, and is a long-term support release, supported for five years, until April 2027."),
+                new InputChoiceInfo("n-adjective", "24.04 (N-adjective N-animal)", "Ubuntu 24.04, codenamed N-adjective N-animal, is planned to be released on April 2024, and is a long-term support release, supported for five years, until April 2029."),
             };
             var altChoices = new List<InputChoiceInfo>()
             {
-                new InputChoiceInfo("kinetic", "22.10 (Kinetic Kudu)", "Ubuntu 22.10, codenamed Kinetic Kudu, is an interim release and was made on 20 October 2022."),
-                new InputChoiceInfo("lunar", "23.04 (Lunar Lobster)", "Ubuntu 23.04 Lunar Lobster is an interim release, scheduled 20 April 2023."),
+                new InputChoiceInfo("lunar", "23.04 (Lunar Lobster)", "Ubuntu 23.04 Lunar Lobster is an interim release, released 20 April 2023."),
+                new InputChoiceInfo("mantic", "23.10 (Mantic Minotaur)", "Ubuntu 23.10 Mantic Minotaur is an interim release, scheduled 12 October 2023."),
             };
             var answers = SelectionMultipleStyle.PromptMultipleSelection("Which Ubuntu version would you like to run?", choices, altChoices);
             TextWriterColor.Write(string.Join(", ", answers));
