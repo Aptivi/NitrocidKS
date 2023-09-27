54638a058249e7b0c65f7dbca6b9d94165db386b
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-09-27 13:00:35 +0300

    imp - Disclaimers should respect the nosplash environmen...
    ...t
    
    Disclaimers should not mess everything up when booting without splash.
    
    ---
    
    When booting without splash, disclaimers should use the plain text writing instead of informational boxes.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid/ConsoleBase/Writers/MiscWriters/WelcomeMessage.cs
+++ public/Nitrocid/ConsoleBase/Writers/MiscWriters/WelcomeMessage.cs
@@ -114,8 +114,7 @@ namespace KS.ConsoleBase.Writers.MiscWriters
             // no-op
             return;
 #else
-            // Show development disclaimer
-            InfoBoxColor.WriteInfoBox(
+            string message =
 #if SPECIFIERDEV
                 Translate.DoTranslation("You're running the development version of the kernel. While you can experience upcoming features which may exist in the final release, you may run into bugs, instabilities, or even data loss. We recommend using the stable version, if possible.")
 #elif SPECIFIERRC
@@ -123,7 +122,13 @@ namespace KS.ConsoleBase.Writers.MiscWriters
 #elif SPECIFIERREL == false
                 Translate.DoTranslation("We recommend against running this version of the kernel, because it is unsupported. If you have downloaded this kernel from unknown sources, this message may appear. Please download from our official downloads page.")
 #endif
-            + "\n\n" + Translate.DoTranslation("Press any key to continue."), KernelColorType.DevelopmentWarning);
+            ;
+
+            // Show development disclaimer
+            if (KernelFlags.EnableSplash)
+                InfoBoxColor.WriteInfoBox($"{message}\n\n" + Translate.DoTranslation("Press any key to continue."), KernelColorType.DevelopmentWarning);
+            else
+                TextWriterColor.Write($"* {message}", true, KernelColorType.DevelopmentWarning);
 #endif
         }
 
@@ -132,7 +137,11 @@ namespace KS.ConsoleBase.Writers.MiscWriters
 #if NET7_0
             // Show .NET 7.0 version disclaimer
             // TODO: Remove this when .NET 8.0 releases on November and Nitrocid KS gets re-targeted to that version on December.
-            InfoBoxColor.WriteInfoBox("You're running a .NET 7.0 version of Nitrocid KS. This is going to be used as a testing ground to ensure that we can have smooth upgrade experience to .NET 8.0. Meanwhile, you can evaluate this version until .NET 8.0 gets released on November." + "\n\n" + Translate.DoTranslation("Press any key to continue."), KernelColorType.DevelopmentWarning);
+            string message = "You're running a .NET 7.0 version of Nitrocid KS. This is going to be used as a testing ground to ensure that we can have smooth upgrade experience to .NET 8.0. Meanwhile, you can evaluate this version until .NET 8.0 gets released on November.";
+            if (KernelFlags.EnableSplash)
+                InfoBoxColor.WriteInfoBox($"{message}\n\n" + Translate.DoTranslation("Press any key to continue."), KernelColorType.DevelopmentWarning);
+            else
+                TextWriterColor.Write($"* {message}", true, KernelColorType.DevelopmentWarning);
 #endif
         }
         
