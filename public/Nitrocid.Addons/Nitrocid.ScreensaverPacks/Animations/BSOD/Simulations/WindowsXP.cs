c1cae840f15f3a0aadcbd736e3c5450290b80547
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-10-04 13:49:35 +0300

    add - Added common Windows XP bugchecks
    We've added common Windows XP bugchecks to the screensaver mode
    
    ---
    
    BSOD screensaver mode, Windows XP, now has more bugchecks that are common.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.ScreensaverPacks/Animations/BSOD/Simulations/WindowsXP.cs
+++ public/Nitrocid.Addons/Nitrocid.ScreensaverPacks/Animations/BSOD/Simulations/WindowsXP.cs
@@ -32,7 +32,30 @@ namespace Nitrocid.ScreensaverPacks.Animations.BSOD.Simulations
         {
             IRQL_NOT_LESS_OR_EQUAL,
             DRIVER_IRQL_NOT_LESS_OR_EQUAL,
-            CRITICAL_PROCESS_DIED
+            CRITICAL_OBJECT_TERMINATION,
+            BAD_POOL_HEADER,
+            MEMORY_MANAGEMENT,
+            KMODE_EXCEPTION_NOT_HANDLED,
+            PHASE0_INITIALIZATION_FAILED,
+            PHASE1_INITIALIZATION_FAILED,
+            SYSTEM_SERVICE_EXCEPTION,
+            PAGE_FAULT_IN_NONPAGED_AREA,
+            HAL_INITIALIZATION_FAILED,
+            UNSUPPORTED_PROCESSOR,
+            BAD_SYSTEM_CONFIG_INFO,
+            INACCESSIBLE_BOOT_DEVICE,
+            SYSTEM_THREAD_EXCEPTION_NOT_HANDLED,
+            UNEXPECTED_KERNEL_MODE_TRAP,
+            KERNEL_MODE_EXCEPTION_NOT_HANDLED,
+            MACHINE_CHECK_EXCEPTION,
+            DRIVER_POWER_STATE_FAILURE,
+            BAD_POOL_CALLER,
+            THREAD_STUCK_IN_DEVICE_DRIVER,
+            UNMOUNTABLE_BOOT_VOLUME,
+            MAXIMUM_WAIT_OBJECTS_EXCEEDED,
+            WHEA_UNCORRECTABLE_ERROR,
+            UNEXPECTED_STORE_EXCEPTION,
+            MANUALLY_INITIATED_CRASH,
         }
 
         public class BugCheckParams
@@ -44,9 +67,219 @@ namespace Nitrocid.ScreensaverPacks.Animations.BSOD.Simulations
 
         private static readonly Dictionary<BugCheckCodes, BugCheckParams> BugChecks = new()
         {
-            { BugCheckCodes.IRQL_NOT_LESS_OR_EQUAL,         new BugCheckParams() { WindowsBugCheckCode = 0xA,  DisplayMessage = false } },
-            { BugCheckCodes.DRIVER_IRQL_NOT_LESS_OR_EQUAL,  new BugCheckParams() { WindowsBugCheckCode = 0xD1, DisplayMessage = false } },
-            { BugCheckCodes.CRITICAL_PROCESS_DIED,          new BugCheckParams() { WindowsBugCheckCode = 0xEF, DisplayMessage = true, Message = "\nA process or thread crucial to system operation has unexpectedly exited or been terminated." } },
+            { BugCheckCodes.IRQL_NOT_LESS_OR_EQUAL,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0xA, 
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.MAXIMUM_WAIT_OBJECTS_EXCEEDED,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0xC, 
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.DRIVER_IRQL_NOT_LESS_OR_EQUAL,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0xD1,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.CRITICAL_OBJECT_TERMINATION,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0xF4,
+                    DisplayMessage = true,
+                    Message =
+                    """
+
+                    A process or thread crucial to system operation has unexpectedly exited or been terminated.
+                    """
+                }
+            },
+
+            { BugCheckCodes.BAD_POOL_HEADER,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x19,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.MEMORY_MANAGEMENT,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x1A,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.KMODE_EXCEPTION_NOT_HANDLED,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x1E,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.PHASE0_INITIALIZATION_FAILED,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x31,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.PHASE1_INITIALIZATION_FAILED,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x32,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.SYSTEM_SERVICE_EXCEPTION,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x3B,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.PAGE_FAULT_IN_NONPAGED_AREA,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x50,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.HAL_INITIALIZATION_FAILED,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x5C,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.UNSUPPORTED_PROCESSOR,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x5D,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.BAD_SYSTEM_CONFIG_INFO,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x74,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.INACCESSIBLE_BOOT_DEVICE,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x7B,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.SYSTEM_THREAD_EXCEPTION_NOT_HANDLED,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x7E,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.UNEXPECTED_KERNEL_MODE_TRAP,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x7F,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.KERNEL_MODE_EXCEPTION_NOT_HANDLED,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x8E,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.MACHINE_CHECK_EXCEPTION,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x9C,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.DRIVER_POWER_STATE_FAILURE,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x9F,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.BAD_POOL_CALLER,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0xC2,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.MANUALLY_INITIATED_CRASH,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0xE2,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.THREAD_STUCK_IN_DEVICE_DRIVER,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0xEA,
+                    DisplayMessage = true,
+                    Message =
+                        """
+
+                        The device driver got stuck in an infinite loop. This usually indicates
+                        problem with the device itself or with the device driver programming the
+                        hardware incorrectly.
+
+                        Please check with your hardware device vendor for any driver updates.
+                        """
+                }
+            },
+
+            { BugCheckCodes.UNMOUNTABLE_BOOT_VOLUME,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0xED,
+                    DisplayMessage = false
+                }
+            },
+
+            { BugCheckCodes.WHEA_UNCORRECTABLE_ERROR,
+                new BugCheckParams()
+                {
+                    WindowsBugCheckCode = 0x124,
+                    DisplayMessage = false
+                }
+            },
         };
 
         public override void Simulate()
