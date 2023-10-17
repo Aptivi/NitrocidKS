a0b19fa0ec4d6ddc0c55051fa4e9bfd48d830b29
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-10-17 14:39:29 +0300

    fix - reg - Auto complete should no longer crash
    We've fixed a regression caused by the recent change to the shell
    
    ---
    
    A recent major change to the shell caused the auto completer to crash, so we've fixed this regression.
    
    ---
    
    Type: fix
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid/Shell/ShellBase/Arguments/ArgumentsParser.cs
+++ public/Nitrocid/Shell/ShellBase/Arguments/ArgumentsParser.cs
@@ -76,10 +76,11 @@ namespace KS.Shell.ShellBase.Arguments
                               ShellCommands.ContainsKey(Command) ? ShellCommands[Command] :
                               aliases.Any((info) => info.Alias == Command) ? aliases.Single((info) => info.Alias == Command).TargetCommand :
                               null;
+            var fallback = new ProvidedArgumentsInfo(Command, arguments, words.Skip(1).ToArray(), argumentsOrig, wordsOrig.Skip(1).ToArray(), Array.Empty<string>(), true, true, true, Array.Empty<string>(), Array.Empty<string>(), Array.Empty<string>(), true, true);
             if (CommandInfo != null)
                 return ProcessArgumentOrShellCommandArguments(CommandText, CommandInfo, null);
             else
-                return (new ProvidedArgumentsInfo(Command, arguments, words.Skip(1).ToArray(), argumentsOrig, wordsOrig.Skip(1).ToArray(), Array.Empty<string>(), true, true, true, Array.Empty<string>(), Array.Empty<string>(), Array.Empty<string>(), true, true), Array.Empty<ProvidedArgumentsInfo>());
+                return (fallback, new[] { fallback });
         }
 
         /// <summary>
@@ -103,10 +104,11 @@ namespace KS.Shell.ShellBase.Arguments
 
             // Check to see if the caller has provided a switch that subtracts the number of required arguments
             var ArgumentInfo = KernelArguments.ContainsKey(Argument) ? KernelArguments[Argument] : null;
+            var fallback = new ProvidedArgumentsInfo(Argument, arguments, words.Skip(1).ToArray(), argumentsOrig, wordsOrig.Skip(1).ToArray(), Array.Empty<string>(), true, true, true, Array.Empty<string>(), Array.Empty<string>(), Array.Empty<string>(), true, true);
             if (ArgumentInfo != null)
                 return ProcessArgumentOrShellCommandArguments(ArgumentText, null, ArgumentInfo);
             else
-                return (new ProvidedArgumentsInfo(Argument, arguments, words.Skip(1).ToArray(), argumentsOrig, wordsOrig.Skip(1).ToArray(), Array.Empty<string>(), true, true, true, Array.Empty<string>(), Array.Empty<string>(), Array.Empty<string>(), true, true), Array.Empty<ProvidedArgumentsInfo>());
+                return (fallback, new[] { fallback });
         }
 
         private static (ProvidedArgumentsInfo satisfied, ProvidedArgumentsInfo[] total) ProcessArgumentOrShellCommandArguments(string CommandText, CommandInfo CommandInfo, ArgumentInfo ArgumentInfo)
