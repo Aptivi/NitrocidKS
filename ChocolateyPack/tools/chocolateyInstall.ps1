ea3cdd6fb600be37b91ed6437b989046856b17fb
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-10-20 20:58:40 +0300

    imp - Finalized the Chocolatey scripts for use with zip ...
    ...files
    
    As we've made changes to the pack scripts, we've also made changes to the Chocolatey scripts.
    
    ---
    
    Changes made to the Chocolatey scripts were necessary, because we've switched from RAR packing format to ZIP packing format. This is to ease installation of Nitrocid from ZIP files across platforms.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
-$url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.1.0-b2/0.1.0-b2-bin.rar"
+$url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.1.0-b2/0.1.0-b2-bin.zip"
 $md5check   = "eef3e36f0f09e619b54b9e7a13bbe4bc"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
