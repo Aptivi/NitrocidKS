84594184b0c3e1a5a3b58cf0c83058021c222ec9
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-10-21 18:58:00 +0300

    add - Added preliminary support for MP3 file handler
    ---
    
    We've added preliminary support for the MP3 file handler. Please note that this is currently not supported yet, and will fail to work until BassBoom is updated.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.Extras.BassBoom/BassBoomInit.cs
+++ public/Nitrocid.Addons/Nitrocid.Extras.BassBoom/BassBoomInit.cs
@@ -16,8 +16,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+using KS.ConsoleBase.Colors;
+using KS.ConsoleBase.Writers.FancyWriters;
+using KS.Files.Extensions;
 using KS.Kernel.Configuration;
 using KS.Kernel.Extensions;
+using KS.Languages;
 using KS.Misc.Screensaver;
 using KS.Shell.ShellBase.Arguments;
 using KS.Shell.ShellBase.Commands;
@@ -28,12 +32,48 @@ using Nitrocid.Extras.BassBoom.Screensavers;
 using Nitrocid.Extras.BassBoom.Settings;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+
+#if NET7_0
+using BassBoom.Basolia;
+using BassBoom.Basolia.File;
+using BassBoom.Basolia.Format;
+#endif
 
 namespace Nitrocid.Extras.BassBoom
 {
     internal class BassBoomInit : IAddon
     {
+        private readonly ExtensionHandler handler = new(
+            ".mp3",
+            (_) => InfoBoxColor.WriteInfoBoxKernelColor(Translate.DoTranslation("You'll be able to play music soon. Hang tight!"), KernelColorType.Warning),
+            (path) =>
+            {
+                // Get the ID3 metadata
+                FileTools.OpenFile(path);
+                AudioInfoTools.GetId3Metadata(out var metadatav1, out var metadatav2);
+                FileTools.CloseFile();
+
+                // Now, populate the necessary variables
+                string musicName =
+                    !string.IsNullOrEmpty(metadatav2.Title) ? metadatav2.Title :
+                    !string.IsNullOrEmpty(metadatav1.Title) ? metadatav1.Title :
+                    Path.GetFileNameWithoutExtension(path);
+                string musicArtist =
+                    !string.IsNullOrEmpty(metadatav2.Artist) ? metadatav2.Artist :
+                    !string.IsNullOrEmpty(metadatav1.Artist) ? metadatav1.Artist :
+                    Translate.DoTranslation("Unknown Artist");
+                string musicGenre =
+                    !string.IsNullOrEmpty(metadatav2.Genre) ? metadatav2.Genre :
+                    metadatav1.GenreIndex >= 0 ? $"{metadatav1.Genre} [{metadatav1.GenreIndex}]" :
+                    Translate.DoTranslation("Unknown Genre");
+                return
+                    $"{Translate.DoTranslation("Song name")}: {musicName}\n" +
+                    $"{Translate.DoTranslation("Song artist")}: {musicArtist}\n" +
+                    $"{Translate.DoTranslation("Song genre")}: {musicGenre}";
+            }
+        );
         private readonly Dictionary<string, CommandInfo> addonCommands = new()
         {
             { "playlyric",
@@ -76,6 +116,12 @@ namespace Nitrocid.Extras.BassBoom
             var bbConfig = new BassBoomConfig();
             ConfigTools.RegisterBaseSetting(saversConfig);
             ConfigTools.RegisterBaseSetting(bbConfig);
+
+#if NET7_0
+            // Additionally, register a custom extension handler that handles music playback
+            InitBasolia.Init();
+            ExtensionHandlerTools.extensionHandlers.Add(handler);
+#endif
         }
 
         void IAddon.StopAddon()
@@ -84,6 +130,7 @@ namespace Nitrocid.Extras.BassBoom
             ScreensaverManager.Screensavers.Remove("lyrics");
             ConfigTools.UnregisterBaseSetting(nameof(BassBoomSaversConfig));
             ConfigTools.UnregisterBaseSetting(nameof(BassBoomConfig));
+            ExtensionHandlerTools.extensionHandlers.Remove(handler);
         }
 
         void IAddon.FinalizeAddon()
