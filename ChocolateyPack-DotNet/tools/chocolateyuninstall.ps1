ac201314cb06e01dccf708f36f79695364c68a99
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-10-24 13:13:12 +0300

    dev - Started development of 0.0.24.9
    ---
    
    Type: dev
    Breaking: False
    Doc Required: False
    Part: 1/1

--- ChocolateyPack-DotNet/tools/chocolateyuninstall.ps1
+++ ChocolateyPack-DotNet/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.24.8-bin.rar"
+  zipFileName   = "0.0.24.9-bin.rar"
 }
 $userProfile = $Env:USERPROFILE
 
