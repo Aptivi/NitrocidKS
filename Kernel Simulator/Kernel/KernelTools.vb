f28058af1d697dfc6744c61f0b6aa2cbfe807f13
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-10-24 13:47:40 +0300

    imp - Updated the kernel update system to use zip
    ---
    
    We've updated the kernel update system to fetch ZIPs instead of RARs. This ensures that we would get future updates.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- Kernel Simulator/Kernel/KernelTools.vb
+++ Kernel Simulator/Kernel/KernelTools.vb
@@ -473,7 +473,7 @@ Namespace Kernel
                     ReportProgress(DoTranslation("Found new version: "), 10, ColTypes.ListEntry)
                     ReportProgress(AvailableUpdate.UpdateVersion.ToString, 10, ColTypes.ListValue)
                     If AutoDownloadUpdate Then
-                        DownloadFile(AvailableUpdate.UpdateURL.ToString, Path.Combine(Environment.CurrentDirectory, "update.rar"))
+                        DownloadFile(AvailableUpdate.UpdateURL.ToString, Path.Combine(Environment.CurrentDirectory, "update.zip"))
                         ReportProgress(DoTranslation("Downloaded the update successfully!"), 10, ColTypes.Success)
                     Else
                         ReportProgress(DoTranslation("You can download it at: "), 10, ColTypes.ListEntry)
