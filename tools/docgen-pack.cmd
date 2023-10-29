2580dd2eb62bcc760af8612bbe1e350485a74b8e
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-10-29 14:14:10 +0300

    und - Update build system to use ZIP

--- tools/docgen-pack.cmd
+++ tools/docgen-pack.cmd
@@ -1,22 +1,5 @@
 @echo off
 
-REM    Nitrocid KS  Copyright (C) 2018-2022  Aptivi
-REM
-REM    This file is part of Nitrocid KS
-REM
-REM    Nitrocid KS is free software: you can redistribute it and/or modify
-REM    it under the terms of the GNU General Public License as published by
-REM    the Free Software Foundation, either version 3 of the License, or
-REM    (at your option) any later version.
-REM
-REM    Nitrocid KS is distributed in the hope that it will be useful,
-REM    but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-REM    GNU General Public License for more details.
-REM
-REM    You should have received a copy of the GNU General Public License
-REM    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 REM This script builds KS documentation and packs the artifacts. Use when you have VS installed.
 for /f "tokens=* USEBACKQ" %%f in (`type version`) do set ksversion=%%f
 
