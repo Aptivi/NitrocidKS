48a773177946d3374aefdef457f9ea3eae9dab59
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-11-09 10:49:55 +0300

    imp - Diamond can now shine
    ---
    
    A diamond in the Diamond screensaver can now shine.
    
    This means that the screensaver is now complete!
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.ScreensaverPacks/Screensavers/Diamond.cs
+++ public/Nitrocid.Addons/Nitrocid.ScreensaverPacks/Screensavers/Diamond.cs
@@ -58,6 +58,8 @@ namespace Nitrocid.ScreensaverPacks.Screensavers
     public class DiamondDisplay : BaseScreensaver, IScreensaver
     {
 
+        private static bool shining;
+        private static int maxShineSteps = 15;
         private static Color selectedColor = ConsoleColors.LightCyan1;
         private static readonly List<Color> diamondColors = new()
         {
@@ -89,6 +91,11 @@ namespace Nitrocid.ScreensaverPacks.Screensavers
 
             // Get the color shades
             var colorShaded = new Color(selectedColor.R / 3, selectedColor.G / 3, selectedColor.B / 3);
+            int shinePlaceX = halfWidth;
+            int shinePlaceY = halfHeight - (halfWidth - minLeft);
+            int shinePlaceX2 = minLeft;
+            int shinePlaceY2 = halfHeight;
+            shining = RandomDriver.RandomChance(5);
 
             // Now, draw the diamond (the first part)
             for (int left = minLeft; left <= halfWidth; left++)
@@ -119,6 +126,130 @@ namespace Nitrocid.ScreensaverPacks.Screensavers
                     // Determine the color by draw mode
                     var finalColor = drawingEdge ? selectedColor : colorShaded;
                     TextWriterWhereColor.WriteWhereColorBack(" ", left, top, Color.Empty, finalColor);
+                }
+            }
+
+            // If shining, fade the small light in
+            if (shining)
+            {
+                // Get the shined color
+                int shinedR = selectedColor.R * 2;
+                if (shinedR > 255)
+                    shinedR = 255;
+                int shinedG = selectedColor.G * 2;
+                if (shinedG > 255)
+                    shinedG = 255;
+                int shinedB = selectedColor.B * 2;
+                if (shinedB > 255)
+                    shinedB = 255;
+                var shined = new Color(shinedR, shinedG, shinedB);
+
+                // Get the threshold from either the shade (for the bottom), the diamond color (for the center), or the
+                // black color (the rest of the directions)
+                var black = new Color(ConsoleColors.Black);
+                double thresholdBottomR = (colorShaded.R - shined.R) / (double)maxShineSteps;
+                double thresholdBottomG = (colorShaded.G - shined.G) / (double)maxShineSteps;
+                double thresholdBottomB = (colorShaded.B - shined.B) / (double)maxShineSteps;
+                double thresholdCenterR = (selectedColor.R - shined.R) / (double)maxShineSteps;
+                double thresholdCenterG = (selectedColor.G - shined.G) / (double)maxShineSteps;
+                double thresholdCenterB = (selectedColor.B - shined.B) / (double)maxShineSteps;
+                double thresholdRestR = (black.R - shined.R) / (double)maxShineSteps;
+                double thresholdRestG = (black.G - shined.G) / (double)maxShineSteps;
+                double thresholdRestB = (black.B - shined.B) / (double)maxShineSteps;
+
+                // Now, transition to the target color
+                double currentBottomR = colorShaded.R;
+                double currentBottomG = colorShaded.G;
+                double currentBottomB = colorShaded.B;
+                double currentCenterR = selectedColor.R;
+                double currentCenterG = selectedColor.G;
+                double currentCenterB = selectedColor.B;
+                double currentRestR = black.R;
+                double currentRestG = black.G;
+                double currentRestB = black.B;
+                for (int currentStep = 1; currentStep <= maxShineSteps; currentStep++)
+                {
+                    if (ConsoleResizeListener.WasResized(false))
+                        break;
+
+                    // Add the values according to the threshold
+                    currentBottomR -= thresholdBottomR;
+                    currentBottomG -= thresholdBottomG;
+                    currentBottomB -= thresholdBottomB;
+                    currentCenterR -= thresholdCenterR;
+                    currentCenterG -= thresholdCenterG;
+                    currentCenterB -= thresholdCenterB;
+                    currentRestR -= thresholdRestR;
+                    currentRestG -= thresholdRestG;
+                    currentRestB -= thresholdRestB;
+
+                    // Now, draw the shiny part
+                    Color colBottom = new((int)currentBottomR, (int)currentBottomG, (int)currentBottomB);
+                    Color colCenter = new((int)currentCenterR, (int)currentCenterG, (int)currentCenterB);
+                    Color colRest = new((int)currentRestR, (int)currentRestG, (int)currentRestB);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY, Color.Empty, colCenter);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY + 1, Color.Empty, colBottom);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY + 2, Color.Empty, colBottom);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY - 1, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY - 2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX - 1, shinePlaceY, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX - 2, shinePlaceY, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX + 1, shinePlaceY, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX + 2, shinePlaceY, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2, Color.Empty, colCenter);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2 + 1, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2 + 2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2 - 1, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2 - 2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2 - 1, shinePlaceY2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2 - 2, shinePlaceY2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2 + 1, shinePlaceY2, Color.Empty, colBottom);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2 + 2, shinePlaceY2, Color.Empty, colBottom);
+
+                    // Sleep
+                    ThreadManager.SleepNoBlock(DiamondSettings.DiamondDelay, ScreensaverDisplayer.ScreensaverDisplayerThread);
+                }
+                for (int currentStep = 1; currentStep <= maxShineSteps; currentStep++)
+                {
+                    if (ConsoleResizeListener.WasResized(false))
+                        break;
+
+                    // Add the values according to the threshold
+                    currentBottomR -= -thresholdBottomR;
+                    currentBottomG -= -thresholdBottomG;
+                    currentBottomB -= -thresholdBottomB;
+                    currentCenterR -= -thresholdCenterR;
+                    currentCenterG -= -thresholdCenterG;
+                    currentCenterB -= -thresholdCenterB;
+                    currentRestR -= -thresholdRestR;
+                    currentRestG -= -thresholdRestG;
+                    currentRestB -= -thresholdRestB;
+
+                    // Now, draw the shiny part
+                    Color colBottom = new((int)currentBottomR, (int)currentBottomG, (int)currentBottomB);
+                    Color colCenter = new((int)currentCenterR, (int)currentCenterG, (int)currentCenterB);
+                    Color colRest = new((int)currentRestR, (int)currentRestG, (int)currentRestB);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY, Color.Empty, colCenter);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY + 1, Color.Empty, colBottom);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY + 2, Color.Empty, colBottom);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY - 1, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX, shinePlaceY - 2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX - 1, shinePlaceY, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX - 2, shinePlaceY, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX + 1, shinePlaceY, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX + 2, shinePlaceY, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2, Color.Empty, colCenter);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2 + 1, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2 + 2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2 - 1, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2, shinePlaceY2 - 2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2 - 1, shinePlaceY2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2 - 2, shinePlaceY2, Color.Empty, colRest);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2 + 1, shinePlaceY2, Color.Empty, colBottom);
+                    TextWriterWhereColor.WriteWhereColorBack(" ", shinePlaceX2 + 2, shinePlaceY2, Color.Empty, colBottom);
+
+                    // Sleep
+                    ThreadManager.SleepNoBlock(DiamondSettings.DiamondDelay, ScreensaverDisplayer.ScreensaverDisplayerThread);
                 }
             }
 
