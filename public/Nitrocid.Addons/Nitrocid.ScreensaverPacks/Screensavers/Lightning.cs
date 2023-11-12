63ea57a27b60f33cb40d9ad1e9489b1f6cc576cd
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-11-12 14:55:51 +0300

    imp - Lightning bolt now renders faster
    ---
    
    We've used buffering to render the lightning bolt faster.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.ScreensaverPacks/Screensavers/Lightning.cs
+++ public/Nitrocid.Addons/Nitrocid.ScreensaverPacks/Screensavers/Lightning.cs
@@ -24,7 +24,9 @@ using KS.Drivers.RNG;
 using KS.Kernel.Threading;
 using KS.Misc.Screensaver;
 using System;
+using System.Text;
 using Terminaux.Colors;
+using Terminaux.Sequences.Builder.Types;
 
 namespace Nitrocid.ScreensaverPacks.Screensavers
 {
@@ -115,59 +117,96 @@ namespace Nitrocid.ScreensaverPacks.Screensavers
                     bool showStrike = step % 2 == 0;
                     if (showStrike)
                     {
+                        var strike = new StringBuilder();
+
                         // Draw the flashes first
+                        strike.Append(new Color(ConsoleColors.Yellow3_d7d700).VTSequenceBackground);
                         for (int y = 0; y < boltFirstHalfEndY; y++)
                         {
                             int x = (int)Math.Round(boltTopEdgeX + boltFromTopToFirstHalfX * y, MidpointRounding.ToZero);
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x + 1, y, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x, y, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x - 1, y, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
+                            strike.Append(
+                                CsiSequences.GenerateCsiCursorPosition(x + 2, y + 1) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x + 1, y + 1) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x, y + 1) +
+                                " "
+                            );
                         }
                         int xCount = 0;
                         for (int x = boltFirstHalfEndX; x < boltSecondHalfEndX; x++)
                         {
                             int y = (int)Math.Round(boltFirstHalfEndY + boltFromFirstHalfToSecondHalfY * xCount, MidpointRounding.ToZero);
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x + 1, y + 1, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x + 1, y, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x + 1, y - 1, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x, y + 1, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x, y, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x, y - 1, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x - 1, y + 1, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x - 1, y, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x - 1, y - 1, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
+                            strike.Append(
+                                CsiSequences.GenerateCsiCursorPosition(x + 2, y + 2) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x + 2, y + 1) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x + 2, y) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x + 1, y + 2) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x + 1, y + 1) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x + 1, y) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x, y + 2) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x, y + 1) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x, y) +
+                                " "
+                            );
                             xCount++;
                         }
                         int yCount = 0;
                         for (int y = boltSecondHalfEndY; y < ConsoleWrapper.WindowHeight; y++)
                         {
                             int x = (int)Math.Round(boltSecondHalfEndX + boltFromSecondHalfToBottomX * yCount, MidpointRounding.ToZero);
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x + 1, y, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x, y, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x - 1, y, Color.Empty, new Color(ConsoleColors.Yellow3_d7d700));
+                            strike.Append(
+                                CsiSequences.GenerateCsiCursorPosition(x + 2, y + 1) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x + 1, y + 1) +
+                                " " +
+                                CsiSequences.GenerateCsiCursorPosition(x, y + 1) +
+                                " "
+                            );
                             yCount++;
                         }
 
                         // Draw the lightning!
+                        strike.Append(new Color(ConsoleColors.LightYellow3).VTSequenceBackground);
                         for (int y = 0; y < boltFirstHalfEndY; y++)
                         {
                             int x = (int)Math.Round(boltTopEdgeX + boltFromTopToFirstHalfX * y, MidpointRounding.ToZero);
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x, y, Color.Empty, new Color(ConsoleColors.LightYellow3));
+                            strike.Append(
+                                CsiSequences.GenerateCsiCursorPosition(x + 1, y + 1) +
+                                " "
+                            );
                         }
                         xCount = 0;
                         for (int x = boltFirstHalfEndX; x < boltSecondHalfEndX; x++)
                         {
                             int y = (int)Math.Round(boltFirstHalfEndY + boltFromFirstHalfToSecondHalfY * xCount, MidpointRounding.ToZero);
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x, y, Color.Empty, new Color(ConsoleColors.LightYellow3));
+                            strike.Append(
+                                CsiSequences.GenerateCsiCursorPosition(x + 1, y + 1) +
+                                " "
+                            );
                             xCount++;
                         }
                         yCount = 0;
                         for (int y = boltSecondHalfEndY; y < ConsoleWrapper.WindowHeight; y++)
                         {
                             int x = (int)Math.Round(boltSecondHalfEndX + boltFromSecondHalfToBottomX * yCount, MidpointRounding.ToZero);
-                            TextWriterWhereColor.WriteWhereColorBack(" ", x, y, Color.Empty, new Color(ConsoleColors.LightYellow3));
+                            strike.Append(
+                                CsiSequences.GenerateCsiCursorPosition(x + 1, y + 1) +
+                                " "
+                            );
                             yCount++;
                         }
+
+                        // Write the rendered strike
+                        TextWriterColor.WritePlain(strike.ToString(), false);
                         ThreadManager.SleepNoBlock(LightningSettings.LightningDelay, ScreensaverDisplayer.ScreensaverDisplayerThread);
                     }
                     else
