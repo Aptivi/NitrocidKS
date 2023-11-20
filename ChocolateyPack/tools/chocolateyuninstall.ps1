d609d92e796e8f924a3378275f9059eea0a40474
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-11-20 13:32:26 +0300

    dev - Started development of 0.0.16.24
    ---
    
    Type: dev
    Breaking: False
    Doc Required: False
    Part: 1/1

--- ChocolateyPack/tools/chocolateyuninstall.ps1
+++ ChocolateyPack/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  zipFileName   = "0.0.16.23-bin.zip"
+  zipFileName   = "0.0.16.24-bin.zip"
 }
 $userProfile = $Env:USERPROFILE
 
