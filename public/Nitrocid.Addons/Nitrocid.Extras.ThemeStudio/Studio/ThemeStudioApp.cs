e35c71ced0c64caaf809d2e8a2001db18c022292
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-11-29 17:08:01 +0300

    imp - Theme Studio should use the color selector
    ---
    
    As the old color wheel is going away after Terminaux 2.0.0 releases, we need to change the theme studio so that it uses a brand new color selector.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.Extras.ThemeStudio/Studio/ThemeStudioApp.cs
+++ public/Nitrocid.Addons/Nitrocid.Extras.ThemeStudio/Studio/ThemeStudioApp.cs
@@ -172,7 +172,7 @@ namespace Nitrocid.Extras.ThemeStudio.Studio
                 {
                     KernelColorTools.LoadBack(0);
                     SelectedColorInstance = ThemeStudioTools.SelectedColors[ThemeStudioTools.SelectedColors.Keys.ElementAt(response - 1)];
-                    string ColorWheelReturn = ColorWheel.InputForColor(SelectedColorInstance).PlainSequence;
+                    string ColorWheelReturn = ColorSelector.OpenColorSelector(SelectedColorInstance).PlainSequence;
                     ThemeStudioTools.SelectedColors[ThemeStudioTools.SelectedColors.Keys.ElementAt(response - 1)] = new Color(ColorWheelReturn);
                 }
             }
