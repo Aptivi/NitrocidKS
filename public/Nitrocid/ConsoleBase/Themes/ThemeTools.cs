7912f79556e48debf021cf75b8ee03c8633a3667
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-01-11 10:42:04 +0300

    imp - SetColorsTheme() adjusted for recent changes
    ---
    
    SetColorsTheme() now clears the console with the correct background color once the theme change is committed.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid/ConsoleBase/Themes/ThemeTools.cs
+++ public/Nitrocid/ConsoleBase/Themes/ThemeTools.cs
@@ -225,7 +225,7 @@ namespace Nitrocid.ConsoleBase.Themes
                     DebugWriter.WriteDebug(DebugLevel.I, "Kernel color type {0}, setting theme color {1}...", type.ToString(), themeColor.PlainSequence);
                     KernelColorTools.KernelColors[type] = themeColor;
                 }
-                ColorTools.LoadBack();
+                ColorTools.LoadBack(KernelColorTools.GetColor(KernelColorType.Background));
                 Config.CreateConfig();
 
                 // Raise event
