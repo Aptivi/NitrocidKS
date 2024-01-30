3a5bf0ea95324b9c3af010ed13b1dcb034e408e5
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-01-30 17:46:46 +0300

    fin - Finalized chocolatey
    ---
    
    Type: fin
    Breaking: False
    Doc Required: False
    Part: 1/1

--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
 $url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.0.24.21-beta/0.0.24.21-bin.zip"
-$md5check   = "7c393e7757b98e4e14dae73fb266887f"
+$md5check   = "8fe6cb73589533a34007b1aa63a84aba"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
