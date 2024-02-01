519a77e5717dafa5b9950752e0ba3f510999d630
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-02-01 13:28:31 +0300

    dev - Started development of 0.0.24.22
    ---
    
    Type: dev
    Breaking: False
    Doc Required: False
    Part: 1/1

--- ChocolateyPack/tools/chocolateyInstall.ps1
+++ ChocolateyPack/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
-$url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.0.24.21-beta/0.0.24.21-bin.zip"
+$url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.0.24.22-beta/0.0.24.22-bin.zip"
 $md5check   = "8fe6cb73589533a34007b1aa63a84aba"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
