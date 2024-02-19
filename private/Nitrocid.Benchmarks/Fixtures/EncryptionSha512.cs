1f627813b66c380bcd1016bc75cfc3b16a16ed7d
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-02-19 14:25:37 +0300

    add - Added Nitrocid.Benchmarks
    ---
    
    This is an internal tool that allows you to benchmark various Nitrocid functions. Currently, it'll stay that way until the development of 0.1.1 starts.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Part: 1/1

---
+++ private/Nitrocid.Benchmarks/Fixtures/EncryptionSha512.cs
@@ -0,0 +1,42 @@
+﻿//
+// Nitrocid KS  Copyright (C) 2018-2024  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using BenchmarkDotNet.Attributes;
+using Nitrocid.Drivers;
+using Nitrocid.Drivers.Encryption;
+using Nitrocid.Drivers.RNG;
+
+namespace Nitrocid.Benchmarks.Fixtures
+{
+    public class EncryptionSha512 : BenchFixture
+    {
+        private readonly IEncryptionDriver encryption = DriverHandler.GetDriver<IEncryptionDriver>("SHA512");
+        private readonly IRandomDriver random = DriverHandler.GetDriver<IRandomDriver>("Default");
+
+        [Benchmark]
+        public override void Run()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                int num = random.Random();
+                encryption.GetEncryptedString($"#{num}");
+            }
+        }
+    }
+}
