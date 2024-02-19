e4f9227966f287d6ea2604ed6811c11d0c677b4d
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-02-19 14:50:41 +0300

    imp - Template pack moved to main Nitrocid project
    ---
    
    We've moved the template pack to the main Nitrocid project. We've also added a logo to make it more recognizable.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

---
+++ public/Nitrocid.Templates/templates/KSMod/Command.cs
@@ -0,0 +1,12 @@
+﻿using Nitrocid.Shell.ShellBase.Commands;
+
+namespace KSMod
+{
+    internal class TuiCommand : BaseCommand, ICommand
+    {
+        public override int Execute(CommandParameters parameters, ref string variableValue)
+        {
+            return 0;
+        }
+    }
+}
