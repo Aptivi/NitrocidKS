9fc5faada731845ed5227f41dee4072769a36e9d
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-02-20 10:17:13 +0300

    und - Removed hash checking from Chocolatey
    ---
    
    Type: und
    Breaking: False
    Doc Required: False
    Part: 1/1

--- tools/chocopack-lite/tools/chocolateyInstall.ps1
+++ tools/chocopack-lite/tools/chocolateyInstall.ps1
@@ -2,13 +2,11 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "kslite"
 $url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.1.0/0.1.0-bin-lite.zip"
-$md5check   = "55f6eab726e00230704127608ffd0ca3"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
 Write-Output "<*> Package Name: $pkgName"
 Write-Output "<*> URL: $url"
-Write-Output "<*> Expected MD5 Sum: $md5check"
 Write-Output "<+> Configuration will be automatically generated on startup."
 
-Install-ChocolateyZipPackage $pkgName $url $toolsDir -ChecksumType "md5" -Checksum $md5check
+Install-ChocolateyZipPackage $pkgName $url $toolsDir
