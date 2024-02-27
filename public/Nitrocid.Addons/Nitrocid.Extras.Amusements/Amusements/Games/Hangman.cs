7103e5ec2eb77cf8bc7f9ab81d9e0baa6c9cc75c
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-02-27 20:48:35 +0300

    imp - Hangman now uses Screen
    ---
    
    Hangman now uses Terminaux's screen feature to allow resizing.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.Extras.Amusements/Amusements/Games/Hangman.cs
+++ public/Nitrocid.Addons/Nitrocid.Extras.Amusements/Amusements/Games/Hangman.cs
@@ -29,6 +29,8 @@ using Terminaux.Colors;
 using Textify.Words;
 using Terminaux.Base;
 using Terminaux.Reader;
+using Terminaux.Base.Buffered;
+using Terminaux.Inputs.Styles.Infobox;
 
 namespace Nitrocid.Extras.Amusements.Amusements.Games
 {
@@ -40,6 +42,10 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
         /// </summary>
         public static void InitializeHangman(HangmanDifficulty difficulty)
         {
+            // Clear the screen
+            ConsoleWrapper.CursorVisible = false;
+            ColorTools.LoadBack();
+
             // Get a random word and populate some variables
             string RandomWord = WordManager.GetRandomWord();
             bool hung = false;
@@ -47,10 +53,17 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
             List<char> gotChars = [];
             List<char> wrongChars = [];
 
-            while (!hung)
+            // Add a buffer
+            Screen screen = new();
+            ScreenPart part = new();
+
+            // Add a UI rendering logic
+            Color hangerSeq = new(RandomDriver.Random(255), RandomDriver.Random(255), RandomDriver.Random(255));
+            Color wrongSeq = new(RandomDriver.Random(255), RandomDriver.Random(255), RandomDriver.Random(255));
+            Color underscoresSeq = new(RandomDriver.Random(255), RandomDriver.Random(255), RandomDriver.Random(255));
+            part.AddDynamicText(() =>
             {
-                ConsoleWrapper.CursorVisible = false;
-                ConsoleWrapper.Clear();
+                StringBuilder builder = new();
 
                 // Draw how many underscores to place
                 var underscoresRender = new StringBuilder();
@@ -58,17 +71,19 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
                     underscoresRender.Append(gotChars.Contains(RandomWord[i]) ? $"{RandomWord[i]} " : "_ ");
                 int underscoresPosX = ConsoleWrapper.WindowWidth / 2 - underscoresRender.Length / 2;
                 int underscoresPosY = ConsoleWrapper.WindowHeight - 2;
-                Color underscoresSeq = new(RandomDriver.Random(255), RandomDriver.Random(255), RandomDriver.Random(255));
                 DebugWriter.WriteDebug(DebugLevel.I, "{0} cells for underscore length", underscoresRender.Length);
-                TextWriterWhereColor.WriteWhereColor(underscoresRender.ToString(), underscoresPosX, underscoresPosY, underscoresSeq);
+                builder.Append(
+                    TextWriterWhereColor.RenderWhereColor(underscoresRender.ToString(), underscoresPosX, underscoresPosY, underscoresSeq)
+                );
 
                 // Draw the wrong characters written
                 string wrongRender = string.Join(" ", wrongChars);
                 int wrongPosX = ConsoleWrapper.WindowWidth / 2 - wrongRender.Length / 2;
                 int wrongPosY = ConsoleWrapper.WindowHeight - 4;
-                Color wrongSeq = new(RandomDriver.Random(255), RandomDriver.Random(255), RandomDriver.Random(255));
                 DebugWriter.WriteDebug(DebugLevel.I, "{0} cells for {1} wrong letters", wrongRender.Length, wrongChars.Count);
-                TextWriterWhereColor.WriteWhereColor(wrongRender.ToString(), wrongPosX, wrongPosY, wrongSeq);
+                builder.Append(
+                    TextWriterWhereColor.RenderWhereColor(wrongRender.ToString(), wrongPosX, wrongPosY, wrongSeq)
+                );
 
                 // Draw the hanger
                 string hangerSprite =
@@ -84,8 +99,9 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
                 ;
                 int hangerPosX = 3;
                 int hangerPosY = 2;
-                Color hangerSeq = new(RandomDriver.Random(255), RandomDriver.Random(255), RandomDriver.Random(255));
-                TextWriterWhereColor.WriteWhereColor(hangerSprite, hangerPosX, hangerPosY, hangerSeq);
+                builder.Append(
+                    TextWriterWhereColor.RenderWhereColor(hangerSprite, hangerPosX, hangerPosY, hangerSeq)
+                );
 
                 // Now, draw the hung man based on attempts
                 if (currentAttempt >= 1)
@@ -93,45 +109,69 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
                     // Draw the head
                     int headPosX = 14;
                     int headPosY = 5;
-                    TextWriterWhereColor.WriteWhereColor("*", headPosX, headPosY, hangerSeq);
+                    builder.Append(
+                        TextWriterWhereColor.RenderWhereColor("*", headPosX, headPosY, hangerSeq)
+                    );
                 }
                 if (currentAttempt >= 2)
                 {
                     // Draw the body
                     int bodyPosX = 14;
                     int bodyPosY = 6;
-                    TextWriterWhereColor.WriteWhereColor("|", bodyPosX, bodyPosY, hangerSeq);
-                    TextWriterWhereColor.WriteWhereColor("|", bodyPosX, bodyPosY + 1, hangerSeq);
+                    builder.Append(
+                        TextWriterWhereColor.RenderWhereColor("|", bodyPosX, bodyPosY, hangerSeq) +
+                        TextWriterWhereColor.RenderWhereColor("|", bodyPosX, bodyPosY + 1, hangerSeq)
+                    );
                 }
                 if (currentAttempt >= 3)
                 {
                     // Draw the first hand
                     int handPosX = 13;
                     int handPosY = 6;
-                    TextWriterWhereColor.WriteWhereColor("-", handPosX, handPosY, hangerSeq);
+                    builder.Append(
+                        TextWriterWhereColor.RenderWhereColor("-", handPosX, handPosY, hangerSeq)
+                    );
                 }
                 if (currentAttempt >= 4)
                 {
                     // Draw the second hand
                     int handPosX = 15;
                     int handPosY = 6;
-                    TextWriterWhereColor.WriteWhereColor("-", handPosX, handPosY, hangerSeq);
+                    builder.Append(
+                        TextWriterWhereColor.RenderWhereColor("-", handPosX, handPosY, hangerSeq)
+                    );
                 }
                 if (currentAttempt >= 5)
                 {
                     // Draw the first leg
                     int legPosX = 13;
                     int legPosY = 8;
-                    TextWriterWhereColor.WriteWhereColor("/", legPosX, legPosY, hangerSeq);
+                    builder.Append(
+                        TextWriterWhereColor.RenderWhereColor("/", legPosX, legPosY, hangerSeq)
+                    );
                 }
                 if (currentAttempt >= 6)
                 {
                     // Draw the second leg
                     int legPosX = 15;
                     int legPosY = 8;
-                    TextWriterWhereColor.WriteWhereColor("\\", legPosX, legPosY, hangerSeq);
+                    builder.Append(
+                        TextWriterWhereColor.RenderWhereColor("\\", legPosX, legPosY, hangerSeq)
+                    );
                     hung = true;
                 }
+
+                // Return the result
+                return builder.ToString();
+            });
+            screen.AddBufferedPart("Hangman - UI", part);
+            ScreenTools.SetCurrent(screen);
+
+            // Main game loop
+            while (!hung)
+            {
+                // Render it
+                ScreenTools.Render();
 
                 // Check to see if we won the game
                 bool won = false;
@@ -148,21 +188,13 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
                 if (hung)
                 {
                     // Lost the game
-                    string hungStr = Translate.DoTranslation("You're hung!");
-                    int hungPosX = ConsoleWrapper.WindowWidth / 2 - hungStr.Length / 2;
-                    int hungPosY = ConsoleWrapper.WindowHeight - 6;
-                    TextWriterWhereColor.WriteWhereColor(hungStr, hungPosX, hungPosY, hangerSeq);
-                    Thread.Sleep(5000);
+                    InfoBoxColor.WriteInfoBox(Translate.DoTranslation("You're hung!"));
                 }
                 else if (won)
                 {
                     // Won the game
                     hung = true;
-                    string wonStr = Translate.DoTranslation("You win!");
-                    int wonPosX = ConsoleWrapper.WindowWidth / 2 - wonStr.Length / 2;
-                    int wonPosY = ConsoleWrapper.WindowHeight - 6;
-                    TextWriterWhereColor.WriteWhereColor(wonStr, wonPosX, wonPosY, hangerSeq);
-                    Thread.Sleep(5000);
+                    InfoBoxColor.WriteInfoBox(Translate.DoTranslation("You win!"));
                 }
                 else
                 {
@@ -197,8 +229,9 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
             }
 
             // Clear after ourselves
+            ScreenTools.UnsetCurrent(screen);
             ConsoleWrapper.CursorVisible = true;
-            ConsoleWrapper.Clear();
+            ColorTools.LoadBack();
         }
 
     }
