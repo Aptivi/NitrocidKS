0f99c25f4faa92ad35f40065dab535ab7683e104
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-03-08 10:09:46 +0300

    upd - Updated template for 0.1.0 Final
    ---
    
    This was overlooked, so we've decided to update the ModName.cs template file.
    
    ---
    
    Type: upd
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSMod/ModName.cs
+++ public/Nitrocid.Templates/templates/KSMod/ModName.cs
@@ -10,7 +10,7 @@ namespace KSMod
         public string Name { get; set; } = "My Mod";
         public string Version { get; set; } = "1.0.0";
 
-        public Version MinimumSupportedApiVersion => new(3, 0, 25, 411);
+        public Version MinimumSupportedApiVersion => new(3, 0, 25, 437);
 
         public ReadOnlyDictionary<string, Delegate> PubliclyAvailableFunctions => null;
 
