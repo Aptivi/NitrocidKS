111d3d49fb57355839dc40dae4ae0275445291b0
Aptivi <eoflaoevicecity@gmail.com>
2024-06-07 11:54:47 +0300

    imp - Indicate color changes in the theme studio
    ---
    
    We need to be able to tell the user that they've changed a color type declaration in the theme studio to be able to more easily tell what colors have changed.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.Extras.ThemeStudio/Studio/ThemeStudioApp.cs
+++ public/Nitrocid.Addons/Nitrocid.Extras.ThemeStudio/Studio/ThemeStudioApp.cs
@@ -54,6 +54,7 @@ namespace Nitrocid.Extras.ThemeStudio.Studio
             // Maximum options is number of kernel colors plus more options
             int MaximumOptions = ThemeStudioTools.SelectedColors.Count + 9;
             var StudioExiting = false;
+            var originalColors = new Dictionary<KernelColorType, Color>(ThemeStudioTools.SelectedColors);
 
             while (!StudioExiting)
             {
@@ -64,9 +65,10 @@ namespace Nitrocid.Extras.ThemeStudio.Studio
                 List<InputChoiceInfo> choices = [];
                 for (int key = 0; key < ThemeStudioTools.SelectedColors.Count; key++)
                 {
-                    var colorType = ThemeStudioTools.SelectedColors.Keys.ElementAt(key);
-                    var color = ThemeStudioTools.SelectedColors.Values.ElementAt(key).PlainSequence;
-                    choices.Add(new InputChoiceInfo($"{key + 1}", $"{colorType}: [{color}] "));
+                    var colorType = colors.Keys.ElementAt(key);
+                    var color = colors.Values.ElementAt(key).PlainSequence;
+                    bool changed = originalColors.Values.ElementAt(key) != colors.Values.ElementAt(key);
+                    choices.Add(new InputChoiceInfo($"{key + 1}{(changed ? "*" : "")}", $"{colorType}: [{color}]"));
                 }
                 List<InputChoiceInfo> altChoices =
                 [
