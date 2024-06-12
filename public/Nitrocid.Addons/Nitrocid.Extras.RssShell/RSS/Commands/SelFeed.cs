7ede144490f8aa3c734ed49582c151a997846710
Aptivi <eoflaoevicecity@gmail.com>
2024-06-12 12:15:15 +0300

    add - doc - Restored feed selection as feed searcher
    ---
    
    We've restored feed selection as feed searching facility.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Part: 1/1

---
+++ public/Nitrocid.Addons/Nitrocid.Extras.RssShell/RSS/Commands/SelFeed.cs
@@ -0,0 +1,55 @@
+﻿//
+// Nitrocid KS  Copyright (C) 2018-2024  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System.Linq;
+using Nitrocid.Extras.RssShell.Tools;
+using Nettify.Rss.Instance;
+using Nitrocid.Shell.ShellBase.Commands;
+using Nitrocid.ConsoleBase.Writers;
+using Terminaux.Writer.ConsoleWriters;
+using Nitrocid.ConsoleBase.Colors;
+using Textify.General;
+using Terminaux.Base.Extensions;
+using Nettify.Rss.Searcher;
+
+namespace Nitrocid.Extras.RssShell.RSS.Commands
+{
+    /// <summary>
+    /// Searches the feeds
+    /// </summary>
+    /// <remarks>
+    /// If you want to search the feed library for a feed, you can use this command.
+    /// </remarks>
+    class SelFeedCommand : BaseCommand, ICommand
+    {
+
+        public override int Execute(CommandParameters parameters, ref string variableValue)
+        {
+            var foundFeeds = SearcherTools.GetRssFeeds(parameters.ArgumentsList[0]);
+            foreach (var feed in foundFeeds)
+            {
+                TextWriters.Write("- {0}: ", false, KernelColorType.ListEntry, feed.Title);
+                TextWriters.Write(feed.FeedId, true, KernelColorType.ListValue);
+                TextWriterColor.Write("    {0}", feed.Description.SplitNewLines()[0].Truncate(200));
+            }
+            return 0;
+        }
+
+    }
+}
