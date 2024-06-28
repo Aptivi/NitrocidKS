bc6e6711ebe250f385f6eb5758c5dd5febdf9e21
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-06-28 15:20:32 +0300

    imp - Finished the picture preview
    ---
    
    We've finished the picture preview implementation. We just need to fix a bug regarding handling the VT sequences.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.Extras.Images/Commands/Preview.cs
+++ public/Nitrocid.Addons/Nitrocid.Extras.Images/Commands/Preview.cs
@@ -17,9 +17,18 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+using ImageMagick;
 using Nitrocid.ConsoleBase.Colors;
 using Nitrocid.ConsoleBase.Writers;
+using Nitrocid.Files.Editors.TextEdit;
+using Nitrocid.Files.Operations.Querying;
+using Nitrocid.Languages;
 using Nitrocid.Shell.ShellBase.Commands;
+using System.Linq;
+using System.Text;
+using Terminaux.Colors;
+using Terminaux.Images;
+using Textify.General;
 
 namespace Nitrocid.Extras.Images.Commands
 {
@@ -28,7 +37,41 @@ namespace Nitrocid.Extras.Images.Commands
 
         public override int Execute(CommandParameters parameters, ref string variableValue)
         {
-            TextWriters.Write("Coming soon...", KernelColorType.Error);
+            string path = parameters.ArgumentsList[0];
+            if (string.IsNullOrEmpty(path))
+            {
+                TextWriters.Write(Translate.DoTranslation("Path to the image is not provided."), KernelColorType.Error);
+                return 39;
+            }
+            if (!Checking.FileExists(path))
+            {
+                TextWriters.Write(Translate.DoTranslation("Image file doesn't exist."), KernelColorType.Error);
+                return 39;
+            }
+
+            // Currently, Terminaux.Images doesn't provide a way to get the image's accurate dimensions, so we need to
+            // use the colors array to get the dimension so that we can preview the image 100%.
+            var colors = ImageProcessor.GetColorsFromImage(path);
+            (int width, int height) = (colors.GetLength(0), colors.GetLength(1));
+
+            // Unfortunately, Terminaux.Images assumes that we need to render in the console somewhere by specifying the
+            // x and y positions, but this is out of scope because we need an interactive text viewer, and it doesn't
+            // deal with such things. Use the colors array to formulate a sequence that describes the whole image in
+            // scanlines.
+            StringBuilder builder = new();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var color = colors[x, y];
+                    builder.Append(color.VTSequenceBackgroundTrueColor + " ");
+                }
+                builder.AppendLine();
+            }
+
+            // Use the interactive text viewer to describe the image.
+            string[] lines = builder.ToString().SplitNewLines();
+            TextViewInteractive.OpenInteractive([.. lines]);
             return 0;
         }
 
