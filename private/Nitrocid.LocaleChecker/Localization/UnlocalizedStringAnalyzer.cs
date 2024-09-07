86e308fd45222e7e4d31b8e91aaae384e86c9f4d
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-09-07 23:48:31 +0300

    add - Added scaffolding for Roslyn-based translati...
    ...on detection
    
    ---
    
    Using Roslyn analyzers, we're going to analyze every single Nitrocid source code file to check for any untranslated strings.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ private/Nitrocid.LocaleChecker/Localization/UnlocalizedStringAnalyzer.cs
@@ -0,0 +1,65 @@
+﻿//
+// Nitrocid KS  Copyright (C) 2018-2024  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Globalization;
+using System.Collections.Immutable;
+using Nitrocid.LocaleChecker.Resources;
+
+namespace Nitrocid.LocaleChecker.Localization
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class UnlocalizedStringAnalyzer : DiagnosticAnalyzer
+    {
+        // Some constants
+        public const string DiagnosticId = "NLOC0001";
+        private const string Category = "Localization";
+
+        // Some strings
+        private static readonly LocalizableString Title =
+            new LocalizableResourceString(nameof(AnalyzerResources.UnlocalizedStringAnalyzerTitle), AnalyzerResources.ResourceManager, typeof(AnalyzerResources));
+        private static readonly LocalizableString MessageFormat =
+            new LocalizableResourceString(nameof(AnalyzerResources.UnlocalizedStringAnalyzerMessageFormat), AnalyzerResources.ResourceManager, typeof(AnalyzerResources));
+        private static readonly LocalizableString Description =
+            new LocalizableResourceString(nameof(AnalyzerResources.UnlocalizedStringAnalyzerDescription), AnalyzerResources.ResourceManager, typeof(AnalyzerResources));
+
+        // A rule
+        private static readonly DiagnosticDescriptor Rule =
+            new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        // Supported diagnostics
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+            ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeLocalization, SyntaxKind.SimpleMemberAccessExpression);
+        }
+
+        private static void AnalyzeLocalization(SyntaxNodeAnalysisContext context)
+        {
+            // TODO: Populate this to replace Nitrocid.LocaleCheck.
+        }
+    }
+}
