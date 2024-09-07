514682b26cd6137e9fdbef5cd69dde44bc36c828
Aptivi <eoflaoevicecity@gmail.com>
2024-09-07 17:11:33 +0300

    fix - Fixed ShipDuet transparency
    ---
    
    Now, ShipDuet should support full console transparency.
    
    ---
    
    Type: fix
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.Extras.Amusements/Amusements/Games/ShipDuet.cs
+++ public/Nitrocid.Addons/Nitrocid.Extras.Amusements/Amusements/Games/ShipDuet.cs
@@ -193,12 +193,12 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
                     {
                         if (y != SpaceshipHeightPlayer1)
                             buffer.Append(
-                                new Color(ConsoleColors.Black).VTSequenceBackground +
+                                ColorTools.RenderSetConsoleColor(new Color(ConsoleColors.Black), true) +
                                 TextWriterWhereColor.RenderWhere(" ", 0, y)
                             );
                         if (y != SpaceshipHeightPlayer2)
                             buffer.Append(
-                                new Color(ConsoleColors.Black).VTSequenceBackground +
+                                ColorTools.RenderSetConsoleColor(new Color(ConsoleColors.Black), true) +
                                 TextWriterWhereColor.RenderWhere(" ", ConsoleWrapper.WindowWidth - 1, y)
                             );
                     }
@@ -207,7 +207,7 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
                     for (int Bullet = 0; Bullet <= BulletsPlayer1.Count - 1; Bullet++)
                     {
                         buffer.Append(
-                            new Color(ConsoleColors.Black).VTSequenceBackground +
+                            ColorTools.RenderSetConsoleColor(new Color(ConsoleColors.Black), true) +
                             TextWriterWhereColor.RenderWhere(" ", BulletsPlayer1[Bullet].Item1, BulletsPlayer1[Bullet].Item2)
                         );
                         int BulletX = BulletsPlayer1[Bullet].Item1 + 1;
@@ -219,7 +219,7 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
                     for (int Bullet = 0; Bullet <= BulletsPlayer2.Count - 1; Bullet++)
                     {
                         buffer.Append(
-                            new Color(ConsoleColors.Black).VTSequenceBackground +
+                            ColorTools.RenderSetConsoleColor(new Color(ConsoleColors.Black), true) +
                             TextWriterWhereColor.RenderWhere(" ", BulletsPlayer2[Bullet].Item1, BulletsPlayer2[Bullet].Item2)
                         );
                         int BulletX = BulletsPlayer2[Bullet].Item1 - 1;
@@ -231,7 +231,7 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
                     for (int Star = 0; Star <= Stars.Count - 1; Star++)
                     {
                         buffer.Append(
-                            new Color(ConsoleColors.Black).VTSequenceBackground +
+                            ColorTools.RenderSetConsoleColor(new Color(ConsoleColors.Black), true) +
                             TextWriterWhereColor.RenderWhere(" ", Stars[Star].Item1, Stars[Star].Item2)
                         );
                         int StarX = Stars[Star].Item1 - 1;
@@ -299,7 +299,7 @@ namespace Nitrocid.Extras.Amusements.Amusements.Games
                         int StarY = Star.Item2;
                         buffer.Append(
                             new Color(ConsoleColors.White).VTSequenceForeground +
-                            new Color(ConsoleColors.Black).VTSequenceBackground +
+                            ColorTools.RenderSetConsoleColor(new Color(ConsoleColors.Black), true) +
                             TextWriterWhereColor.RenderWhere(Convert.ToString(StarSymbol), StarX, StarY, false)
                         );
                     }
