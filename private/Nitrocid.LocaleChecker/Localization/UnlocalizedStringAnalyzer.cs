f1f10d42e95c0898f3b21a6e7d98911db1781ae0
Aptivi <eoflaoevicecity@gmail.com>
2024-09-08 13:02:06 +0300

    imp - prt - Populated lanugage analyzer (pt. 1)
    ---
    
    The analysis code has been added. Not done.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/2

--- private/Nitrocid.LocaleChecker/Localization/UnlocalizedStringAnalyzer.cs
+++ private/Nitrocid.LocaleChecker/Localization/UnlocalizedStringAnalyzer.cs
@@ -24,12 +24,24 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using System.Globalization;
 using System.Collections.Immutable;
 using Nitrocid.LocaleChecker.Resources;
+using System.Diagnostics;
+using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.Text;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace Nitrocid.LocaleChecker.Localization
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class UnlocalizedStringAnalyzer : DiagnosticAnalyzer
     {
+        // This assembly
+        private readonly Assembly thisAssembly = typeof(UnlocalizedStringAnalyzer).Assembly;
+
         // Some constants
         public const string DiagnosticId = "NLOC0001";
         private const string Category = "Localization";
@@ -46,6 +58,9 @@ namespace Nitrocid.LocaleChecker.Localization
         private static readonly DiagnosticDescriptor Rule =
             new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
+        // English localization list
+        private readonly HashSet<string> localizationList = [];
+
         // Supported diagnostics
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
             ImmutableArray.Create(Rule);
@@ -54,12 +69,64 @@ namespace Nitrocid.LocaleChecker.Localization
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
-            context.RegisterSyntaxNodeAction(AnalyzeLocalization, SyntaxKind.SimpleMemberAccessExpression);
+            context.RegisterCompilationStartAction(PopulateEnglishLocalizations);
+            Debugger.Launch();
+        }
+
+        private void PopulateEnglishLocalizations(CompilationStartAnalysisContext context)
+        {
+            // Find the English JSON stream and open it.
+            var stream = thisAssembly.GetManifestResourceStream("Nitrocid.LocaleChecker.eng.json") ??
+                throw new Exception("Opening the eng.json resource stream has failed.");
+            var reader = new StreamReader(stream);
+            var jsonReader = new JsonTextReader(reader);
+            var document = JToken.Load(jsonReader) ??
+                throw new Exception("Unable to parse JSON for English localizations.");
+            var localizations = document["Localizations"]?.Values<string>() ??
+                throw new Exception("Unable to get localizations.");
+
+            // Now, add all localizations to a separate array
+            foreach (var localization in localizations)
+            {
+                if (localization is null)
+                    throw new Exception("There is no localization.");
+                string localizationString = localization.ToString();
+                localizationList.Add(localizationString);
+            }
+
+            // Register the localization analysis action
+            context.RegisterSyntaxNodeAction(AnalyzeLocalization, SyntaxKind.InvocationExpression);
         }
 
         private static void AnalyzeLocalization(SyntaxNodeAnalysisContext context)
         {
-            // TODO: Populate this to replace Nitrocid.LocaleCheck.
+            // Check for argument
+            var exp = (InvocationExpressionSyntax)context.Node;
+            var args = exp.ArgumentList.Arguments;
+            if (args.Count < 1)
+                return;
+
+            // Now, check for the Translate.DoTranslation() call
+            if (exp.Expression is not MemberAccessExpressionSyntax expMaes)
+                return;
+            if (expMaes.Expression is IdentifierNameSyntax expIdentifier && expMaes.Name is IdentifierNameSyntax identifier)
+            {
+                // Verify that we're dealing with Translate.DoTranslation()
+                var location = context.Node.GetLocation();
+                var idExpression = expIdentifier.Identifier.Text;
+                var idName = identifier.Identifier.Text;
+                if (idExpression == "Translate" && idName == "DoTranslation")
+                {
+                    // Now, get the string representation from the argument count and compare it with the list of translations.
+                    // You'll notice that we sometimes call Translate.DoTranslation() with a variable instead of a string, so
+                    // check that first, because they're usually obtained from a string representation usually prefixed with
+                    // either the /* Localizable */ comment or in individual kernel resources. However, the resources don't
+                    // have a prefix, so the key names alone are enough.
+
+                    var diagnostic = Diagnostic.Create(Rule, location);
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
         }
     }
 }
