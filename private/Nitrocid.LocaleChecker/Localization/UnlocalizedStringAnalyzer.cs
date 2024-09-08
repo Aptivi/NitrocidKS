6788e0f1d481f90272137808162f59538354370b
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-09-08 15:54:05 +0300

    imp - Locale checker checks resources once
    ---
    
    We've added a new analyzer that also checks for unlocalized strings in all the resources.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- private/Nitrocid.LocaleChecker/Localization/UnlocalizedStringAnalyzer.cs
+++ private/Nitrocid.LocaleChecker/Localization/UnlocalizedStringAnalyzer.cs
@@ -56,7 +56,7 @@ namespace Nitrocid.LocaleChecker.Localization
 
         // A rule
         private static readonly DiagnosticDescriptor Rule =
-            new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+            new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description, customTags: ["CompilationEnd"]);
 
         // English localization list
         private static readonly HashSet<string> localizationList = [];
@@ -96,6 +96,7 @@ namespace Nitrocid.LocaleChecker.Localization
             // Register the localization analysis action
             context.RegisterSyntaxNodeAction(AnalyzeLocalization, SyntaxKind.InvocationExpression);
             context.RegisterSyntaxNodeAction(AnalyzeImplicitLocalization, SyntaxKind.CompilationUnit);
+            context.RegisterCompilationEndAction(AnalyzeResourceLocalization);
         }
 
         private static void AnalyzeLocalization(SyntaxNodeAnalysisContext context)
@@ -197,6 +198,83 @@ namespace Nitrocid.LocaleChecker.Localization
                             Process(literalText);
                         else if (child is ArgumentSyntax argument && argument.Expression is LiteralExpressionSyntax literalArgText)
                             Process(literalArgText);
+                    }
+                }
+            }
+        }
+
+        private void AnalyzeResourceLocalization(CompilationAnalysisContext context)
+        {
+            // Just launch once
+            if (context.Compilation.Assembly.Name != "Nitrocid")
+                return;
+
+            // Open every resource except the English translations file and the analyzer string resources
+            var resourceNames = thisAssembly.GetManifestResourceNames().Except([
+                "Nitrocid.LocaleChecker.eng.json",
+                "Nitrocid.LocaleChecker.Resources.AnalyzerResources.resources",
+            ]);
+            foreach (var resourceName in resourceNames)
+            {
+                // Open the resource and load it to a JSON token instance
+                var stream = thisAssembly.GetManifestResourceStream(resourceName) ??
+                    throw new Exception($"Opening the {resourceName} resource stream has failed.");
+                var reader = new StreamReader(stream);
+                var jsonReader = new JsonTextReader(reader);
+                var document = JToken.Load(jsonReader) ??
+                    throw new Exception($"Unable to parse JSON for {resourceName}.");
+
+                // Determine if this is a theme or a settings entries list
+                var themeMetadata = document.Type == JTokenType.Array ? null : document["Metadata"];
+                if (themeMetadata is not null)
+                {
+                    // It's a theme. Get its description and its localizable boolean value
+                    string description = ((string?)themeMetadata["Description"] ?? "").Replace("\\\"", "\"");
+                    bool localizable = (bool?)themeMetadata["Localizable"] ?? false;
+                    if (!string.IsNullOrEmpty(description) && localizable && !localizationList.Contains(description))
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, null, description);
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
+                else if (document.Type == JTokenType.Array)
+                {
+                    // It's likely a settings entry list, but verify
+                    foreach (var settingsEntryList in document)
+                    {
+                        // Check the description and the display
+                        string description = ((string?)settingsEntryList["Desc"] ?? "").Replace("\\\"", "\"");
+                        string displayAs = ((string?)settingsEntryList["DisplayAs"] ?? "").Replace("\\\"", "\"");
+                        if (!string.IsNullOrEmpty(description) && !localizationList.Contains(description))
+                        {
+                            var diagnostic = Diagnostic.Create(Rule, null, description);
+                            context.ReportDiagnostic(diagnostic);
+                        }
+                        if (!string.IsNullOrEmpty(displayAs) && !localizationList.Contains(displayAs))
+                        {
+                            var diagnostic = Diagnostic.Create(Rule, null, displayAs);
+                            context.ReportDiagnostic(diagnostic);
+                        }
+
+                        // Now, check the keys
+                        JArray? keys = (JArray?)settingsEntryList["Keys"];
+                        if (keys is null || keys.Count == 0)
+                            continue;
+                        foreach (var key in keys)
+                        {
+                            string keyName = ((string?)key["Name"] ?? "").Replace("\\\"", "\"");
+                            string keyDesc = ((string?)key["Description"] ?? "").Replace("\\\"", "\"");
+                            if (!string.IsNullOrEmpty(keyName) && !localizationList.Contains(keyName))
+                            {
+                                var diagnostic = Diagnostic.Create(Rule, null, keyName);
+                                context.ReportDiagnostic(diagnostic);
+                            }
+                            if (!string.IsNullOrEmpty(keyDesc) && !localizationList.Contains(keyDesc))
+                            {
+                                var diagnostic = Diagnostic.Create(Rule, null, keyDesc);
+                                context.ReportDiagnostic(diagnostic);
+                            }
+                        }
                     }
                 }
             }
