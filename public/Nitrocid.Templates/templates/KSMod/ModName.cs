956a179974fdfac714f3558acf0e0644e337778c
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-10-03 23:09:06 +0300

    fin - Finalized changelogs
    ---
    
    Type: fin
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSMod/ModName.cs
+++ public/Nitrocid.Templates/templates/KSMod/ModName.cs
@@ -10,7 +10,7 @@ namespace KSMod
         public string Name { get; set; } = "My Mod";
         public string Version { get; set; } = "1.0.0";
 
-        public Version MinimumSupportedApiVersion => new(3, 0, 26, 51);
+        public Version MinimumSupportedApiVersion => new(3, 0, 26, 52);
 
         public ReadOnlyDictionary<string, Delegate> PubliclyAvailableFunctions => null;
 
