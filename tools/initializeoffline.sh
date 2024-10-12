00c4ccd095fdba4d732945207d24e6603d086f1c
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-10-12 21:56:59 +0300

    ref - Cleaned up error handling for scripts (.sh)
    ---
    
    Type: ref
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/initializeoffline.sh
+++ tools/initializeoffline.sh
@@ -1,26 +1,54 @@
 #!/bin/bash
 
+#    Nitrocid KS  Copyright (C) 2018-2024  Aptivi
+#
+#    This file is part of Nitrocid KS
+#
+#    Nitrocid KS is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Nitrocid KS is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Convenience functions
+checkerror() {
+    if [ $1 != 0 ]
+    then
+        printf "$2 - Error $1\n" >&2
+        exit $1
+    fi
+}
+
 # Restore packages
 echo "- Restoring packages..."
 echo "  - HOME=`pwd`/nuget dotnet restore Nitrocid.sln"
 HOME=`pwd`/nuget dotnet restore Nitrocid.sln
-if [ "$?" -ne 0 ]; then
-	exit $?
-fi
+checkerror $? "  - Failed to restore NuGet packages"
 
 # Copy dependencies to deps
 echo "- Copying dependencies to deps..."
 echo "  - mkdir deps"
 mkdir deps
+checkerror $? "  - Failed to mkdir deps"
 echo "  - cp nuget/.nuget/packages/*/*/*.nupkg ./deps/"
 cp nuget/.nuget/packages/*/*/*.nupkg ./deps/
+checkerror $? "  - Failed to copy deps"
 echo "  - rm -rf nuget"
 rm -rf nuget
+checkerror $? "  - Failed to remove nuget folder"
 
 # Copy NuGet.config for offline use
 echo "- Copying NuGet.config..."
 echo "  - cp tools/OfflineNuGet.config ./NuGet.config"
 cp tools/OfflineNuGet.config ./NuGet.config
+checkerror $? "  - Failed to copy offline NuGet config"
 
 echo "- You should be able to build offline!"
 
--- tools/push.sh
+++ tools/push.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
+
+#    Nitrocid KS  Copyright (C) 2018-2024  Aptivi
+#
+#    This file is part of Nitrocid KS
+#
+#    Nitrocid KS is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Nitrocid KS is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Convenience functions
+checkerror() {
+    if [ $1 != 0 ]
+    then
+        printf "$2 - Error $1\n" >&2
+        exit $1
+    fi
+}
+
 # This script pushes. Use when you have dotnet installed.
 dotnetpath=`which dotnet`
-if [ ! $? == 0 ]; then
-	echo dotnet is not found.
-	exit 1
-fi
+checkerror $? "dotnet is not found"
 
 # Push packages
 echo Pushing packages...
 find ../public/Nitrocid/KSBuild/ -maxdepth 1 -type f -name "*.nupkg" -exec dotnet nuget push {} --api-key $NUGET_APIKEY --source "nuget.org" \;
-if [ ! $? == 0 ]; then
-	echo Push failed.
-	exit 1
-fi
+checkerror $? "Failed to push"
 find ../public/Nitrocid/KSAnalyzer/ -maxdepth 1 -type f -name "*.nupkg" -exec dotnet nuget push {} --api-key $NUGET_APIKEY --source "nuget.org" \;
-if [ ! $? == 0 ]; then
-	echo Push failed.
-	exit 1
-fi
+checkerror $? "Failed to push"
 
 # Inform success
 echo Push successful.
