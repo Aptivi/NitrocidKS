76ff918276e478ae124c7b70fb455093d87cc771
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-12-02 23:19:36 +0300

    rem - brk|doc - Removed unused ConfigCategory
    ---
    
    Although technically not backportable, this commit is an exception,
    because ConfigCategory is left unused across the 0.1.0, 0.1.1, and 0.1.2
    versions, so we've decided to remove it, making a breaking change.
    
    ---
    
    The following commits or tags are used for this backport:
      - cb5287e8482b4f8c9be87ad29604a15fb0d9562c
    
    ---
    
    Type: rem
    Breaking: True
    Doc Required: True
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSMod/ModName.cs
+++ public/Nitrocid.Templates/templates/KSMod/ModName.cs
@@ -10,7 +10,7 @@ namespace KSMod
         public string Name { get; set; } = "My Mod";
         public string Version { get; set; } = "1.0.0";
 
-        public Version MinimumSupportedApiVersion => new(3, 0, 26, 52);
+        public Version MinimumSupportedApiVersion => new(3, 0, 26, 53);
 
         public ReadOnlyDictionary<string, Delegate> PubliclyAvailableFunctions => null;
 
