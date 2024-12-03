8f172d9b43afae4178c61f9e222145f754c37066
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-12-03 09:40:33 +0300

    add - doc - Added element command
    ---
    
    We've added a new addon that allows you to get chemical substance
    information in the command line. Soon, the screensaver will be made.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Backport Required: False
    Part: 1/1

---
+++ public/Nitrocid.Addons/Nitrocid.Extras.Chemistry/ChemistryInit.cs
@@ -0,0 +1,67 @@
+﻿//
+// Nitrocid KS  Copyright (C) 2018-2024  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Nitrocid.Extras.Chemistry.Commands;
+using Nitrocid.Kernel.Extensions;
+using Nitrocid.Modifications;
+using Nitrocid.Shell.ShellBase.Arguments;
+using Nitrocid.Shell.ShellBase.Commands;
+using Nitrocid.Shell.ShellBase.Shells;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Nitrocid.Extras.Chemistry
+{
+    internal class ChemistryInit : IAddon
+    {
+        private readonly List<CommandInfo> addonCommands =
+        [
+            new CommandInfo("element", /* Localizable */ "Prints information about a chemical substance to the console.",
+                [
+                    new CommandArgumentInfo(
+                    [
+                        new CommandArgumentPart(true, "name/symbol/atomicNumber"),
+                    ])
+                ], new ElementCommand())
+        ];
+
+        string IAddon.AddonName =>
+            InterAddonTranslations.GetAddonName(KnownAddons.ExtrasChemistry);
+
+        ModLoadPriority IAddon.AddonType => ModLoadPriority.Optional;
+
+        ReadOnlyDictionary<string, Delegate>? IAddon.PubliclyAvailableFunctions => null;
+
+        ReadOnlyDictionary<string, PropertyInfo>? IAddon.PubliclyAvailableProperties => null;
+
+        ReadOnlyDictionary<string, FieldInfo>? IAddon.PubliclyAvailableFields => null;
+
+        void IAddon.StartAddon() =>
+            CommandManager.RegisterAddonCommands(ShellType.Shell, [.. addonCommands]);
+
+        void IAddon.StopAddon() =>
+            CommandManager.UnregisterAddonCommands(ShellType.Shell, [.. addonCommands.Select((ci) => ci.Command)]);
+
+        void IAddon.FinalizeAddon()
+        { }
+    }
+}
