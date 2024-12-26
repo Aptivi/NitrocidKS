8f5b02e8e932fc177fb4349000464b31cd54d5a6
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-12-26 12:45:29 +0300

    add - Added scaffolding for N.E.Mods
    ---
    
    We've added scaffolding for this addon that will incorporate mod loading
    code. The Lite version will no longer support kernel modifications.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ public/Nitrocid.Addons/Nitrocid.Extras.Mods/ModsInit.cs
@@ -0,0 +1,50 @@
+﻿//
+// Nitrocid KS  Copyright (C) 2018-2025  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Nitrocid.Kernel.Extensions;
+using Nitrocid.Modifications;
+using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+namespace Nitrocid.Extras.Mods
+{
+    internal class ModsInit : IAddon
+    {
+        string IAddon.AddonName =>
+            InterAddonTranslations.GetAddonName(KnownAddons.ExtrasMods);
+
+        ModLoadPriority IAddon.AddonType => ModLoadPriority.Important;
+
+        ReadOnlyDictionary<string, Delegate>? IAddon.PubliclyAvailableFunctions => null;
+
+        ReadOnlyDictionary<string, PropertyInfo>? IAddon.PubliclyAvailableProperties => null;
+
+        ReadOnlyDictionary<string, FieldInfo>? IAddon.PubliclyAvailableFields => null;
+
+        void IAddon.FinalizeAddon()
+        { }
+
+        void IAddon.StartAddon()
+        { }
+
+        void IAddon.StopAddon()
+        { }
+    }
+}
