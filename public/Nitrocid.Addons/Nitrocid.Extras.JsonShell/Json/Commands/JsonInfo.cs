44244d087cdd6e5afe9d687db4b860fd3bcfaaaf
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-12-31 14:18:17 +0300

    fix - Fixed invalid values in some keys in JsonInf...
    ...o
    
    ---
    
    ", true, false"? Seriously?
    
    ---
    
    Type: fix
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.Extras.JsonShell/Json/Commands/JsonInfo.cs
+++ public/Nitrocid.Addons/Nitrocid.Extras.JsonShell/Json/Commands/JsonInfo.cs
@@ -37,7 +37,7 @@ namespace Nitrocid.Extras.JsonShell.Json.Commands
             // Base info
             SeparatorWriterColor.WriteSeparator(Translate.DoTranslation("Base JSON token information"));
             TextWriterColor.Write(Translate.DoTranslation("Base type") + ": {0}", JsonShellCommon.FileToken.Type);
-            TextWriterColor.Write(Translate.DoTranslation("Base has values") + ": {0}", true, false, JsonShellCommon.FileToken.HasValues);
+            TextWriterColor.Write(Translate.DoTranslation("Base has values") + ": {0}", JsonShellCommon.FileToken.HasValues);
             TextWriterColor.Write(Translate.DoTranslation("Children token count") + ": {0}", JsonShellCommon.FileToken.Count());
             TextWriterColor.Write(Translate.DoTranslation("Base path") + ": {0}", JsonShellCommon.FileToken.Path);
             TextWriterRaw.Write();
@@ -49,7 +49,7 @@ namespace Nitrocid.Extras.JsonShell.Json.Commands
                 {
                     SeparatorWriterColor.WriteSeparator(Translate.DoTranslation("Individual JSON token information") + " [{0}]", true, token.Path);
                     TextWriterColor.Write(Translate.DoTranslation("Token type") + ": {0}", token.Type);
-                    TextWriterColor.Write(Translate.DoTranslation("Token has values") + ": {0}", true, false, token.HasValues);
+                    TextWriterColor.Write(Translate.DoTranslation("Token has values") + ": {0}", token.HasValues);
                     TextWriterColor.Write(Translate.DoTranslation("Children token count") + ": {0}", token.Count());
                     TextWriterColor.Write(Translate.DoTranslation("Token path") + ": {0}", token.Path);
                     if (parameters.SwitchesList.Contains("-showvals"))
