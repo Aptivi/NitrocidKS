ebab35cbe33689849c945abc57ba8a175e85ee07
Aptivi <eoflaoevicecity@gmail.com>
2025-02-02 12:05:44 +0300

    imp - Prevent disposed stream errors for audio cue...
    ...s
    
    ---
    
    We've improved the audio cue container code so that it doesn't try to give
    disposed streams.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid/Misc/Audio/AudioCueContainer.cs
+++ public/Nitrocid/Misc/Audio/AudioCueContainer.cs
@@ -44,82 +44,114 @@ namespace Nitrocid.Misc.Audio
         /// <summary>
         /// A stream containing audio data for the intense version of the alarm sound effect
         /// </summary>
-        public Stream? AlarmStream { get; internal set; } = ResourcesManager.GetData("the_mirage.alarm.mp3", ResourcesType.Audio);
+        public Stream? AlarmStream =>
+            ResourcesManager.GetData($"{Name}.alarm.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.alarm.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the idle version of the alarm sound effect
         /// </summary>
-        public Stream? AlarmIdleStream { get; internal set; } = ResourcesManager.GetData("the_mirage.alarm-idle.mp3", ResourcesType.Audio);
+        public Stream? AlarmIdleStream =>
+            ResourcesManager.GetData($"{Name}.alarm-idle.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.alarm-idle.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the intense version of the ambience sound effect
         /// </summary>
-        public Stream? AmbienceStream { get; internal set; } = ResourcesManager.GetData("the_mirage.ambience.mp3", ResourcesType.Audio);
+        public Stream? AmbienceStream =>
+            ResourcesManager.GetData($"{Name}.ambience.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.ambience.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the idle version of the ambience sound effect
         /// </summary>
-        public Stream? AmbienceIdleStream { get; internal set; } = ResourcesManager.GetData("the_mirage.ambience-idle.mp3", ResourcesType.Audio);
+        public Stream? AmbienceIdleStream =>
+            ResourcesManager.GetData($"{Name}.ambience-idle.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.ambience-idle.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the high-priority beep sound
         /// </summary>
-        public Stream? BeepHighStream { get; internal set; } = ResourcesManager.GetData("the_mirage.beep-high.mp3", ResourcesType.Audio);
+        public Stream? BeepHighStream =>
+            ResourcesManager.GetData($"{Name}.beep-high.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.beep-high.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the medium-priority beep sound
         /// </summary>
-        public Stream? BeepMediumStream { get; internal set; } = ResourcesManager.GetData("the_mirage.beep-med.mp3", ResourcesType.Audio);
+        public Stream? BeepMediumStream =>
+            ResourcesManager.GetData($"{Name}.beep-med.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.beep-med.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the low-priority beep sound
         /// </summary>
-        public Stream? BeepLowStream { get; internal set; } = ResourcesManager.GetData("the_mirage.beep-low.mp3", ResourcesType.Audio);
+        public Stream? BeepLowStream =>
+            ResourcesManager.GetData($"{Name}.beep-low.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.beep-low.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the keyboard cue backspace sound
         /// </summary>
-        public Stream? KeyboardCueBackspaceStream { get; internal set; } = ResourcesManager.GetData("the_mirage.keyboard-cue-backspace.mp3", ResourcesType.Audio);
+        public Stream? KeyboardCueBackspaceStream =>
+            ResourcesManager.GetData($"{Name}.keyboard-cue-backspace.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.keyboard-cue-backspace.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the keyboard cue enter sound
         /// </summary>
-        public Stream? KeyboardCueEnterStream { get; internal set; } = ResourcesManager.GetData("the_mirage.keyboard-cue-enter.mp3", ResourcesType.Audio);
+        public Stream? KeyboardCueEnterStream =>
+            ResourcesManager.GetData($"{Name}.keyboard-cue-enter.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.keyboard-cue-enter.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the keyboard cue type sound
         /// </summary>
-        public Stream? KeyboardCueTypeStream { get; internal set; } = ResourcesManager.GetData("the_mirage.keyboard-cue-type.mp3", ResourcesType.Audio);
+        public Stream? KeyboardCueTypeStream =>
+            ResourcesManager.GetData($"{Name}.keyboard-cue-type.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.keyboard-cue-type.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the high-priority notification sound
         /// </summary>
-        public Stream? NotificationHighStream { get; internal set; } = ResourcesManager.GetData("the_mirage.notification-high.mp3", ResourcesType.Audio);
+        public Stream? NotificationHighStream =>
+            ResourcesManager.GetData($"{Name}.notification-high.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.notification-high.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the medium-priority notification sound
         /// </summary>
-        public Stream? NotificationMediumStream { get; internal set; } = ResourcesManager.GetData("the_mirage.notification-medium.mp3", ResourcesType.Audio);
+        public Stream? NotificationMediumStream =>
+            ResourcesManager.GetData($"{Name}.notification-medium.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.notification-medium.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the low-priority notification sound
         /// </summary>
-        public Stream? NotificationLowStream { get; internal set; } = ResourcesManager.GetData("the_mirage.notification-low.mp3", ResourcesType.Audio);
+        public Stream? NotificationLowStream =>
+            ResourcesManager.GetData($"{Name}.notification-low.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.notification-low.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the shutdown sound
         /// </summary>
-        public Stream? ShutdownStream { get; internal set; } = ResourcesManager.GetData("the_mirage.shutdown.mp3", ResourcesType.Audio);
+        public Stream? ShutdownStream =>
+            ResourcesManager.GetData($"{Name}.shutdown.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.shutdown.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the special beep sound
         /// </summary>
-        public Stream? SpecialBeepStream { get; internal set; } = ResourcesManager.GetData("the_mirage.special-beep.mp3", ResourcesType.Audio);
+        public Stream? SpecialBeepStream =>
+            ResourcesManager.GetData($"{Name}.special-beep.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.special-beep.mp3", ResourcesType.Audio);
 
         /// <summary>
         /// A stream containing audio data for the startup sound
         /// </summary>
-        public Stream? StartupStream { get; internal set; } = ResourcesManager.GetData("the_mirage.startup.mp3", ResourcesType.Audio);
+        public Stream? StartupStream =>
+            ResourcesManager.GetData($"{Name}.startup.mp3", ResourcesType.Audio) ??
+            ResourcesManager.GetData("the_mirage.startup.mp3", ResourcesType.Audio);
 
         internal Stream? GetStream(AudioCueType cueType) =>
             cueType switch
@@ -151,24 +183,6 @@ namespace Nitrocid.Misc.Audio
         {
             Name = name;
             DisplayName = displayName;
-
-            // Populate streams
-            AlarmStream = ResourcesManager.GetData($"{Name}.alarm.mp3", ResourcesType.Audio) ?? AlarmStream;
-            AlarmIdleStream = ResourcesManager.GetData($"{Name}.alarm-idle.mp3", ResourcesType.Audio) ?? AlarmIdleStream;
-            AmbienceStream = ResourcesManager.GetData($"{Name}.ambience.mp3", ResourcesType.Audio) ?? AmbienceStream;
-            AmbienceIdleStream = ResourcesManager.GetData($"{Name}.ambience-idle.mp3", ResourcesType.Audio) ?? AmbienceIdleStream;
-            BeepHighStream = ResourcesManager.GetData($"{Name}.beep-high.mp3", ResourcesType.Audio) ?? BeepHighStream;
-            BeepMediumStream = ResourcesManager.GetData($"{Name}.beep-med.mp3", ResourcesType.Audio) ?? BeepMediumStream;
-            BeepLowStream = ResourcesManager.GetData($"{Name}.beep-low.mp3", ResourcesType.Audio) ?? BeepLowStream;
-            KeyboardCueBackspaceStream = ResourcesManager.GetData($"{Name}.keyboard-cue-backspace.mp3", ResourcesType.Audio) ?? KeyboardCueBackspaceStream;
-            KeyboardCueEnterStream = ResourcesManager.GetData($"{Name}.keyboard-cue-enter.mp3", ResourcesType.Audio) ?? KeyboardCueEnterStream;
-            KeyboardCueTypeStream = ResourcesManager.GetData($"{Name}.keyboard-cue-type.mp3", ResourcesType.Audio) ?? KeyboardCueTypeStream;
-            NotificationHighStream = ResourcesManager.GetData($"{Name}.notification-high.mp3", ResourcesType.Audio) ?? NotificationHighStream;
-            NotificationMediumStream = ResourcesManager.GetData($"{Name}.notification-medium.mp3", ResourcesType.Audio) ?? NotificationMediumStream;
-            NotificationLowStream = ResourcesManager.GetData($"{Name}.notification-low.mp3", ResourcesType.Audio) ?? NotificationLowStream;
-            ShutdownStream = ResourcesManager.GetData($"{Name}.shutdown.mp3", ResourcesType.Audio) ?? ShutdownStream;
-            SpecialBeepStream = ResourcesManager.GetData($"{Name}.special-beep.mp3", ResourcesType.Audio) ?? SpecialBeepStream;
-            StartupStream = ResourcesManager.GetData($"{Name}.startup.mp3", ResourcesType.Audio) ?? StartupStream;
         }
     }
 }
--- public/Nitrocid/Misc/Reflection/Internal/ResourcesManager.cs
+++ public/Nitrocid/Misc/Reflection/Internal/ResourcesManager.cs
@@ -30,108 +30,31 @@ namespace Nitrocid.Misc.Reflection.Internal
 {
     internal static class ResourcesManager
     {
-        private static readonly Dictionary<Assembly, Dictionary<string, Stream>> assemblies = [];
-
         internal static bool DataExists(string resource, ResourcesType type, out Stream? data, Assembly? asm = null)
         {
             asm ??= Assembly.GetExecutingAssembly();
-            data = null;
-            InitializeData(asm);
-            if (!assemblies.TryGetValue(asm, out Dictionary<string, Stream>? asmResources))
-            {
-                DebugWriter.WriteDebug(DebugLevel.E, $"{asm.FullName ?? "This unknown assembly"} doesn't have any resource.");
-                return false;
-            }
 
             // Get the stream
-            RepopulateDisposed(resource, type, asm);
-            bool result =
-                type == ResourcesType.Misc ?
-                asmResources.TryGetValue(resource, out data) :
-                asmResources.TryGetValue($"{type}.{resource}", out data);
+            string resourceName = type == ResourcesType.Misc ? resource : $"{type}.{resource}";
+            string resourceFullName = $"{asm.GetName().Name}.Resources.{resourceName}";
+            data = asm.GetManifestResourceStream(resourceFullName);
             DebugWriter.WriteDebug(DebugLevel.I, $"{asm.FullName ?? "This unknown assembly"} got resource {resource} with type {type} and got {(data is not null ? $"a data stream totalling {data.Length} bytes" : "nothing")}.");
-            return result;
+            return data is not null;
         }
 
         internal static Stream? GetData(string resource, ResourcesType type, Assembly? asm = null)
         {
             asm ??= Assembly.GetExecutingAssembly();
-            InitializeData(asm);
             if (!DataExists(resource, type, out Stream? data, asm))
-                throw new KernelException(KernelExceptionType.Reflection, $"Resource {resource} not found for type {type} in between {assemblies.Count} assemblies");
+                throw new KernelException(KernelExceptionType.Reflection, $"Resource {resource} not found for type {type} in {asm.FullName ?? "this unknown assembly"}");
             return data;
         }
 
         internal static string[] GetResourceNames(Assembly? asm)
         {
             asm ??= Assembly.GetExecutingAssembly();
-            InitializeData(asm);
-            if (!assemblies.TryGetValue(asm, out Dictionary<string, Stream>? asmResources))
-                return [];
-            return asmResources.Select((kvp) => kvp.Key).ToArray();
-        }
-
-        internal static void InitializeData(Assembly? assembly)
-        {
-            if (assembly is null)
-                return;
-            if (assemblies.ContainsKey(assembly))
-                return;
-            DebugWriter.WriteDebug(DebugLevel.I, $"Initializing data for {assembly.FullName ?? "an unknown assembly"}...");
-            var resourceFullNames = assembly.GetManifestResourceNames();
-            var asmResources = new Dictionary<string, Stream>();
-            foreach (string resourceFullName in resourceFullNames)
-            {
-                // Get the stream
-                DebugWriter.WriteDebug(DebugLevel.I, $"Processing resource {resourceFullName}...");
-                var contentStream = assembly.GetManifestResourceStream(resourceFullName);
-                if (contentStream is null)
-                {
-                    DebugWriter.WriteDebug(DebugLevel.E, $"Assembly doesn't have stream for {resourceFullName}. Ignoring...");
-                    continue;
-                }
-                
-                // Afterwards, add the resulting content to the resources dictionary to cache it
-                string fileName = resourceFullName.RemovePrefix($"{assembly.GetName().Name}.Resources.");
-                DebugWriter.WriteDebug(DebugLevel.I, $"Adding resource as {fileName}...");
-                asmResources.Add(fileName, contentStream);
-            }
-            if (asmResources.Count > 0)
-            {
-                DebugWriter.WriteDebug(DebugLevel.I, $"Adding {assembly.FullName ?? "an unknown assembly"} with {asmResources.Count} resources...");
-                assemblies.TryAdd(assembly, asmResources);
-            }
-        }
-
-        internal static void RepopulateDisposed(string resource, ResourcesType type, Assembly asm)
-        {
-            // Bail if we don't have any stream
-            if (!assemblies.TryGetValue(asm, out var asmResources))
-                return;
-
-            // Now, get the resource name and try to get it
-            string resourceName = type == ResourcesType.Misc ? resource : $"{type}.{resource}";
-            string resourceFullName = $"{asm.GetName().Name}.Resources.{resourceName}";
-            if (!asmResources.TryGetValue(resourceName, out var data))
-                return;
-
-            // Check to see if this stream is disposed, and regenerate the stream if needed
-            try
-            {
-                DebugWriter.WriteDebug(DebugLevel.I, $"{asm.FullName ?? "This unknown assembly"} got resource {resource} with type {type} and got {(data is not null ? $"a data stream totalling {data.Length} bytes" : "nothing")}.");
-            }
-            catch (ObjectDisposedException)
-            {
-                DebugWriter.WriteDebug(DebugLevel.W, $"{asm.FullName ?? "This unknown assembly"} got resource {resource} with type {type} and got data, but it's disposed. Regenerating...");
-                var contentStream = asm.GetManifestResourceStream(resourceFullName);
-                DebugWriter.WriteDebug(DebugLevel.I, $"While checking for disposed stream, {asm.FullName ?? "This unknown assembly"} got resource {resource} with type {type} and got {(contentStream is not null ? $"a data stream totalling {contentStream.Length} bytes" : "nothing")} to be added.");
-                if (contentStream is null)
-                {
-                    DebugWriter.WriteDebug(DebugLevel.E, $"Assembly doesn't have stream for {resourceFullName}. Ignoring...");
-                    return;
-                }
-                assemblies[asm][resourceName] = contentStream;
-            }
+            var resources = asm.GetManifestResourceNames().Select((resource) => resource.RemovePrefix($"{asm.GetName().Name}.Resources.")).ToArray();
+            return resources;
         }
 
         internal static string ConvertToString(Stream? contentStream)
