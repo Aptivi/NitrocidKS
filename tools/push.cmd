a6b3fce0dd6279dd9e04ac43a58a8dfe4af31215
Aptivi <eoflaoevicecity@gmail.com>
2025-02-05 11:22:32 +0300

    int - Added release canary workflow
    ---
    
    The release canary workflow pushes to GitHub Package Registry.
    
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/push.cmd
+++ tools/push.cmd
@@ -1,9 +1,11 @@
 @echo off
 set apikey=%1
+set source=%2
+if "%source%" == "" set source=nuget.org
 
 REM This script pushes. Use when you have VS installed.
 echo Pushing...
-cmd /C "forfiles /s /m *.nupkg /p ..\ /C "cmd /c dotnet nuget push @path --api-key %apikey% --source nuget.org""
+cmd /C "forfiles /s /m *.nupkg /p ..\ /C "cmd /c dotnet nuget push @path --api-key %apikey% --source %source%""
 if %errorlevel% == 0 goto :success
 echo There was an error trying to push (%errorlevel%).
 goto :finished
