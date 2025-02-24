0463a156c29c85dec22b146a93a9dfc33409fd0e
Aptivi <eoflaoevicecity@gmail.com>
2025-02-24 14:09:41 +0300

    add - Added Visual Basic mod template
    ---
    
    A new template for Nitrocid mods that provides a minimal template for
    Visual Basic has been added.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ public/Nitrocid.Templates/templates/KSModVB/Command.vb
@@ -0,0 +1,11 @@
+﻿Imports Nitrocid.Shell.ShellBase.Commands
+
+Namespace KSMod
+    Friend Class TuiCommand
+        Inherits BaseCommand
+        Implements ICommand
+        Public Overrides Function Execute(parameters As CommandParameters, ByRef variableValue As String) As Integer
+            Return 0
+        End Function
+    End Class
+End Namespace
---
+++ public/Nitrocid.Templates/templates/KSModVB/ModName.vb
@@ -0,0 +1,40 @@
+﻿Imports Nitrocid.Modifications
+
+Namespace KSMod
+    Public Class ModName
+        Implements IMod
+        Public ReadOnly Property Name As String Implements IMod.Name
+            Get
+                Return "My Mod"
+            End Get
+        End Property
+
+        Public ReadOnly Property Version As String Implements IMod.Version
+            Get
+                Return "1.0.0"
+            End Get
+        End Property
+
+        Public ReadOnly Property MinimumSupportedApiVersion As Version Implements IMod.MinimumSupportedApiVersion
+            Get
+                Return New Version(3, 1, 27, 48)
+            End Get
+        End Property
+
+        Public ReadOnly Property LoadPriority As ModLoadPriority Implements IMod.LoadPriority
+            Get
+                Return ModLoadPriority.Optional
+            End Get
+        End Property
+
+        Public Sub StartMod() Implements IMod.StartMod
+
+        End Sub
+
+        Public Sub StopMod() Implements IMod.StopMod
+
+        End Sub
+    End Class
+End Namespace
+
+' Refer to https://aptivi.github.io/Nitrocid for up-to-date API documentation for mod developers.
