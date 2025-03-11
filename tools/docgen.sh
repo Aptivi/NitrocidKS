89d4a6532ced59c155b8ffcf5582441c6d2c19fa
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-11 19:56:37 +0300

    int - Backported build system improvements
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/docgen.sh
+++ tools/docgen.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-#    Nitrocid KS  Copyright (C) 2018-2022  Aptivi
-#
-#    This file is part of Nitrocid KS
-#
-#    Nitrocid KS is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Nitrocid KS is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Repository root
+ROOTDIR=$( cd -- "$( dirname -- "$0" )/.." &> /dev/null && pwd )
 
-# This script builds KS and packs the artifacts. Use when you have MSBuild installed.
 # Check for dependencies
 msbuildpath=`which docfx`
 if [ ! $? == 0 ]; then
@@ -29,9 +14,9 @@ fi
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_NOLOGO=1
 
-# Build KS
-echo Building KS documentation...
-docfx ../DocGen/docfx.json
+# Build docs
+echo Building documentation...
+docfx $ROOTDIR/DocGen/docfx.json
 if [ ! $? == 0 ]; then
 	echo Build failed.
 	exit 1
