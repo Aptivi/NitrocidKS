b4a8d6d7663a4ecffb9c94c5f1dec9904793f056
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-11 19:56:38 +0300

    int - Backported build system improvements
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/docgen-pack.sh
+++ tools/docgen-pack.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-#    Nitrocid KS  Copyright (C) 2018-2025  Aptivi
-#
-#    This file is part of Nitrocid KS
-#
-#    Nitrocid KS is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Nitrocid KS is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Repository root
+ROOTDIR=$( cd -- "$( dirname -- "$0" )/.." &> /dev/null && pwd )
 
 # Convenience functions
 checkerror() {
@@ -26,8 +12,8 @@ checkerror() {
     fi
 }
 
-# This script builds KS and packs the artifacts. Use when you have MSBuild installed.
-ksversion=$(grep "<Version>" ../Directory.Build.props | cut -d "<" -f 2 | cut -d ">" -f 2)
+# This script builds the documentation and packs the artifacts.
+version=$(grep "<Version>" $ROOTDIR/Directory.Build.props | cut -d "<" -f 2 | cut -d ">" -f 2)
 checkerror $? "Failed to get version. Check to make sure that the version is specified correctly in D.B.props"
 
 # Check for dependencies
@@ -36,17 +22,17 @@ checkerror $? "zip is not found"
 
 # Pack documentation
 echo Packing documentation...
-cd "../docs/" && "$zippath" -r /tmp/$ksversion-doc.zip . && cd -
+cd "$ROOTDIR/docs/" && "$zippath" -r /tmp/$version-doc.zip . && cd -
 checkerror $? "Failed to pack"
 
 # Inform success
-rm -rf "../DocGen/api"
+rm -rf "$ROOTDIR/DocGen/api"
 checkerror $? "Failed to remove api folder"
-rm -rf "../DocGen/obj"
+rm -rf "$ROOTDIR/DocGen/obj"
 checkerror $? "Failed to remove obj folder"
-rm -rf "../docs"
+rm -rf "$ROOTDIR/docs"
 checkerror $? "Failed to remove docs folder"
-mv /tmp/$ksversion-doc.zip .
+mv /tmp/$version-doc.zip "$ROOTDIR/tools"
 checkerror $? "Failed to move archive from temporary folder"
 echo Pack successful.
 exit 0
