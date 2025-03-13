b4a410bad22ba64c73811892abf0d73589234eb5
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-13 20:46:57 +0300

    int - Backported changes to build script (prepare)
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- vnd/vendor-docgen.cmd
+++ vnd/vendor-docgen.cmd
@@ -14,10 +14,5 @@ set DOTNET_NOLOGO=1
 
 echo Building the documentation...
 %USERPROFILE%\.dotnet\tools\docfx.exe "%ROOTDIR%\DocGen\docfx.json"
-if %errorlevel% == 0 goto :success
-echo There was an error trying to build documentation (%errorlevel%).
-goto :finished
 
-:success
-echo Build and pack successful.
 :finished
